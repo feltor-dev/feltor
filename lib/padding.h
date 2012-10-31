db5d27091cad149f2ad97a13195a0c804b9a105e
matthias <Matthias.Wiesenberger@uibk.ac.at>
2012-10-31 18:53:27 +0100

    outsourced padding routines to padding.h

---
+++ lib/padding.h
@@ -0,0 +1,52 @@
+#ifndef _PADDING_
+#define _PADDING_
+
+namespace toefl{
+
+    enum Padding{ TL_NONE, TL_DFT_1D, TL_DFT_2D, TL_DST_DFT, TL_DFT_DFT};
+
+    /*! @brief template traits class for the efficient implementation of
+     * the access operators in the matrix class.
+     *
+     * These functions are also used in copy and construction operators.
+     */
+    template <enum Padding P> //i.e. TL_NONE
+    struct TotalNumberOf
+    {
+        static inline size_t cols( const size_t m){return m;}
+        static inline size_t elements( const size_t n, const size_t m){return n*m;}
+    };
+    
+    template <>
+    struct TotalNumberOf<TL_DFT_1D>
+    {
+        static inline size_t cols( const size_t m){ return m - m%2 + 2;}
+        static inline size_t elements( const size_t n, const size_t m){return n*(m - m%2 + 2);}
+    };
+    
+    template <>
+    struct TotalNumberOf<TL_DFT_2D>
+    {
+        static inline size_t cols( const size_t m){ return m;}
+        static inline size_t elements( const size_t n, const size_t m){return n*(m - m%2 + 2);}
+    };
+    template <>
+    struct TotalNumberOf<TL_DST_DFT>
+    {
+        static inline size_t cols( const size_t m){ return m;}
+        static inline size_t elements( const size_t n, const size_t m){return m*(n - n%2 + 2);}
+    };
+    template <>
+    struct TotalNumberOf<TL_DFT_DFT>
+    {
+        static inline size_t cols( const size_t m){ return m - m%2 + 2;}
+        static inline size_t elements( const size_t n, const size_t m){return n*(m - m%2 + 2);}
+    };
+
+
+
+}
+
+
+
+#endif //_PADDING_
