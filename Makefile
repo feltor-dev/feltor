dc2c9d7a1b4023a8d995370feb63a50503a1cd85
matthias <Matthias.Wiesenberger@uibk.ac.at>
2012-11-07 15:29:24 +0100

    added equation coefficients in coefficients.h

--- Makefile
+++ Makefile
@@ -2,3 +2,9 @@
 doc: 
 	doxygen Doxyfile
 
+%.pdf: %.tex
+	latex $*
+	dvips $*
+	ps2pdf $*.ps
+	rm $*.dvi $*.log $*.ps 
+
---
+++ source/coefficients.h
@@ -0,0 +1,161 @@
+#ifndef _COEFFICIENTS_
+#define _COEFFICIENTS_
+
+#include "matrix.h"
+#include "vector.h"
+
+namespace toefl{
+   
+    //Vorläufig
+    struct Physical
+    {
+        const double dd, nu;
+        const double g_e, g_i, g_z;
+        const double kappa_x, kappa_y;
+        const double a_i, mu_i, tau_i;
+        const double a_z, mu_z, tau_z;
+    };
+    /*! @brief Yield the linear part of the local toefl equations
+     */
+    class Equations
+    {
+      private:
+        typedef std::complex<double> complex;
+        Poisson p;
+        const double dd, nu;
+        const double g_e, g_i, g_z;
+        const double kappa_x, kappa_y;
+        const double tau_i, tau_z;
+        double L, rho;
+        complex curv;
+      public:
+        Equations( const Physical phys):p(phys) dd(phys.dd), nu(phys.nu), tau_i(phys.tau_i), tau_z(phys.tau_z), kappa_x(phys.kappa_x), kappa_y(phys.kappa_y)
+        g_e(phys.g_e), g_i(phys.g_i), g_z(phys.g_z){}
+        /*! @brief compute the linear part of the toefl equations without impurities
+         *
+         * @param coeff Contains the coefficients on output
+         * @param dx    The value of the x-derivative in fourier space
+         * @param dy    The value of the y-derivative in fourier space
+         * \note This way you have the freedom to use various expansion functions (e.g. sine, cosine or exponential functions) 
+         */
+        void operator()( QuadMat< complex,2>& coeff, const complex dx, const complex dy);
+        /*! @brief compute the linear part of the toefl equations with impurities
+         *
+         * @param coeff Contains the coefficients on output
+         * @param dx    The value of the x-derivative in fourier space
+         * @param dy    The value of the y-derivative in fourier space
+         * \note This way you have the freedom to use various expansion functions (e.g. sine, cosine or exponential functions) 
+         */
+        void operator()( QuadMat< complex,3>& coeff, const complex dx, const complex dy);
+    };
+    
+    /*! @brief Yield the coefficients for the local Poisson equation
+     */
+    class Poisson
+    {
+      private:
+        const double a_i, mu_i, tau_i;
+        const double a_z, mu_z, tau_z;
+      public: 
+        Poisson( const Physical phys);
+        /*! @brief Compute prefactors for ne and ni in local poisson equation
+         *
+         * @param phi   phi[0] multiplies with ne, phi[1]  with ni
+         * @param laplace   The laplacian in fourier space
+         */
+        void operator()( Vector< double,2>& phi, const double laplace);
+        /*! @brief Compute prefactors for ne, ni and nz in local poisson equation
+         *
+         * @param phi   phi[0] multiplies with ne, phi[1]  with ni, phi[2] with nz
+         * @param laplace   The laplacian in fourier space
+         */
+        void operator()( Vector< double,3>& phi, const double laplace);
+        /*! @brief Compute Gamma_i
+         *
+         * @param laplace The laplacian in fourier space
+         * @return Gamma_i
+         */
+        inline double gamma_i( const double laplace);
+        /*! @brief Compute Gamma_z
+         *
+         * @param laplace The laplacian in fourier space
+         * @return Gamma_z
+         */
+        inline double gamma_z( const double laplace);
+        /*! @brief Compute Gamma2_i
+         *
+         * @param laplace The laplacian in fourier space
+         * @return Gamma2_i
+         */
+        inline double gamma2_i( const double laplace);
+        inline double gamma2_z( const double laplace);
+    };
+    Poisson::Poisson(const Physical phys):a_i(phys.a_i), mu_i(phys.mu_i), tau_i(phys.tau_i),
+                                          a_z(phys.a_z), mu_z(phys.mu_z), tau_z(phys.tau_z) 
+    {
+    }
+    
+    void Equations::operator()(QuadMat< complex, 2>& c, const complex dx, const complex dy) 
+    {
+        Vector< double,2> phi;
+        L = (dx*dx + dy*dy).real(); 
+        p( phi, L); //prefactors in Poisson equations (phi = phi[0]*ne + phi[1]*ni)
+        curv = (kappa_x*dx + kappa_y*dy); //note that curv is complex
+        complex P = -g_e*dy + curv + dd;
+        complex Q = -g_i*dy*p.gamma_i(L) + curv( p.gamma_i(L) + 0.5 *p.gamma2_i(L));
+
+        c(0,0) = P*phi[0] - curv - dd + nu*L*L; c(0,1) = P*phi[1];
+        c(1,0) = Q*phi[0];                      c(1,1) = Q*phi[1] + tau_i*curv + nu*L*L;
+    }
+    void Equations::operator()(QuadMat< complex, 3>& c, const complex dx, const complex dy) 
+    {
+        Vector< double,3> phi;
+        L = (dx*dx + dy*dy).real(); 
+        p( phi, L);
+        curv = (kappa_x*dx + kappa_y*dy); //note that curv is complex
+        complex P = -g_e*dy + curv + dd;
+        complex Q = -g_i*dy*p.gamma_i(L) + curv( p.gamma_i(L) + 0.5 *p.gamma2_i(L));
+        complex R = -g_z*dy*p.gamma_z(L) + curv( p.gamma_z(L) + 0.5 *p.gamma2_z(L));
+
+        c(0,0) = P*phi[0] - curv - dd + nu*L*L; c(0,1) = P*phi[1];                       c(0,2) = P*phi[2];
+        c(1,0) = Q*phi[0];                      c(1,1) = Q*phi[1] + tau_i*curv + nu*L*L; c(1,2) = Q*phi[2];
+        c(2,0) = R*phi[0];                      c(2,1) = R*phi[1];                       c(2,2) = R*phi[2] + tau_z*curv + nu*L*L;
+    }
+
+    void Poisson::operator()( Vector< double, 2>& c, const double laplace)
+    {
+        double rho = - a_i*mu_i*laplace/(1.+ tau_i*mu_i*laplace);
+        c[0] = 1./rho;
+        c[1] = -a_i*gamma_i(laplace)/rho;
+    }
+    void Poisson::operator()( Vector< double, 3>& c, const double laplace)
+    {
+        double rho = (a_i*mu_i/(1.- tau_i*mu_i*laplace) + a_z*mu_z/(1.-tau_z*mu_z*laplace))*laplace;
+        c[0] = 1./rho;
+        c[1] = -a_i*gamma_i(laplace)/rho;
+        c[2] = -a_z*gamma_z(laplace)/rho;
+    }
+    double Poisson::gamma_i( const double laplace)
+    {
+        return (1./(1. - 0.5*tau_i*mu_i*laplace));
+    }
+    double Poisson::gamma_z( const double laplace)
+    {
+        return (1./(1. - 0.5*tau_z*mu_z*laplace));
+    }
+    double Poisson::gamma2_i( const double laplace)
+    {
+        double gamma = gamma_i(laplace);
+        return 0.5*tau_i*mu_i*laplace*gamma*gamma;
+    }
+    double Poisson::gamma2_z( const double laplace)
+    {
+        double gamma = gamma_z(laplace);
+        return 0.5*tau_z*mu_z*laplace*gamma*gamma;
+    }
+
+}
+
+
+
+#endif //_COEFFICIENTS_
