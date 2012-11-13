277e22c6e2552084ac19f66b2b65aefc4d3c4503
matthias <Matthias.Wiesenberger@uibk.ac.at>
2012-11-13 11:22:29 +0100

    ein globales include file spendiert blueprint mit consistency check ausgerüstet

---
+++ lib/tl_numerics.h
@@ -0,0 +1,28 @@
+#ifndef _TL_NUMERICS_
+#define _TL_NUMERICS_
+
+//Message handling
+#include "message.h"
+#include "exceptions.h"
+//benchmarking
+#include "timer.h"
+//Matrices and vectors
+#include "vector.h"
+#include "quadmat.h"
+#include "padding.h"
+#include "matrix.h"
+#include "ghostmatrix.h"
+//Arkawa scheme
+#include "arakawa.h"
+//Fourier transforms
+#include "fft.h"
+#include "dft_dft.h"
+#include "dft_drt.h"
+#include "drt_dft.h"
+#include "drt_drt.h"
+//Coefficients
+#include "coeff.h"
+
+
+
+#endif
--- source/blueprint.h
+++ source/blueprint.h
@@ -11,13 +11,11 @@ enum target{ TL_ELECTRONS, TL_IONS, TL_IMPURITIES, TL_POTENTIAL};
 
 struct Physical
 {
-    private:
     double d, nu;
     double kappa_x, kappa_y;
     double g_e, g_i, g_z;
     double a_i, mu_i, tau_i;
     double a_z, mu_z, tau_z;
-    public:
     /*! @brief Construct container with all values set to zero
      */ 
     Physical(){
@@ -27,38 +25,45 @@ struct Physical
         mu_i = mu_z = 0; 
         tau_i = tau_z = 0;
     }
-    void setSpecies( enum target t, double a, double mu, double tau, double gradient);
-    friend class Equations;
 };
 
 struct Boundary
 {
     double lx;
     double ly;
+    enum bc BC;
+    Boudary():lx(0), ly(0), BC(TL_PERIODIC){}
 };
 
 struct Algorithmic
 {
     size_t nx; 
     size_t ny;
+    double h, dt;
+    enum method algorithm;
+    Algorithmic():nx(0),ny(0), h(0), dt(0), algorithm(TL_KARNIADAKIS){}
 };
 
 
-    
 /*! @brief The Setting for the pipeline 
  *
  * The Setting consists of parameters and capacities!
  * With this construction plan you can go to 
  * the pipeline manufacturer who constructs the pipeline. 
+ * It is recommended to call 
+ * \code
+ *  try{ blueprint.consistencyCheck();}
+ *  catch( Message& m){m.display();}
+ *  \endcode
+ * before constructing a Pipeline to catch any Messages before construction.
  */
 class BluePrint
 {
-
     const Physical phys;
     const Boundary bound;
     const Algorithmic alg;
     bool curvature, coupling, imp, global;
-    public:
+  public:
     /*! @brief Init parameters
      *
      * All capacities are disabled by default!
@@ -97,7 +102,24 @@ class BluePrint
             default: throw Message( "Unknown Capacity\n");
         }
     }
-
+    void consistencyCheck();
 }
+
+void BluePrint::consistencyCheck()
+{
+    //Check algorithm and boundaries
+    if( alg.dt <= 0) throw Message( "dt < 0!\n", ping);
+    if( alg.h - bound.lx/(double)alg.nx > 1e-15) throw Message( "h != lx/nx\n", ping); 
+    if( alg.h - bound.ly/(double)alg.ny > 1e-15) throw Message( "h != ly/ny\n", ping);
+    if( alg.nx == 0||alg.ny == 0) throw Message( "Set nx and ny!\n", ping);
+    //Check physical parameters
+    if( curvature && phys.kappa_x == 0 && phys.kappa_y ==0 ) throw Message( "Curvature enabled but zero!\n", ping);
+    if( phys.nu < 0) throw Message( "nu < 0!\n", ping);
+    if( phys.a_i <= 0 || phys.mu_i <= 0 || phys.tau_i <= 0) throw Message( "Ion species badly set\n", ping);
+    if( imp && (phys.a_i <= 0 || phys.mu_i <= 0 || phys.tau_i <= 0)) throw Message( "Impuritiy species badly set\n", ping);
+    if( global) throw Message( "Global solver not yet implemented\n", ping);
+}
+
+
 
 #endif //_BLUEPRINT_
