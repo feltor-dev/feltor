ea9caa72121945b3459d3040343afdc4b18b5482
matthias <Matthias.Wiesenberger@uibk.ac.at>
2013-02-14 11:21:14 +0100

    started dg lib

---
+++ dg_lib/projection_functions.h
@@ -0,0 +1,50 @@
+#ifndef _DG_PROJECTION_
+#define _DG_PROJECTION_
+
+namespace dg{
+
+double pipj( unsigned i, unsigned j)
+{
+    if( i==j) 
+        return 2./(2.*(double)i+1.);
+    return 0;
+}
+double pipj_inv( unsigned i, unsigned j)
+{
+    if( i==j)
+        return (2.*(double)i+1.)/2.;
+    return 0;
+}
+double pidxpj( unsigned i, unsigned j)
+{
+    if( i < j)
+    {
+        if( (i+j)%2 == 0)
+            return 0;
+        else
+            return 2;
+    }
+    return 0;
+}
+double rirj( unsigned i, unsigned j)
+{
+    return 1.;
+}
+double rilj( unsigned i, unsigned j)
+{
+    if( j%2==0)
+        return 1.;
+    return -1.;
+}
+double lirj( unsigned i, unsigned j)
+{
+    return rily(i,j);
+}
+double lilj( unsigned i, unsigned j)
+{
+    if( (i+j)%2 ==0)
+        return 1.;
+    return -1.;
+}
+
+#endif //_DG_PROJECTION_
