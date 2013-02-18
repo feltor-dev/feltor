0432539a15ed444c33bc8792ed582fe855cad2c9
matthias <Matthias.Wiesenberger@uibk.ac.at>
2013-02-18 16:58:24 +0100

    diagonally preconditioned cg method

--- dg_lib/cg.h
+++ dg_lib/cg.h
@@ -24,13 +24,19 @@ struct CG_BLAS1
 {
     static double ddot( const Vector& x, const Vector& y);
     static void daxpby( double alpha, const Vector& x, double beta, Vector& y);
+    //better to implement daxpy and daypx?
+    //A: unlikely, because in all three cases all elements of x and y have to be loaded
+    //and daxpy is memory bound
 };
 
 template < class Matrix, class Vector>
 struct CG_BLAS2
 {
     static void dsymv( double alpha, const Matrix& m, const Vector& x, double beta, Vector& y);
+    //preconditioned CG needs diagonal scaling:
+    static void ddot( const Vector& x, const Matrix& P, const Vector& y);
 };
+//CUDA relevant: BLAS routines must block until result is ready 
 
 template< class Matrix, class Vector>
 class CG
@@ -41,30 +47,86 @@ class CG
     double get_eps( ) {return eps;}
     void set_max( unsigned new_max) {max_iter = new_max;}
     unsigned get_max() {return max_iter;}
-    unsigned solve( const Matrix& A, Vector& x, const Vector& b);
+    unsigned operator()( const Matrix& A, Vector& x, const Vector& b);//solve?
   private:
-    Vector r, p, ap;
+    Vector r, p, ap; //could contain solutions of previous iterations?
     double eps;
     unsigned max_iter;
 };
 
 template< class Matrix, class Vector>
-unsigned CG<Matrix, Vector>::solve( const Matrix& A, Vector& x, const Vector& b)
+unsigned CG<Matrix, Vector>::operator()( const Matrix& A, Vector& x, const Vector& b)
 {
     double nrm2b = CG_BLAS1<Vector>::ddot(b,b);
-    p = r = b;
-    CG_BLAS2<Matrix, Vector>::dsymv( -1., A, x, 1.,r); //compute r_0 
-    double nrm2r_old = ddot( r, r); //and store the norm of it
+    r = b; CG_BLAS2<Matrix, Vector>::dsymv( -1., A, x, 1.,r); //compute r_0 
+    p = r;
+    double nrm2r_old = CG_BLAS1<Vector>::ddot( r, r); //and store the norm of it
     double alpha, nrm2r_new;
     for( unsigned i=1; i<max_iter; i++)
     {
         CG_BLAS2<Matrix, Vector>::dsymv( 1., A, p, 0., ap);
-        alpha = nrm2r_old /ddot( p, ap);
+        alpha = nrm2r_old /CG_BLAS1<Vector>::ddot( p, ap);
         CG_BLAS1<Vector>::daxpby( alpha, p, 1.,x);
         CG_BLAS1<Vector>::daxpby( -alpha, ap, 1., r);
-        nrm2r_new = ddot( r,r);
-        if( sqrt( nrm2r_new/nrm2b) < eps) return i;
+        nrm2r_new = CG_BLAS1<Vector>::ddot( r,r);
+        if( sqrt( nrm2r_new/nrm2b) < eps) 
+            return i;
         CG_BLAS1<Vector>::daxpby(1., r, nrm2r_new/nrm2r_old, p );
+        nrm2r_old=nrm2r_new;
+    }
+    return max_iter;
+}
+
+template< class Matrix, class Vector, class Preconditioner>
+class CG
+{
+  public:
+    CG( unsigned size):r(size), p(r), ap(r), eps(1e-10), max_iter(size){}
+    void set_eps( double eps_rel) {eps = eps_rel;}
+    double get_eps( ) {return eps;}
+    void set_max( unsigned new_max) {max_iter = new_max;}
+    unsigned get_max() {return max_iter;}
+    unsigned operator()( const Matrix& A, Vector& x, const Vector& b, const Preconditioner& P);//solve?
+  private:
+    Vector r, p, ap; //could contain solutions of previous iterations?
+    double eps;
+    unsigned max_iter;
+};
+
+/*
+//compared to unpreconditioned compare
+//ddot(r,r), axpby()
+//to 
+//ddot( r,P,r), dsymv(P)
+//i.e. it will be slower 
+//compared to normal preconditioned compare
+//ddot(r,P,r), dsymv(P)
+//to
+//ddot(r,z), dsymv(P), axpby(), (storage for z)
+//i.e. it's surely faster if P contains no more elements than z 
+//(which is the case for diagonal scaling)
+//NOTE: the same comparison hold for A with the result that A contains 
+//significantly more elements than z whence ddot(r,A,r) is far slower than ddot(r,z)
+*/
+template< class Matrix, class Vector, class Preconditioner>
+unsigned CG< Matrix, Vector, Preconditioner>::operator()( const Matrix& A, Vector& x, const Vector& b, const Preconditioner& P)
+{
+    double nrm2b = PCG_BLAS2<Preconditioner, Vector>::ddot( b,P,b);
+    r = b; CG_BLAS2<Matrix, Vector>::dsymv( -1., A, x, 1.,r); //compute r_0 
+    CG_BLAS2<Preconditioner, Vector>::dsymv(1.,P, r, 0., p );//<-- compute p_0
+    double nrm2r_old = CG_BLAS2<Preconditioner, Vector>::ddot( r,P,r); //and store the norm of it
+    double alpha, nrm2r_new;
+    for( unsigned i=1; i<max_iter; i++)
+    {
+        CG_BLAS2<Matrix, Vector>::dsymv( 1., A, p, 0., ap);
+        alpha = nrm2r_old /CG_BLAS1<Vector>::ddot( p, ap);
+        CG_BLAS1<Vector>::daxpby( alpha, p, 1.,x);
+        CG_BLAS1<Vector>::daxpby( -alpha, ap, 1., r);
+        nrm2r_new = CG_BLAS2<Preconditioner, Vector>::ddot( r,P, r); //<--
+        if( sqrt( nrm2r_new/nrm2b) < eps) 
+            return i;
+        CG_BLAS2<Preconditioner, Vector>::dsymv(1.,P, r, nrm2r_new/nrm2r_old, p );//<--
+        nrm2r_old=nrm2r_new;
     }
     return max_iter;
 }
