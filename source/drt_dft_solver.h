1be6f5b2db90d080768b87f18c2398b7fd79f3ec
matthias <Matthias.Wiesenberger@uibk.ac.at>
2013-03-22 16:45:29 +0100

    final prerelease

--- source/drt_dft_solver.h
+++ source/drt_dft_solver.h
@@ -233,6 +233,8 @@ void DRT_DFT_Solver<n>::init( std::array< Matrix<double, TL_DRT_DFT>,n>& v, enum
                         cphi[0](i,j) += cdens[k](i,j)*phi_coeff(i,j)[k];
                 }
             break;
+        case( TL_ALL):
+            throw Message( "TL_ALL not treated yet!", ping);
     }
     //compute the rest cphi[k]
     for( unsigned k=0; k<n-1; k++)
@@ -262,6 +264,7 @@ void DRT_DFT_Solver<n>::getField( Matrix<double, TL_DRT_DFT>& m, enum target t)
         case( TL_IONS):         swap_fields( m, nonlinear[1]); break;
         case( TL_IMPURITIES):   swap_fields( m, nonlinear[2]); break;
         case( TL_POTENTIAL):    swap_fields( m, cphi[0]); break;
+        case( TL_ALL):          throw Message( "TL_ALL not allowed here", ping);
     }
 }
 template< size_t n>
@@ -274,6 +277,7 @@ const Matrix<double, TL_DRT_DFT>& DRT_DFT_Solver<n>::getField( enum target t) co
         case( TL_IONS):         m = &dens[1]; break;
         case( TL_IMPURITIES):   m = &dens[2]; break;
         case( TL_POTENTIAL):    m = &phi[0]; break;
+        case( TL_ALL):          throw Message( "TL_ALL not allowed here", ping);
     }
     return *m;
 }
