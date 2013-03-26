bc485db07aa76b3c1041f068abbd0a87638c6109
Matthias <MWiesenberger@gmx.de>
2013-03-26 22:13:28 +0100

    debugged laplace2d test

--- dg_lib/nvcc/evaluation_t.cu
+++ dg_lib/nvcc/evaluation_t.cu
@@ -17,14 +17,17 @@ double function( double x)
 
 double function( double x, double y)
 {
-    return exp(x)*exp(y);
+        return exp(x)*exp(y);
 }
 
 const unsigned n = 3;
-const unsigned N = 1000;
+const unsigned N = 10;
 
 const unsigned Nx = N;
 const unsigned Ny = N/2;
+
+const double lx = 2;
+const double ly = 2;
 
 typedef thrust::device_vector< double>   DVec;
 typedef thrust::host_vector< double>     HVec;
@@ -37,21 +40,21 @@ using namespace std;
 int main()
 {
     cout << "# of polynomial coefficients is: "<< n<<endl;
-    double h = 1./(double)N;
-    double hx = 1./(double)Nx; 
-    double hy = 1./(double)Ny; 
-    HArrVec h_v = dg::expand< double(&) (double), n>( function, 0, 1, N);
-    HArrMat h_m = dg::expand< double(&) (double, double), n>( function, 0, 1, 0, 1, Nx, Ny);
+    double h = lx/(double)N;
+    double hx = lx/(double)Nx; 
+    double hy = ly/(double)Ny; 
+    HArrVec h_v = dg::expand< double(&) (double), n>( function, 0, lx, N);
+    HArrMat h_m = dg::expand< double(&) (double, double), n>( function, 0, lx, 0, ly, Nx, Ny);
     DArrVec d_v( h_v.data());
     dg::blas2::symv( 1., dg::S1D<double, n>(h), h_v.data(), 0., h_v.data());
-    dg::blas2::symv( 1., dg::S2D<double, n>(hx,hy), h_m.data(), 0., h_m.data());
+    //dg::blas2::symv( dg::S2D<double, n>(hx,hy), h_m.data(), h_m.data());
     double norm = dg::blas2::dot( h_v.data(), dg::T1D<double, n>(h), h_v.data());
-    double norm2 = dg::blas2::dot( h_m.data(), dg::T2D<double, n>(hx,hy), h_m.data());
+    double norm2 = dg::blas2::dot( dg::S2D<double, n>(hx,hy), h_m.data());
     cout<< "Square normalized 1D norm "<< norm <<"\n";
-    double solution = (exp(2.) -exp(0))/2.;
+    double solution = (exp(4.) -exp(0))/2.;
     cout << "Correct square norm of exp(x) is "<<solution<<endl;
     cout<< "Square normalized 2D norm "<< norm2 <<"\n";
-    double solution2 = (exp(2.)-exp(0))/2.*(exp(2.) -exp(0))/2.;
+    double solution2 = (exp(4.)-exp(0))/2.*(exp(4.) -exp(0))/2.;
     cout << "Correct square norm of exp(x)exp(y) is "<<solution2<<endl;
     return 0;
 }
--- dg_lib/nvcc/laplace2d_t.cu
+++ dg_lib/nvcc/laplace2d_t.cu
@@ -8,11 +8,13 @@
 #include "dgvec.cuh"
 #include "dgmat.cuh"
 #include "evaluation.cuh"
+#include "preconditioner.cuh"
 #include "blas.h"
 
-const unsigned n = 2;
-const unsigned Nx = 4; //minimum 3
-const unsigned Ny = 3; //minimum 3
+
+const unsigned n = 3;
+const unsigned Nx = 10; //minimum 3
+const unsigned Ny = 10; //minimum 3
 
 const double lx = 2.*M_PI;
 const double ly = 2.*M_PI;
@@ -30,7 +32,7 @@ typedef dg::ArrVec2d< double, n, DVec>  DArrMat;
 typedef cusp::ell_matrix<int, double, cusp::host_memory> HMatrix;
 typedef cusp::ell_matrix<int, double, cusp::device_memory> DMatrix;
 
-double function( double x, double y) { return sin(x);}
+double function( double x, double y) { return sin(y);}
 double function( double x) { return sin(x);}
 
 int main()
@@ -42,6 +44,12 @@ int main()
     const double hy = ly/(double)Ny;
     HArrMat hv2d = expand< double(&)(double, double), n>( function, 0, lx, 0, ly, Nx, Ny), hw2d( hv2d);
     HArrVec hv1d = expand< double(&)(double), n>( function, 0, lx, Nx), hw1d( hv1d);
+    cout << "Before multiplication: \n";
+    double norm2 = blas2::dot( S1D<double, n>( hx), hw1d.data());
+    cout << "Norm2 1D is : "<<norm2<<endl;
+    cout << "yields in 2D: "<< norm2*ly<<endl;
+    double norm2_= blas2::dot( S2D<double, n>( hx, hy), hw2d.data());
+    cout << "Norm2 2D is : "<<norm2_<<endl;
 
     HMatrix laplace1d = create::laplace1d_per<n>(Nx, hx);
     HMatrix laplace2d = create::tensor<n>(create::laplace1d_per<n>( Ny, hy),
@@ -50,11 +58,17 @@ int main()
     blas2::symv( laplace1d, hv1d.data(), hw1d.data() );
     blas2::symv( laplace2d, hv2d.data(), hw2d.data() );
 
-    cout << "hw1d: \n"<<hw1d<<endl;
-    cout << "hw2d: \n"<<hw2d<<endl;
+    //cout << "hw1d: \n"<<hw1d<<endl;
+    //cout << "hw2d: \n"<<hw2d<<endl;
     blas2::symv( laplace2d_, hv2d.data(), hw2d.data() );
-    cout << "hw2d_: \n"<<hw2d<<endl;
+    //cout << "hw2d_: \n"<<hw2d<<endl;
 
+    cout << "After multiplication: \n";
+    norm2 = blas2::dot( S1D<double, n>( hx), hw1d.data());
+    cout << "Norm2 1D is : "<<norm2<<endl;
+    cout << "yields in 2D: "<< norm2*ly<<endl;
+    norm2_= blas2::dot( S2D<double, n>( hx, hy), hw2d.data());
+    cout << "Norm2 2D is : "<<norm2_<<endl;
 
     return 0;
 }
