52e5aa28f2f69560fe3cd925506a476ca407c0bf
Matthias <MWiesenberger@gmx.de>
2013-03-27 21:01:02 +0100

    Realized that preconditioner is slow

--- dg_lib/nvcc/preconditioner_b.cu
+++ dg_lib/nvcc/preconditioner_b.cu
@@ -5,6 +5,8 @@
 #include <cusp/ell_matrix.h>
 #include <cusp/dia_matrix.h>
 
+
+#include "preconditioner.cuh"
 #include "blas.h"
 #include "laplace.cuh"
 #include "timer.cuh"
@@ -19,7 +21,7 @@ using namespace std;
 using namespace dg;
 
 const unsigned n = 3;
-const unsigned N = 1e6;
+const unsigned N = 1e5;
 
 typedef thrust::device_vector<double>   DVec;
 typedef thrust::host_vector<double>     HVec;
@@ -29,39 +31,36 @@ typedef thrust::host_vector< Array<double, n> >   HArrVec;
 
 typedef ArrVec1d< double, n, HVec> HArrVec_;
 typedef ArrVec1d< double, n, DVec> DArrVec_;
-typedef cusp::ell_matrix<int, double, cusp::host_memory>   HMatrix;
-typedef cusp::ell_matrix<int, double, cusp::device_memory> DMatrix;
+typedef cusp::dia_matrix<int, double, cusp::host_memory>   HMatrix;
+typedef cusp::dia_matrix<int, double, cusp::device_memory> DMatrix;
 
 template< size_t n>
-struct Forward{
-    Forward(): forward( DLT<n>::forward){}
+struct Diagonal{
+    Diagonal( const double h = 2.): t1d( h){}
     
     __host__ __device__
     Array<double, n> operator() (const Array<double, n>& arr)
     {
         Array<double,n > temp(0.);
         for( unsigned i=0; i<n; i++)
-            for( unsigned j=0; j<n; j++)
-                temp[i] += forward(i,j)*arr[j];
+            temp[i] = t1d(i)*arr[i];
         return temp;
-               
     }
     private:
-    Operator<double, n> forward;
+    T1D<double, n> t1d;
 };
 
 template< size_t n>
-cusp::coo_matrix<int, double, cusp::host_memory> createForward( unsigned N)
+cusp::coo_matrix<int, double, cusp::host_memory> createDiagonal( unsigned N)
 {
-    cusp::coo_matrix<int, double, cusp::host_memory> A( n*N, n*N, n*n*N);
-    Operator< double, n> forward(DLT<n>::forward);
+    cusp::coo_matrix<int, double, cusp::host_memory> A( n*N, n*N, n*N);
+    T1D<double, n> t1d(2.);
     //std::cout << a << "\n"<<b <<std::endl;
     //assemble the matrix
     int number = 0;
     for( unsigned i=0; i<N; i++)
         for( unsigned k=0; k<n; k++)
-            for( unsigned l=0; l<n; l++)
-                create::detail::add_index<n>(A, number, i, i, k, l, forward(k,l));
+            create::detail::add_index<n>(A, number, i, i, k, k, t1d(i*n+k));
     return A;
 };
 
@@ -81,21 +80,27 @@ int main()
     DArrVec  dv( hv);
     DArrVec  dv2( N);
     DArrVec_ dv_( hv_);
-    DArrVec_ dv_2( N);
-    DMatrix dm = createForward<n>( N);
+    DArrVec_ dv_2( dv_);
+    DMatrix dm = createDiagonal<n>( N);
 
     t.tic();
-    thrust::transform( dv.begin(), dv.end(), dv.begin(), Forward<n>());
+    thrust::transform( dv.begin(), dv.end(), dv.begin(), Diagonal<n>());
     t.toc();
     cout << "Forward thrust transform took "<<t.diff()<<"s\n";
     t.tic();
     blas2::symv( dm, dv_.data(), dv_.data());
+    //blas1::axpby( 1., dv_.data(), 2., dv_.data());
     t.toc();
     cout << "Foward cusp transform took    "<<t.diff()<<"s\n";
+    t.tic();
+    blas2::symv( T1D<double, n>( 2.), dv_2.data(), dv_2.data());
+    t.toc();
+    cout << "Foward dg transform took      "<<t.diff()<<"s\n";
 
     //test for equality...
     /*
     hv_ = dv_;
+    HArrVec_ hv_2= dv_2;
     hv = dv;
     for( unsigned i=0; i<N; i++)
     {
@@ -105,6 +110,7 @@ int main()
     }
     cout << endl;
     cout << hv_ <<endl;
+    cout << endl << hv_2<<endl;
     */
 
     
