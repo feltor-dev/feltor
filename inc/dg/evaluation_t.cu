c7b4ec14f7844b3b1eb2eff101ecd5cf5bb9a4af
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2013-07-11 17:54:39 +0200

    got evaluation to work with dynamic n

--- inc/dg/evaluation_t.cu
+++ inc/dg/evaluation_t.cu
@@ -19,11 +19,6 @@ double function( double x, double y)
         return exp(x)*exp(y);
 }
 
-const unsigned N = 200;
-
-const unsigned Nx = 5;
-const unsigned Ny = 4;
-
 const double lx = 2;
 const double ly = 2;
 
@@ -36,35 +31,30 @@ int main()
     unsigned n;
     cout << "Type # of polynomial coefficients ( 1, 2,...,5)!\n";
     cin >> n;
+    unsigned N, Nx, Ny;
     cout << "# of polynomial coefficients is: "<< n<<endl;
+    cout << "Type # of grid cells (e.g. 10, 100)! ( Nx = N, Ny = 2N)\n";
+    cin >> N;
+    cout << "# of grid cells is: "<< N<<endl;
+    Nx = N; Ny = 2*N;
+
     dg::Grid1d<double> g1d( 0, lx, n, N);
     dg::Grid<double> g2d( 0, lx,0, ly,n, Nx, Ny);
 
     //test evaluation and expand functions
-    cout << "ping0\n";
     HVec h_v = dg::expand( function, g1d);
-    cout << "ping1\n";
     HVec h_x = dg::evaluate( function, g1d);
-    cout << "ping2\n";
     HVec h_m = dg::expand( function, g2d);
-    cout << "ping3\n";
     HVec h_n = dg::evaluate( function, g2d);
-    cout << "ping4\n";
-    DVec d_v( h_v);
 
     //test preconditioners
     dg::blas2::symv( 1., dg::S1D<double>(g1d), h_v, 0., h_v);
-    cout << "ping5\n";
 
     double norm = dg::blas2::dot( h_v, dg::T1D<double>(g1d), h_v);
-    cout << "ping6\n";
     double normX = dg::blas2::dot( h_x, dg::create::w1d(g1d), h_x);
-    cout << "ping7\n";
     //double norm2 = dg::blas2::dot( dg::S2D<double>(g2d), h_m);
     double norm2 = dg::blas2::dot( dg::create::s2d(g2d), h_m);
-    cout << "ping8\n";
     double norm2X = dg::blas2::dot( dg::create::w2d(g2d), h_n);
-    cout << "ping9\n";
 
     cout<< "Square normalized 1D norm "<< norm <<"\n";
     cout<< "Square normalized 1DXnorm "<< normX <<"\n";
