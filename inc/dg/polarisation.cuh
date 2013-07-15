ba0a4ead2b6768b849321458d91b42852b77edd0
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2013-07-15 14:09:48 +0200

    remove unnecessary assignment in polarisation

--- inc/dg/polarisation.cuh
+++ inc/dg/polarisation.cuh
@@ -276,8 +276,8 @@ cusp::csr_matrix<int, typename container::value_type, typename thrust::iterator_
     cusp::add( temp1, temp3, temp2);  // D_yy + D_xx
     cusp::add( temp2, jump, temp1); // Lap + Jump
     //temp1.sort_by_row_and_column(); //add does not sort
-    cusp::coo_matrix<int, double, cusp::host_memory> temp_ = temp2;
     /*
+    cusp::coo_matrix<int, double, cusp::host_memory> temp_ = temp2;
     t.tic();
     eigenpol.compute( xchi_matrix_view, temp_);
     t.toc();
