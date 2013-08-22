a51df240b00993f321622ec7e5adbbc7c30d71ab
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2013-08-22 18:46:15 +0200

    experimenting...

--- inc/dg/typedefs.cuh
+++ inc/dg/typedefs.cuh
@@ -6,9 +6,9 @@ typedef thrust::device_vector<double> DVec; //!< Device Vector
 typedef thrust::host_vector<double> HVec; //!< Host Vector
 
 typedef cusp::coo_matrix<int, double, cusp::host_memory> Matrix; //!< default matrix
-typedef cusp::ell_matrix<int, double, cusp::host_memory> HMatrix; //!< 
+typedef cusp::csr_matrix<int, double, cusp::host_memory> HMatrix; //!< 
 typedef cusp::ell_matrix<int, double, cusp::device_memory> DMatrix; //!< most efficient matrix format
-typedef cusp::csr_matrix<int, double, cusp::host_memory> CMatrix; //!< 
+//typedef cusp::csr_matrix<int, double, cusp::host_memory> CMatrix; //!< 
 
 }//namespace dg
 
