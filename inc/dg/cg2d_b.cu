a696be66634cb269a14d35584a0f654b12cacb84
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2013-10-19 12:05:26 +0200

    benchmarks

--- inc/dg/cg2d_b.cu
+++ inc/dg/cg2d_b.cu
@@ -36,7 +36,7 @@ int main()
     unsigned n, Nx, Ny; 
     std::cout << "Type n, Nx and Ny\n";
     std::cin >> n >> Nx >> Ny;
-    dg::Grid<double> grid( 0, lx, 0, ly, n, Nx, Ny, dg::DIR, dg::DIR);
+    dg::Grid<double> grid( 0, lx, 0, ly, n, Nx, Ny, dg::DIR, dg::PER);
     dg::S2D<double> s2d( grid);
     cout<<"Expand initial condition\n";
     dg::HVec x = dg::expand( initial, grid);
