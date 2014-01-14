fdd7c20ec53294f46b3f5e9faf7ba0293df13072
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2014-01-14 15:50:24 +0100

    dx_t

--- inc/dg/dx_t.cu
+++ inc/dg/dx_t.cu
@@ -32,7 +32,7 @@ int main ()
     cout << "# of cells          " << N <<"\n";
     Grid1d<double> g( 0, lx, n, N);
     const double hx = lx/(double)N;
-    cusp::ell_matrix< int, double, cusp::host_memory> hm = create::dx_asymm_mt<double>( n, N, hx, DIR);
+    cusp::ell_matrix< int, double, cusp::host_memory> hm = create::dx_symm<double>( n, N, hx, DIR);
     HVec hv = expand( function, g);
     HVec hw = hv;
     const HVec hu = expand( derivative, g);
