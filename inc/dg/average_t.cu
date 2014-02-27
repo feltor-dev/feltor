411763a69654306f9c9f521fd0270317019c06d8
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2014-02-27 10:12:25 +0100

    successful test of poloidal average operator

--- inc/dg/average_t.cu
+++ inc/dg/average_t.cu
@@ -2,9 +2,10 @@
 #include "average.cuh"
 
 
-const double lx = M_PI;
+const double lx = 2.*M_PI;
 const double ly = M_PI;
 double function( double x, double y) {return cos(x)*sin(y);}
+double pol_average( double x, double y) {return cos(x)*2./M_PI;}
 
 int main()
 {
@@ -15,13 +16,14 @@ int main()
 
     dg::PoloidalAverage<dg::HVec, thrust::host_vector<int> > pol(g);
 
-    dg::HVec vector = dg::evaluate( function ,g), vector_y( vector);
+    dg::HVec vector = dg::evaluate( function ,g), average_y( vector);
+    const dg::HVec solution = dg::evaluate( pol_average, g);
     dg::HVec w2d = dg::create::w2d( g);
     std::cout << "Integration is: "<<dg::blas1::dot( vector, w2d)<<std::endl;
 
-    pol( vector, vector_y);
-    dg::blas1::axpby( 1., vector, -1., vector_y, vector);
-    std::cout << "Result of integration is: "<<dg::blas1::dot( vector, w2d)<<std::endl;
+    pol( vector, average_y);
+    dg::blas1::axpby( 1., solution, -1., average_y, vector);
+    std::cout << "Distance to solution is: "<<sqrt(dg::blas2::dot( vector, w2d, vector))<<std::endl;
 
 
 
