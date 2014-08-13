b126f6c5ce09279794cf7e8e04062e7469a537bd
mheld <markus.held@uibk.ac.at>
2014-08-13 15:58:05 +0200

    changed a few geometric input files

--- src/feltor/feltor.cu
+++ src/feltor/feltor.cu
@@ -141,12 +141,12 @@ int main( int argc, char* argv[])
         //transform to Vor
 //         dg::blas2::gemv( rolkar.laplacianM(), feltor.potential()[0], y1[1]);
 //         hvisual = y1[1];
-        hvisual =feltor.potential()[0];
+        hvisual = feltor.potential()[0];
         dg::blas2::gemv( equi, hvisual, visual);
         colors.scalemax() = (float)thrust::reduce( visual.begin(), visual.end(), 0.,thrust::maximum<double>()  );
         colors.scalemin() =  (float)thrust::reduce( visual.begin(), visual.end(), colors.scalemax()  ,thrust::minimum<double>() );
 //         colors.scalemin() = -colors.scalemax();
-        title <<"Vor / "<<colors.scalemin()<<"  " << colors.scalemax()<<"\t";
+        title <<"Phi / "<<colors.scalemin()<<"  " << colors.scalemax()<<"\t";
         for( unsigned k=0; k<p.Nz/v2[2];k++)
         {
             unsigned size=grid.n()*grid.n()*grid.Nx()*grid.Ny();
