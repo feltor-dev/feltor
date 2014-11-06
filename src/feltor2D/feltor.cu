0e3256b175b93a3469d286b7002b8c2eae67d679
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2014-11-06 12:29:26 +0100

    window in feltor2D umgestellt

--- src/feltor2D/feltor.cu
+++ src/feltor2D/feltor.cu
@@ -57,8 +57,8 @@ int main( int argc, char* argv[])
     const solovev::GeomParameters gp(v3);
     gp.display( std::cout);
     v2 = file::read_input( "window_params.txt");
-    GLFWwindow* w = draw::glfwInitAndCreateWindow(  v2[1]*v2[4],(1)/v2[2]*v2[3], "");
-    draw::RenderHostData render( (1)/v2[2],v2[1]);
+    GLFWwindow* w = draw::glfwInitAndCreateWindow(  v2[2]*v2[3], v2[1]*v2[4], "");
+    draw::RenderHostData render( v2[1], v2[2]);
 
 
 
@@ -106,14 +106,16 @@ int main( int argc, char* argv[])
     dg::Karniadakis< std::vector<dg::DVec> > karniadakis( y0, y0[0].size(), p.eps_time);
     std::cout << "initialize karniadakis" << std::endl;
     karniadakis.init( feltor, rolkar, y0, p.dt);
+    feltor.energies(y0); //now energies and potential are at time 0
     std::cout << "Done!\n";
-    std::cout << "first karniadakis" << std::endl;
+    //std::cout << "first karniadakis" << std::endl;
 
-    karniadakis( feltor, rolkar, y0); //now energies and potential are at time 0
-    std::cout << "Done!\n";
+    //karniadakis( feltor, rolkar, y0); //now energies and potential are at time 0
+    //std::cout << "Done!\n";
+
 
     dg::DVec dvisual( grid.size(), 0.);
-    dg::HVec hvisual( grid.size(), 0.), visual(hvisual),avisual(hvisual);
+    dg::HVec hvisual( grid.size(), 0.), visual(hvisual);
     dg::HMatrix equi = dg::create::backscatter( grid);
     draw::ColorMapRedBlueExtMinMax colors(-1.0, 1.0);
 
@@ -130,7 +132,8 @@ int main( int argc, char* argv[])
     while ( !glfwWindowShouldClose( w ))
     {
 
-        hvisual = karniadakis.last()[0];
+        //hvisual = karniadakis.last()[0];
+        hvisual = y0[0];
         dg::blas2::gemv( equi, hvisual, visual);
         colors.scalemax() = (float)thrust::reduce( visual.begin(), visual.end(), 0., thrust::maximum<double>() );
         colors.scalemin() = -colors.scalemax();        
@@ -140,17 +143,12 @@ int main( int argc, char* argv[])
         title << std::setprecision(2) << std::scientific;
         //title <<"ne / "<<(float)thrust::reduce( visual.begin(), visual.end(), colors.scalemax()  ,thrust::minimum<double>() )<<"  " << colors.scalemax()<<"\t";
         title <<"ne-1 / " << colors.scalemax()<<"\t";
-        dg::blas1::axpby(0.0,avisual,0.0,avisual);
-        for( unsigned k=0; k<1/v2[2];k++)
-        {
-            unsigned size=grid.n()*grid.n()*grid.Nx()*grid.Ny();
-            dg::HVec part( visual.begin() + k*v2[2]*size, visual.begin()+(k*v2[2]+1)*size);
-            render.renderQuad( part, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
-        }
+        render.renderQuad( visual, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
 
         //draw ions
         //thrust::transform( y1[1].begin(), y1[1].end(), dvisual.begin(), dg::PLUS<double>(-0.));//ne-1
-        hvisual = karniadakis.last()[1];
+        //hvisual = karniadakis.last()[1];
+        hvisual = y0[1];
         dg::blas2::gemv( equi, hvisual, visual);
         colors.scalemax() = (float)thrust::reduce( visual.begin(), visual.end(), 0., thrust::maximum<double>() );
         //colors.scalemin() = 1.0;        
@@ -160,13 +158,7 @@ int main( int argc, char* argv[])
         title << std::setprecision(2) << std::scientific;
         //title <<"ni / "<<(float)thrust::reduce( visual.begin(), visual.end(), colors.scalemax()  ,thrust::minimum<double>() )<<"  " << colors.scalemax()<<"\t";
         title <<"ni-1 / " << colors.scalemax()<<"\t";
-        dg::blas1::axpby(0.0,avisual,0.0,avisual);
-        for( unsigned k=0; k<1/v2[2];k++)
-        {
-            unsigned size=grid.n()*grid.n()*grid.Nx()*grid.Ny();
-            dg::HVec part( visual.begin() + k*v2[2]*size, visual.begin()+(k*v2[2]+1)*size);
-            render.renderQuad( part, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
-        }
+        render.renderQuad( visual, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
 
         
         //draw potential
@@ -181,45 +173,29 @@ int main( int argc, char* argv[])
         colors.scalemin() = -colors.scalemax();
         //title <<"Phi / "<<colors.scalemin()<<"  " << colors.scalemax()<<"\t";
         title <<"phi / "<< colors.scalemax()<<"\t";
-        dg::blas1::axpby(0.0,avisual,0.0,avisual);
-        for( unsigned k=0; k<1/v2[2];k++)
-        {
-            unsigned size=grid.n()*grid.n()*grid.Nx()*grid.Ny();
-            dg::HVec part( visual.begin() + k*v2[2]*size, visual.begin()+(k*v2[2]+1)*size);
-            render.renderQuad( part, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
-        }
+        render.renderQuad( visual, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
 
         //draw U_e
-        hvisual = karniadakis.last()[2];
+        //hvisual = karniadakis.last()[2];
+        hvisual = y0[2];
         dg::blas2::gemv( equi, hvisual, visual);
         colors.scalemax() = (float)thrust::reduce( visual.begin(), visual.end(), 0.,thrust::maximum<double>()  );
         //colors.scalemin() =  (float)thrust::reduce( visual.begin(), visual.end(), colors.scalemax()  ,thrust::minimum<double>() );
         colors.scalemin() = -colors.scalemax();
         //title <<"Ue / "<<colors.scalemin()<<"  " << colors.scalemax()<<"\t";
         title <<"Ue / " << colors.scalemax()<<"\t";
-        dg::blas1::axpby(0.0,avisual,0.0,avisual);
-        for( unsigned k=0; k<1/v2[2];k++)
-        {
-            unsigned size=grid.n()*grid.n()*grid.Nx()*grid.Ny();
-            dg::HVec part( visual.begin() + k*v2[2]*size, visual.begin()+(k*v2[2]+1)*size);
-            render.renderQuad( part, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
-        }
+        render.renderQuad( visual, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
 
         //draw U_i
-        hvisual = karniadakis.last()[3];
+        //hvisual = karniadakis.last()[3];
+        hvisual = y0[3];
         dg::blas2::gemv( equi, hvisual, visual);
         colors.scalemax() = (float)thrust::reduce( visual.begin(), visual.end(), 0., thrust::maximum<double>()  );
         //colors.scalemin() =  (float)thrust::reduce( visual.begin(), visual.end(), colors.scalemax()  ,thrust::minimum<double>() );
         colors.scalemin() = -colors.scalemax();
         //title <<"Ui / "<<colors.scalemin()<< "  " << colors.scalemax()<<"\t";
         title <<"Ui / " << colors.scalemax()<<"\t";
-        dg::blas1::axpby(0.0,avisual,0.0,avisual);
-        for( unsigned k=0; k<1/v2[2];k++)
-        {
-            unsigned size=grid.n()*grid.n()*grid.Nx()*grid.Ny();
-            dg::HVec part( visual.begin() + k*v2[2]*size, visual.begin()+(k*v2[2]+1)*size);
-            render.renderQuad( part, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
-        }
+        render.renderQuad( visual, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
 
       
         //draw VOR
@@ -233,13 +209,7 @@ int main( int argc, char* argv[])
         colors.scalemin() = -colors.scalemax();
         //title <<"Phi / "<<colors.scalemin()<<"  " << colors.scalemax()<<"\t";
         title <<"vor / "<< colors.scalemax()<<"\t";
-        dg::blas1::axpby(0.0,avisual,0.0,avisual);
-        for( unsigned k=0; k<1/v2[2];k++)
-        {
-            unsigned size=grid.n()*grid.n()*grid.Nx()*grid.Ny();
-            dg::HVec part( visual.begin() + k*v2[2]*size, visual.begin()+(k*v2[2]+1)*size);
-            render.renderQuad( part, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
-        }
+        render.renderQuad( visual, grid.n()*grid.Nx(), grid.n()*grid.Ny(), colors);
         
         
         title << std::fixed; 
@@ -255,15 +225,6 @@ int main( int argc, char* argv[])
 #endif//DG_BENCHMARK
         for( unsigned i=0; i<p.itstp; i++)
         {
-            step++;
-            std::cout << "(m_tot-m_0)/m_0: "<< (feltor.mass()-mass0)/mass_blob0<<"\t";
-            E1 = feltor.energy();
-            diff = (E1 - E0)/p.dt; //
-            double diss = feltor.energy_diffusion( );
-            std::cout << "(E_tot-E_0)/E_0: "<< (E1-energy0)/energy0<<"\t";
-            std::cout << "Accuracy: "<< 2.*(diff-diss)/(diff+diss)<<" d E/dt = " << diff <<" Lambda =" << diss << "\n";
-            E0 = E1;
-
             try{ karniadakis( feltor, rolkar, y0);}
             catch( dg::Fail& fail) { 
                 std::cerr << "CG failed to converge to "<<fail.epsilon()<<"\n";
@@ -271,6 +232,15 @@ int main( int argc, char* argv[])
                 glfwSetWindowShouldClose( w, GL_TRUE);
                 break;
             }
+            step++;
+            feltor.energies(y0);//advance potential and energies
+            std::cout << "(m_tot-m_0)/m_0: "<< (feltor.mass()-mass0)/mass_blob0<<"\t";
+            E1 = feltor.energy();
+            diff = (E1 - E0)/p.dt; //
+            double diss = feltor.energy_diffusion( );
+            std::cout << "(E_tot-E_0)/E_0: "<< (E1-energy0)/energy0<<"\t";
+            std::cout << "Accuracy: "<< 2.*(diff-diss)/(diff+diss)<<" d E/dt = " << diff <<" Lambda =" << diss << "\n";
+            E0 = E1;
         }
         time += (double)p.itstp*p.dt;
 #ifdef DG_BENCHMARK
