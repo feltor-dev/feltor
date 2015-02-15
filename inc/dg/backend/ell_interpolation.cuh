76e133bda6761f4b985a1a7ccb56c0ec24f6d10e
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2015-02-15 16:04:46 +0100

    bug in cusp found

--- inc/dg/backend/ell_interpolation.cuh
+++ inc/dg/backend/ell_interpolation.cuh
@@ -25,8 +25,9 @@ void __host__ __device__ legendre( double* pxn, const double xn, const unsigned 
         double temp[4];
         for( unsigned k=0; k<n; k++)
         {
+            temp[k] = 0;
             for( unsigned i=0; i<n; i++)
-                temp[k] = pxn[i]*forward[i*n+k];
+                temp[k] += pxn[i]*forward[i*n+k];
         }
         for( unsigned k=0; k<n; k++)
             pxn[k] = temp[k];
@@ -66,7 +67,7 @@ template<size_t BLOCK_SIZE>
 __launch_bounds__(BLOCK_SIZE, 1) //cuda performance hint macro, (max_threads_per_block, minBlocksPerMultiprocessor)
  __global__ void interpolation_kernel2d( 
          const int num_rows, const int n, const int Nx, 
-         const int* celln, const int* cellm, 
+         const int* cellx, const int* celly, 
          const double* xn, const double * yn, 
          const int pitch, int* Aj, double* Av, 
          const double * forward)
@@ -82,6 +83,36 @@ __launch_bounds__(BLOCK_SIZE, 1) //cuda performance hint macro, (max_threads_per
         detail::legendre( py, yn[row], n, forward);
         
         int offset = row;
+        //the column is the row in the vector with which to multiply
+        int col_begin = celly[row]*Nx*n*n + cellx[row]*n;
+        //evaluate 2d Legendre polynomials at (xn)...
+        for( int k=0; k<n; k++)
+            for( int l=0; l<n; l++)
+            {
+                Aj[offset] = col_begin + k*n*Nx + l;
+                //Aj[offset] = k*n+l;
+                Av[offset] = py[k]*px[l];
+                offset +=pitch;
+                //offset += n*n;
+            }
+    }
+
+}
+ void interpolation_kernel2dcpu( 
+         const int num_rows, const int n, const int Nx, 
+         const int* celln, const int* cellm, 
+         const double* xn, const double * yn, 
+         const int pitch, int* Aj, double* Av, 
+         const double * forward)
+{
+    for( int row = 0; row<num_rows; row ++)
+    {
+        //evaluate 2d Legendre polynomials at (xn, yn)...
+        double px[4], py[4];
+        detail::legendre( px, xn[row], n, forward); 
+        detail::legendre( py, yn[row], n, forward);
+        
+        int offset = row;
         int col_begin = cellm[row]*Nx*n*n + celln[row]*n;
         //evaluate 2d Legendre polynomials at (xn)...
         for( int k=0; k<n; k++)
@@ -89,7 +120,10 @@ __launch_bounds__(BLOCK_SIZE, 1) //cuda performance hint macro, (max_threads_per
             {
                 Aj[offset] = col_begin + k*n*Nx + l;
                 Av[offset] = py[k]*px[l];
+                //std::cout << pitch <<" "<<offset<<"\n";
+                std::cout << row << " "<<Aj[offset] <<" "<<Av[offset]<<" "<<px[k]*px[l]<<"\n";
                 offset +=pitch;
+               // offset += num_rows;
             }
     }
 
@@ -110,7 +144,7 @@ cusp::ell_matrix<double, int, cusp::device_memory> ell_interpolation( const thru
     const size_t NUM_BLOCKS = std::min<size_t>( 
             MAX_BLOCKS, 
             cusp::system::cuda::DIVIDE_INTO( A.num_rows, BLOCK_SIZE));
-    const int pitch = A.column_indices.pitch;
+    const int pitch = A.column_indices.pitch; //ell has columen major format
     int* Aj = thrust::raw_pointer_cast(&A.column_indices(0,0));
     double * Av = thrust::raw_pointer_cast(&A.values(0,0));
 
@@ -132,10 +166,12 @@ cusp::ell_matrix<double, int, cusp::device_memory> ell_interpolation( const thru
 
 }
 
-cusp::ell_matrix<double, int, cusp::device_memory> ell_interpolation( const thrust::device_vector<double>& x, const thrust::device_vector<double>& y, const Grid2d<double>& g  )
+cusp::ell_matrix<int, double, cusp::host_memory> ell_interpolation( const thrust::host_vector<double>& x, const thrust::host_vector<double>& y, const Grid2d<double>& g  )
 {
+    assert( x.size() == y.size());
     //allocate ell matrix storage
-    cusp::ell_matrix<int, double, cusp::device_memory> A( x.size(), g.size(), x.size()*g.n()*g.n(), g.n()*g.n());
+    cusp::ell_matrix<int, double, cusp::host_memory> A( x.size(), g.size(), x.size()*g.n()*g.n(), g.n()*g.n());
+    std::cout << A.num_rows << " "<<A.num_cols<<" "<<A.num_entries<< " "<<A.column_indices.pitch<<"\n";
 
     //set up kernel parameters
     const size_t BLOCK_SIZE = 256;
@@ -143,33 +179,35 @@ cusp::ell_matrix<double, int, cusp::device_memory> ell_interpolation( const thru
     const size_t NUM_BLOCKS = std::min<size_t>( 
             MAX_BLOCKS, 
             cusp::system::cuda::DIVIDE_INTO( A.num_rows, BLOCK_SIZE));
-    const int pitch = A.column_indices.pitch;
+    const int pitch = A.column_indices.pitch; //# of cols in memory
+    assert( pitch == A.values.pitch);
     int* Aj = thrust::raw_pointer_cast(&A.column_indices(0,0));
     double * Av = thrust::raw_pointer_cast(&A.values(0,0));
 
     //compute normalized x and y values and cell numbers
-    thrust::device_vector<double> xn(x.size()), yn(y.size()), cellnh(x.size()), cellmh(x.size());
+    thrust::host_vector<double> xn(x.size()), yn(y.size()), cellnh(x.size()), cellmh(x.size());
     dg::blas1::transform( x, xn, dg::PLUS<double>( -g.x0()));
     dg::blas1::transform( y, yn, dg::PLUS<double>( -g.y0()));
     dg::blas1::scal( xn, 1./g.hx());
     dg::blas1::scal( yn, 1./g.hy());
-    thrust::device_vector<int> celln( x.size());
-    thrust::device_vector<int> cellm( x.size());
-    thrust::transform( xn.begin(), xn.end(), celln.begin(), dg::FLOOR());
-    thrust::transform( yn.begin(), yn.end(), cellm.begin(), dg::FLOOR());
-    thrust::transform( celln.begin(), celln.end(),cellnh.begin(), dg::PLUS<double>(0.5));
-    thrust::transform( cellm.begin(), cellm.end(),cellmh.begin(), dg::PLUS<double>(0.5));
-    const int* celln_ptr = thrust::raw_pointer_cast( &celln[0]);
-    const int* cellm_ptr = thrust::raw_pointer_cast( &cellm[0]);
+    thrust::host_vector<int> cellX( x.size());
+    thrust::host_vector<int> cellY( y.size());
+    thrust::transform( xn.begin(), xn.end(), cellX.begin(), dg::FLOOR());
+    thrust::transform( yn.begin(), yn.end(), cellY.begin(), dg::FLOOR());
+    thrust::transform( cellX.begin(), cellX.end(),cellnh.begin(), dg::PLUS<double>(0.5));
+    thrust::transform( cellY.begin(), cellY.end(),cellmh.begin(), dg::PLUS<double>(0.5));
+    const int* cellX_ptr = thrust::raw_pointer_cast( &cellX[0]);
+    const int* cellY_ptr = thrust::raw_pointer_cast( &cellY[0]);
     const double* xn_ptr = thrust::raw_pointer_cast( &xn[0]);
     const double* yn_ptr = thrust::raw_pointer_cast( &yn[0]);
     //xn = 2*xn - 2*(celln+0.5)
     dg::blas1::axpby( 2., xn, -2., cellnh, xn);
     dg::blas1::axpby( 2., yn, -2., cellmh, yn);
-    thrust::device_vector<double> forward(std::vector<double> ( g.dlt().forward()));
+    thrust::host_vector<double> forward(std::vector<double> ( g.dlt().forward()));
     const double * forward_ptr = thrust::raw_pointer_cast( &forward[0]);
-    detail::interpolation_kernel2d<BLOCK_SIZE> <<<NUM_BLOCKS, BLOCK_SIZE, 0, 0>>> ( A.num_rows, g.n(), g.Nx(), 
-            celln_ptr, cellm_ptr, 
+    detail::interpolation_kernel2dcpu( A.num_rows, g.n(), g.Nx(), 
+    //detail::interpolation_kernel2d<BLOCK_SIZE> <<<NUM_BLOCKS, BLOCK_SIZE, 0>>> ( A.num_rows, g.n(), g.Nx(), 
+            cellX_ptr, cellY_ptr, 
             xn_ptr, yn_ptr, pitch, Aj, Av, forward_ptr);
     return A;
 
--- inc/dg/backend/ell_interpolation_t.cu
+++ inc/dg/backend/ell_interpolation_t.cu
@@ -2,21 +2,24 @@
 #include "xspacelib.cuh"
 #include "ell_interpolation.cuh"
 #include "typedefs.cuh"
-const unsigned n = 2;
-const unsigned Nx = 2; 
-const unsigned Ny = 2; 
+const unsigned n = 3;
+const unsigned Nx = 1; 
+const unsigned Ny = 1; 
 const unsigned Nz = 2; 
 
 
 int main()
 {
 
-    {
     dg::Grid2d<double> g( -10, 10, -5, 5, n, Nx, Ny);
     dg::Matrix A = dg::create::backscatter( g);
     A.sort_by_row_and_column();
+    cusp::array1d<double, cusp::host_memory> hello( 10, 3);
+    for( unsigned i=0; i<hello.size(); i++)
+        hello[4] = 17;
+    cusp::print ( hello);
 
-    std::vector<double> x( g.size()), y(x);
+    thrust::host_vector<double> x( g.size()), y(x);
     for( unsigned i=0; i<g.Ny()*g.n(); i++)
         for( unsigned j=0; j<g.Nx()*g.n(); j++)
         {
@@ -26,19 +29,27 @@ int main()
                     g.y0() + (i+0.5)*g.hy()/(double)(g.n());
         }
     thrust::device_vector<double> xd(x), yd(y);
-    dg::DMatrix dB = dg::create::ell_interpolation( xd,yd, g);
-    dg::Matrix B = dB;
+    cusp::ell_matrix<int, double, cusp::host_memory> dA = A;
+    cusp::coo_matrix<int, double, cusp::host_memory> C = dA;
+    cusp::ell_matrix<int, double, cusp::host_memory> dB = dg::create::ell_interpolation( xd, yd, g);
+    cusp::coo_matrix<int, double, cusp::host_memory> B = dB;
+    cusp::print(C);
+    cusp::print(A);
     bool passed = true;
-    for( unsigned i=0; i<A.values.size(); i++)
+    for( unsigned i=0; i<C.values.size(); i++)
     {
-        if( (A.values[i] - B.values[i]) > 1e-14)
+        if( (C.values[i] - B.values[i]) > 1e-14)
         {
-            std::cerr << "NOT EQUAL "<<A.row_indices[i] <<" "<<A.column_indices[i]<<" "<<A.values[i] << "\t "<<B.row_indices[i]<<" "<<B.column_indices[i]<<" "<<B.values[i]<<"\n";
+            std::cerr << "NOT EQUCL "<<C.row_indices[i] <<" "<<C.column_indices[i]<<" "<<C.values[i] << "\t "<<B.row_indices[i]<<" "<<B.column_indices[i]<<" "<<B.values[i]<<"\n";
             passed = false;
         }
     }
+    if( C.num_entries != B.num_entries)
+    {
+        std::cerr << "Number of entries not equal!\n";
+        passed = false;
+    }
     if( passed)
         std::cout << "2D TEST PASSED!\n";
-    }
     return 0;
 }
