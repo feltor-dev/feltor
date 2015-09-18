4f7f57e85e138fa650bdaa1e458f57dd5f177178
Matthias <Matthias.Wiesenberger@uibk.ac.at>
2015-09-18 15:56:03 +0200

    updated Makefile in diag directory

--- config/default.mk
+++ config/default.mk
@@ -2,7 +2,7 @@ ifndef INCLUDED #include guard
 INCLUDED=1
 
 #default machine values
-INCLUDE = -I$(HOME)/include  # cusp and thrust libraries
+INCLUDE = -I$(HOME)/include  # cusp and thrust and the draw libraries
 GLFLAGS =$$(pkg-config --static --libs glfw3) #glfw3 installation
 CC=g++ #C++ compiler
 MPICC=mpic++  #mpi compiler
@@ -10,4 +10,4 @@ OPT=-O3 # optimization flag
 NVCCARCH=-arch=sm_20 #nvcc gpu compute capability
 OMPFLAG=-fopenmp #openmp flag for CC and MPICC
 LIBS=-lnetcdf -lhdf5 -lhdf5_hl # netcdf library for file output
-endif
+endif # INCLUDED
