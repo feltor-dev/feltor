dde98ea8d020aec11275e5ccaefcc87c3400287d
Matthias <Matthias.Wiesenberger@uibk.ac.at>
2016-03-10 16:01:31 +0100

    removed viennacl references

--- inc/dg/backend/vector_categories.h
+++ inc/dg/backend/vector_categories.h
@@ -15,7 +15,6 @@ struct StdVectorTag:public AnyVectorTag {};
  */
 struct ThrustVectorTag: public AnyVectorTag {};
 
-struct ViennaCLVectorTag: public AnyVectorTag {};
 struct CuspVectorTag: public ThrustVectorTag {};
 
 struct MPIVectorTag: public AnyVectorTag{};
