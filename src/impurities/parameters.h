b34337c3d10434b0a4dcf2b68613ea9126544ece
Matthias <Matthias.Wiesenberger@uibk.ac.at>
2016-04-06 16:32:27 +0200

    updated impurity equations

--- src/impurities/parameters.h
+++ src/impurities/parameters.h
@@ -1,100 +1,72 @@
-#ifndef _DG_PARAMETERS_ 
-#define _DG_PARAMETERS_
+
+#pragma once
 #include "dg/enums.h"
 
+namespace imp
+{
 /**
  * @brief Provide a mapping between input file and named parameters
  */
 struct Parameters
 {
-    unsigned n, k, Nx, Ny; 
+    unsigned n, Nx, Ny; 
     double dt; 
 
     double eps_pol, eps_gamma;
+
     double lx, ly; 
     enum dg::bc bc_x, bc_y;
 
-    int global;
-    double nu, kappa, tau;
+    double nu, kappa;
 
-    double a_z, mu_z, tau_z;
+    double a[3], mu[3], tau[3];
 
-    double n0, sigma, posX, posY;
+    double amp, sigma, posX, posY;
 
     unsigned itstp; 
     unsigned maxout;
+
+    unsigned mode;
+    unsigned wall_pos, wall_amp, wall_sigma;
 
     /**
      * @brief constructor to make a const object
      *
      * @param v Vector from read_input function
      */
-    Parameters( const std::vector< double>& v, int layout = 0) {
-        layout_ = layout;
-        if( layout == 2) 
-        {
-            a_z = v[22];
-            mu_z = v[23];
-            tau_z = v[24];
-            layout = 0;
-        }
-        if( layout == 0)
-        {
-            n  = (unsigned)v[1]; 
-            Nx = (unsigned)v[2];
-            Ny = (unsigned)v[3];
-            k  = (unsigned)v[4];
-            dt = v[5];
-            eps_pol = v[6];
-            eps_gamma = v[7];
-            lx = v[8]; 
-            ly = v[9];
-            bc_x = map((int)v[10]), bc_y = map((int)v[11]);
-            global = v[12];
-            nu = v[13];
-            kappa = v[14];
-            tau = v[15]; 
-            n0 = v[16];
-            sigma = v[17];
-            posX = v[18];
-            posY = v[19];
-            itstp = v[20];
-            maxout = v[21];
-        }
-        else if( layout == 1||layout == 3)
-        {
-            n = 1;
-            if( layout == 1)
-            {
-                Nx = (unsigned)v[1]/v[25];
-                Ny = (unsigned)v[2]/v[25]; //reduction parameter v[25]!
-            }
-            else 
-            {
-                Nx = (unsigned)v[1];
-                Ny = (unsigned)v[2]; 
-            }
-                
-            k = 3;
-            dt = v[3];
-            eps_pol = 1e-6;
-            eps_gamma = 1e-10;
-            ly = v[4];
-            lx = ly/(double)Ny*(double)Nx;
-            bc_x = bc_y = dg::PER;
-            bc_x = map((int)v[5]);
-            global = 0;
-            nu = v[8];
-            kappa = v[9];
-            tau = v[12];
-            n0 = v[10];
-            sigma = v[21];
-            posX = v[23];
-            posY = v[24];
-            itstp = v[19];
-            maxout = v[22];
-        }
-        else ;
+    Parameters( const std::vector< double>& v) {
+
+        n  = (unsigned)v[1]; 
+        Nx = (unsigned)v[2];
+        Ny = (unsigned)v[3];
+        dt = v[4];
+        eps_pol = v[5];
+        eps_gamma = v[6];
+        lx = v[7]; 
+        ly = v[8];
+        bc_x = map((int)v[9]), bc_y = map((int)v[10]);
+        nu = v[11];
+        kappa = v[12];
+        tau[1] = v[13]; 
+        amp = v[14];
+        sigma = v[15];
+        posX = v[16];
+        posY = v[17];
+        itstp = v[18];
+        maxout = v[19];
+
+        a[2] = v[20];
+        mu[2] = v[21];
+        tau[2] = v[22];
+
+        a[0] = -1, a[1] = 1-a[2];
+        mu[0] = 0, mu[1] = 1;
+        tau[0] = -1;
+        mode = v[23];
+        wall_pos = v[24];
+        wall_amp = v[25];
+        wall_sigma = v[26];
+
     }
     /**
      * @brief Display parameters
@@ -106,18 +78,10 @@ struct Parameters
         os << "Physical parameters are: \n"
             <<"    Viscosity:       = "<<nu<<"\n"
             <<"    Curvature_y:     = "<<kappa<<"\n"
-            <<"    Ion-temperature: = "<<tau<<"\n";
-        if( layout_ == 2)
-        {
-            os <<"    a_z   = "<<a_z<<"\n"
-               <<"    mu_z  = "<<mu_z<<"\n"
-               <<"    tau_z = "<<tau_z<<"\n";
-        }
-        char local[] = "LOCAL" , glo[] = "GLOBAL";
-        os  <<"Mode is:   \n"
-            <<"    "<<(global?glo:local)<<global<<"\n";
-        //char per[] = "PERIODIC", dir[] = "DIRICHLET", neu[] = "NEUMANN";
-        //char dir_neu[] = "DIR_NEU", neu_dir[] = "NEU_DIR";
+            <<"    Ion-temperature: = "<<tau[1]<<"\n";
+        os <<"    a_z   = "<<a[2]<<"\n"
+           <<"    mu_z  = "<<mu[2]<<"\n"
+           <<"    tau_z = "<<tau[2]<<"\n";
         os << "Boundary parameters are: \n"
             <<"    lx = "<<lx<<"\n"
             <<"    ly = "<<ly<<"\n";
@@ -126,7 +90,6 @@ struct Parameters
             <<"    n  = "<<n<<"\n"
             <<"    Nx = "<<Nx<<"\n"
             <<"    Ny = "<<Ny<<"\n"
-            <<"    k  = "<<k<<"\n"
             <<"    dt = "<<dt<<"\n";
         os  <<"Blob parameters are: \n"
             << "    width:        "<<sigma<<"\n"
@@ -139,7 +102,6 @@ struct Parameters
             <<"Number of outputs:       "<<maxout<<std::endl; //the endl is for the implicit flush 
     }
     private:
-    int layout_;
     dg::bc map( int i)
     {
         switch( i)
@@ -185,8 +147,8 @@ struct Parameters
         os <<"\n";
     }
 };
+}//namespace imp
 
 
     
 
-#endif//_DG_PARAMETERS_
--- src/impurities/toeflI.cuh
+++ src/impurities/toeflI.cuh
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "dg/backend/xspacelib.cuh"
 #include "dg/algorithm.h"
 
 #ifdef DG_BENCHMARK
@@ -18,7 +17,7 @@ namespace dg
 template<class Geometry, class Matrix, class container>
 struct Rolkar
 {
-    Rolkar( const Geometry& g, eule::Parameters p):
+    Rolkar( const Geometry& g, imp::Parameters p):
         p(p),
         temp( dg::evaluate(dg::zero, g)),
         LaplacianM_perp ( g,g.bcx(),g.bcy(), dg::normed, dg::centered)
@@ -26,9 +25,9 @@ struct Rolkar
     }
     void operator()( std::vector<container>& x, std::vector<container>& y)
     {
-        /* x[0] := N_e - (bgamp+profamp)
-           x[1] := N_i - (bgamp+profamp)
-           x[2] := N_j - (bgamp+profamp)
+        /* x[0] := N_e - 1
+           x[1] := N_i - 1
+           x[2] := N_j - 1
 
         */
         dg::blas1::axpby( 0., x, 0, y);
@@ -37,7 +36,7 @@ struct Rolkar
             //not linear any more (cannot be written as y = Ax)
             dg::blas2::gemv( LaplacianM_perp, x[i], temp);
             dg::blas2::gemv( LaplacianM_perp, temp, y[i]);
-            dg::blas1::scal( y[i], -p.nu_perp);  //  nu_perp lapl_RZ (lapl_RZ N) 
+            dg::blas1::scal( y[i], -p.nu);  //  nu_perp lapl_RZ (lapl_RZ N) 
         }
 
 
@@ -61,13 +60,9 @@ struct ToeflI
      * @brief Construct a ToeflI solver object
      *
      * @param g The grid on which to operate
-     * @param kappa The curvature
-     * @param nu The artificial viscosity
-     * @param tau The ion temperature
-     * @param eps_pol stopping criterion for polarization equation
-     * @param eps_gamma stopping criterion for Gamma operator
+     * @param p the parameters
      */
-    ToeflI( const Geometry& g, double kappa, double nu, double tau, double a_z, double mu_z, double tau_z, double eps_pol, double eps_gamma);
+    ToeflI( const Geometry& g, imp::Parameters p);
 
 
     /**
@@ -119,8 +114,6 @@ struct ToeflI
     double energy_diffusion( ){ return ediff_;}
 
   private:
-    //use chi and omega as helpers to compute square velocity in omega
-    const container& compute_vesqr( container& potential);
     //extrapolates and solves for phi[1], then adds square velocity ( omega)
     const container& compute_psi( container& potential, int idx);
     const container& polarization( const std::vector<container>& y);
@@ -128,9 +121,9 @@ struct ToeflI
     container chi, omega;
     const container binv; //magnetic field
 
-    std::vector<container> phi, phi_old, dyphi;
-    std::vector<container> expy, dxy, dyy, lapy;
-    std::vector<container> gamma_n, gamma_old;
+    std::vector<container> phi, dyphi;
+    std::vector<container> dyy, lapy;
+    std::vector<container> gammp.an;
 
     //matrices and solvers
     Helmholtz< Geometry, Matrix, container > gamma1;
@@ -139,68 +132,43 @@ struct ToeflI
     dg::Invert<container> invert_pol, invert_invgamma;
 
     const container w2d, v2d, one;
-    const double eps_pol, eps_gamma; 
-    const double kappa, nu;
-    double a_[3], mu_[3], tau_[3];
 
     double mass_, energy_, diff_, ediff_;
+
+    imp::Parameters p;
+
 
 };
 
 template< class Geometry, class Matrix, class container>
-ToeflI< Geometry, Matrix, container>::ToeflI( const Grid2d<value_type>& grid,  
+ToeflI< Geometry, Matrix, container>::ToeflI( const Grid2d<value_type>& grid, imp::Parameters p) :
     chi( grid.size(), 0.), omega(chi),  
     binv( evaluate( LinearX( kappa, 1.), grid)), 
-    phi( 3, chi), phi_old( phi), dyphi( phi),
-    gamma_n( 2, chi), gamma_old( gamma_n),
-    expy( phi), dxy( expy), dyy( dxy), lapy( dyy),
-    gamma1(  grid, -0.5*tau_i),
+    phi( 3, chi), dyphi( phi),
+    gammp.an( 2, chi),
+    dyy( 3, chi), lapy( dyy),
+    gamma1(  grid, -0.5*p.taui),
     arakawa( grid), 
     pol(     grid, not_normed, centered), 
     invert_pol(      omega, omega.size(), p.eps_pol),
     invert_invgamma( omega, omega.size(), p.eps_gamma),
-    w2d( create::weights(grid)), v2d( create::inv_weights(grid)), one( dg::evaluate(dg::one, grid)),
-    eps_pol(eps_pol), eps_gamma( eps_gamma), kappa(kappa), nu(nu)
-{
-    tau_[0] = -1; 
-    tau_[1] = tau_i;
-    tau_[2] = tau_z;
-    double a_i = 1-a_z, mu_i = 1.;
-    a_[0] = 1., a_[1] = a_i, a_[2] = a_z;
-    mu_[0] = 0., mu_[1] = mu_i, mu_[2] = mu_z;
-    //std::cout << a_[0]<<" "<<a_[1]<<" "<<a_[2]<<"\n";
-    //std::cout << mu_[0]<<" "<<mu_[1]<<" "<<mu_[2]<<"\n";
-    //std::cout << tau_[0]<<" "<<tau_[1]<<" "<<tau_[2]<<"\n";
-    //std::cin >> tau_z;
-    //create derivatives
+    w2d( create::weights(grid)), v2d( create::inv_weights(grid)), one( dg::evaluate(dg::one, grid)), p(p)
+    { 
+    }
 
-}
-
-template< class container>
-const container& ToeflI<container>::compute_vesqr( container& potential)
-{
-    blas2::gemv( arakawa.dx(), potential, chi);
-    blas2::gemv( arakawa.dy(), potential, omega);
-    blas1::pointwiseDot( binv, chi, chi);
-    blas1::pointwiseDot( binv, omega, omega);
-    blas1::pointwiseDot( chi, chi, chi);
-    blas1::pointwiseDot( omega, omega, omega);
-    blas1::axpby( 1., chi, 1.,  omega);
-    return omega;
-}
 
 //idx is impurity species one or two
 template< class container>
 const container& ToeflI<container>::compute_psi( container& potential, int idx)
 {
-    gamma1.alpha() = -0.5*tau_[idx]*mu_[idx];
+    gamma1.alpha() = -0.5*p.tau[idx]*p.mu[idx];
     invert_invgamma( gamma1, phi[idx], potential);
 
     arakawa.variation(potential, omega);
     dg::blas1::pointwiseDot( binv, omega, omega);
     dg::blas1::pointwiseDot( binv, omega, omega);
 
-    dg::blas1::axpby( 1., chi, -0.5*mu_[idx], omega, phi[1]);             //psi  Gamma phi - 0.5 u_E^2
+    dg::blas1::axpby( 1., chi, -0.5*p.mu[idx], omega, phi[1]);             //psi  Gamma phi - 0.5 u_E^2
     return phi[idx];    
 }
 
@@ -208,27 +176,27 @@ const container& ToeflI<container>::compute_psi( container& potential, int idx)
 template<class G, class Matrix, class container>
 container& Feltor<G, Matrix, container>::polarisation( const std::vector<container>& y)
 { 
-    //\chi = a_i \mu_i n_i + a_s \mu_s n_s
-    blas1::axpby( a_[1]*mu_[1], y[1], 0., chi); 
-    blas1::axpby( a_[2]*mu_[2], y[2], 1., chi);
-    dg::blas1::plus( chi, ( a_[1]*mu_[1]+a_[2]*mu_[2]); 
+    //\chi = p.ai \p.mui n_i + p.as \p.mus n_s
+    blas1::axpby( p.a[1]*p.mu[1], y[1], 0., chi); 
+    blas1::axpby( p.a[2]*p.mu[2], y[2], 1., chi);
+    dg::blas1::plus( chi, ( p.a[1]*p.mu[1]+p.a[2]*p.mu[2]); 
     dg::blas1::pointwiseDot( chi, binv, chi);
-    dg::blas1::pointwiseDot( chi, binv, chi);       //(\mu_i n_i ) /B^2
-    pol.set_chi( chi);                              //set chi of polarisation: nabla_perp (chi nabla_perp )
+    dg::blas1::pointwiseDot( chi, binv, chi);       //(\p.mui n_i ) /B^2
+    pol.set_chi( chi);                              //set chi of polarisation: nablp.aperp (chi nablp.aperp )
 
 
 
 
-    gamma1.alpha() = -0.5*tau_[1]*mu_[1];
-    invert_invgamma( gamma1, gamma_n[0], y[1]);
-    gamma1.alpha() = -0.5*tau_[2]*mu_[2];
-    invert_invgamma( gamma1, gamma_n[1], y[2]);
+    gamma1.alpha() = -0.5*p.tau[1]*p.mu[1];
+    invert_invgamma( gamma1, gammp.an[0], y[1]);
+    gamma1.alpha() = -0.5*p.tau[2]*p.mu[2];
+    invert_invgamma( gamma1, gammp.an[1], y[2]);
 
     dg::blas1::axpby( 1., y[0], 0., chi);
-    dg::blas1::axpby( -a_[1], gamma_n[0], 1., chi);
-    dg::blas1::axpby( -a_[2], gamma_n[1], 1., chi);
+    dg::blas1::axpby( -p.a[1], gammp.an[0], 1., chi);
+    dg::blas1::axpby( -p.a[2], gammp.an[1], 1., chi);
 
-    unsigned number = invert_pol( pol, phi[0], chi);//a_jGamma n_j + a_iGamma n_i -ne = -nabla chi nabla phi
+    unsigned number = invert_pol( pol, phi[0], chi);//p.ajGamma n_j + p.aiGamma n_i -ne = -nabla chi nabla phi
     if(  number == invert_pol.get_max())
         throw dg::Fail( p.eps_pol);
     return phi[0];
@@ -254,10 +222,10 @@ void ToeflI< container>::operator()(std::vector<container>& y, std::vector<conta
         exp( y, expy);
         mass_ = blas2::dot( one, w2d, expy[0] ); //take real ion density which is electron density!!
         double Ue = blas2::dot( y[0], w2d, expy[0]);
-        double Ui = a_[1]*tau_[1]*blas2::dot( y[1], w2d, expy[1]);
-        double Uz = a_[2]*tau_[2]*blas2::dot( y[2], w2d, expy[2]);
-        double Uphii = 0.5*a_[1]*mu_[1]*blas2::dot( expy[1], w2d, omega); 
-        double Uphiz = 0.5*a_[2]*mu_[2]*blas2::dot( expy[2], w2d, omega); 
+        double Ui = p.a[1]*p.tau[1]*blas2::dot( y[1], w2d, expy[1]);
+        double Uz = p.a[2]*p.tau[2]*blas2::dot( y[2], w2d, expy[2]);
+        double Uphii = 0.5*p.a[1]*p.mu[1]*blas2::dot( expy[1], w2d, omega); 
+        double Uphiz = 0.5*p.a[2]*p.mu[2]*blas2::dot( expy[2], w2d, omega); 
         energy_ = Ue + Ui + Uphii + Uphiz;
 
         for( unsigned i=0; i<y.size(); i++)
@@ -269,12 +237,12 @@ void ToeflI< container>::operator()(std::vector<container>& y, std::vector<conta
         double Gi[3];
         Gi[0] = - blas2::dot( one, w2d, lapy[0]) - blas2::dot( lapy[0], w2d, y[0]); // minus 
         for( unsigned i=1; i<3; i++)
-            Gi[i] = - tau_[i]*(blas2::dot( one, w2d, lapy[i]) + blas2::dot( lapy[i], w2d, y[i])); // minus 
+            Gi[i] = - p.tau[i]*(blas2::dot( one, w2d, lapy[i]) + blas2::dot( lapy[i], w2d, y[i])); // minus 
         double Gphi[3];
         for( unsigned i=0; i<3; i++)
             Gphi[i] = -blas2::dot( phi[i], w2d, lapy[i]);
         //std::cout << "ge "<<Ge<<" gi "<<Gi<<" gphi "<<Gphi<<" gpsi "<<Gpsi<<"\n";
-        ediff_ = nu*( Gi[0] - Gphi[0] + a_[1]*(Gi[1] + Gphi[1]) + a_[2]*( Gi[2] + Gphi[2]));
+        ediff_ = nu*( Gi[0] - Gphi[0] + p.a[1]*(Gi[1] + Gphi[1]) + p.a[2]*( Gi[2] + Gphi[2]));
         */
 
     for( unsigned i=0; i<y.size(); i++)
@@ -292,7 +260,7 @@ void ToeflI< container>::operator()(std::vector<container>& y, std::vector<conta
         blas1::pointwiseDot( dyphi[i], ype[i], dyphi[i]);
         blas1::axpby( kappa, dyphi[i], 1., yp[i]);
 
-        blas1::axpby( tau_[i]*kappa, dyy[i], 1., yp[i]);
+        blas1::axpby( p.tau[i]*kappa, dyy[i], 1., yp[i]);
     }
 }
 
