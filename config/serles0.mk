dd973b3b656e6b26d5301ed8d60a1b29b9d6c1e7
Eduard Reiter <eduard.reiter@uibk.ac.at>
2016-04-18 11:45:30 +0200

    config for serles0 working, not clean

---
+++ config/serles0.mk
@@ -0,0 +1,12 @@
+ifeq ($(strip $(shell hostname)),serles0)    	# uniquely identify system
+INCLUDE  = -I$(HOME)/include
+INCLUDE += -I/usr/include/hdf5/serial          	# cusp and thrust libraries
+GLFLAGS =$$(pkg-config --static --libs glfw3)	# flags for glfw3
+CC=gcc#                                         # the host c++ compiler
+CFLAGS = -D_FORCE_INLINES			#
+MPICC=mpic++#                                   # the mpi compiler
+OPT=-O3#                                        # the optimization flag for the host
+NVCCARCH=-arch=sm_20#                           # nvcc gpu compute capability
+OMPFLAG=-fopenmp#                               # openmp flag for CC and MPICC
+LIBS=-lnetcdf -L/usr/include/hdf5/serial -lhdf5 -lhdf5_hl#                 # netcdf library for file output
+endif
