8f8d6e354d0cedbf80652dfd81e6107479184d0a
Eduard Reiter <eduard.reiter@uibk.ac.at>
2016-04-19 17:08:20 +0200

    e3r-x240 config for mpi

--- config/e3r-x240.mk
+++ config/e3r-x240.mk
@@ -1,7 +1,9 @@
 ifeq ($(strip $(shell hostname)),e3r-x240)   # uniquely identify system
 CC=g++                                       # the host c++ compiler
+MPICC = mpic++
+MPICFLAGS+= -DMPICH_IGNORE_CXX_SEEK
 OPT=-O3                                      # the optimization flag for the host
 OMPFLAG=-fopenmp                             # openmp flag for CC and MPICC
-LIBS=-lnetcdf -lhdf5 -lhdf5_hl               # netcdf library for file output
+LIBS=-lnetcdf -lcurl -lhdf5 -lhdf5_hl               # netcdf library for file output
 GLFLAGS = -lglfw -lGL
 endif
