42d7ffc1f8b3bb31bf943f3ae74931bbc037502a
Matthias <Matthias.Wiesenberger@uibk.ac.at>
2016-06-22 11:49:27 +0200

    changed arch back to 35

--- config/c722-pc161.mk
+++ config/c722-pc161.mk
@@ -7,7 +7,7 @@ GLFLAGS =$$(pkg-config --static --libs glfw3)#  # flags for glfw3
 CC=g++#                                         # the host c++ compiler
 MPICC=mpic++#                                   # the mpi compiler
 OPT=-O3#                                        # the optimization flag for the host
-NVCCARCH=-arch=sm_20#                           # nvcc gpu compute capability
+NVCCARCH=-arch=sm_35#                           # nvcc gpu compute capability
 OMPFLAG=-fopenmp#                               # openmp flag for CC and MPICC
 LIBS=-lnetcdf -lhdf5 -lhdf5_hl#                 # netcdf library for file output
 JSONLIB=-L$(HOME)/include/json/../../src/lib_json -ljsoncpp # json library for input parameters
