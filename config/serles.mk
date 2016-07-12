4dfbb2195ac7915fb571a9f7db6bb82d3ed7226e
Eduard Reiter <eduard.reiter@uibk.ac.at>
2016-07-12 17:02:32 +0200

    serles.mk not working

--- config/serles.mk
+++ config/serles.mk
@@ -1,13 +1,19 @@
 HOST = $(strip $(shell hostname))
 ifeq ($(HOST),$(filter $(HOST),serles0 serles1))       # uniquely identify system 
-INCLUDE  = -I$(HOME)/include
-INCLUDE += -I/usr/include/hdf5/serial          	# cusp and thrust libraries
-GLFLAGS =$$(pkg-config --static --libs glfw3)	# flags for glfw3
-CC=gcc#                                         # the host c++ compiler
-CFLAGS = -D_FORCE_INLINES			#
-MPICC=mpic++#                                   # the mpi compiler
-OPT=-O3#                                        # the optimization flag for the host
-NVCCARCH=-arch=sm_20#                           # nvcc gpu compute capability
-OMPFLAG=-fopenmp#                               # openmp flag for CC and MPICC
-LIBS=-lnetcdf -L/usr/include/hdf5/serial -lhdf5 -lhdf5_hl#                 # netcdf library for file output
+INCLUDE  = -I$(HOME)/include  # locally installed libs like cusp and the draw libraries
+INCLUDE += -I/usr/local/netcdf/include # parallel version of the netcdf library
+INCLUDE += -I/usr/local/json/include/jsoncpp          	# cusp and thrust libraries
+GLFLAGS =$$(pkg-config --static --libs glfw3) #glfw3 installation
+CC=g++ #C++ compiler
+MPICC=mpic++  #mpi compiler
+OPT=-O3 # optimization flag
+NVCCARCH=-arch sm_20 #nvcc gpu compute capability
+OMPFLAG=-fopenmp #openmp flag for CC and MPICC
+LIBS  = -lmpi -lcurl
+LIBS += -L/usr/lib/x86_64-linux-gnu/hdf5/openmpi -lhdf5 -lhdf5_hl  # parallel hdf5 library (debian repo)
+LIBS += -L/usr/local/netcdf/lib -lnetcdf # parallel version of the netcdf library
+CFLAGS+=-D_FORCE_INLINES
+MPICFLAGS+=-D_FORCE_INLINES
+NVCCFLAGS+=-D_FORCE_INLINES # workaround for bug in cuda 7.5 in conjunction with string.h of glibc 2.23
+JSONLIB = -L/usr/local/json/lib/x86_64-linux-gnu -ljsoncpp
 endif
