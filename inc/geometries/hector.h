a38e622bacde2331e86e62084da7d9e9ff9afba1
Matthias <Matthias.Wiesenberger@uibk.ac.at>
2016-09-15 14:30:06 +0200

    hector compiles with new algorithm

--- inc/geometries/hector.h
+++ inc/geometries/hector.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <vector>
 #include "dg/backend/grid.h"
+#include "dg/backend/interpolation.cuh"
 #include "dg/elliptic.h"
 #include "orthogonal.h"
 
@@ -8,80 +10,302 @@
 
 namespace hector
 {
+namespace detail
+{
+
+//interpolate the two components of a vector field
+struct Interpolate
+{
+    Interpolate( const thrust::host_vector<double>& fZeta, 
+                 const thrust::host_vector<double>& fEta, 
+                 const dg::Grid2d<double>& g2d ): 
+        iter0_( dg::create::forward_transform( fZeta, g2d) ), 
+        iter1_( dg::create::forward_transform(  fEta, g2d) ), 
+        g_(g2d){}
+    void operator()(const thrust::host_vector<double>& zeta, thrust::host_vector<double>& fZeta)
+    {
+
+        fZeta[0] = interpolate( zeta[0], zeta[1], iter0_, g_);
+        fZeta[1] = interpolate( zeta[0], zeta[1], iter1_, g_);
+    }
+    void operator()(const std::vector<thrust::host_vector<double> >& zeta, std::vector< thrust::host_vector<double> >& fZeta)
+    {
+        for( unsigned i=0; i<zeta[0].size(); i++)
+        {
+            fZeta[0][i] = interpolate( zeta[0][i], zeta[1][i], iter0_, g_);
+            fZeta[1][i] = interpolate( zeta[0][i], zeta[1][i], iter1_, g_);
+        }
+    }
+    private:
+    dg::Grid2d<double> g_;
+    thrust::host_vector<double> iter0_;
+    thrust::host_vector<double> iter1_;
+};
+
+
+//compute c_0 
+double construct_c0( const thrust::host_vector<double>& etaVinv, const dg::Grid2d<double>& g2d) 
+{
+    Interpolate inter( thrust::host_vector<double>( etaVinv.size(), 0), etaVinv, g2d);
+    thrust::host_vector<double> begin( 2, 0), end(begin), end_old(begin);
+    begin[0] = 0, begin[1] = 0;
+    double eps = 1e10, eps_old = 2e10;
+    unsigned N = 50;
+    while( (eps < eps_old || eps > 1e-7)&& eps > 1e-14)
+    {
+        eps_old = eps, end_old = end;
+        N*=2; dg::stepperRK17( inter, begin, end, 0., 2*M_PI, N);
+        eps = fabs( end[1]-end_old[1]);
+    }
+    std::cout << "\t error "<<eps<<" with "<<N<<" steps\n";
+    //std::cout <<end_old[2] << " "<<end[2] << "error in y is "<<y_eps<<"\n";
+    double f_psi = 2.*M_PI/end_old[1];
+    return f_psi;
+}
+
+
+//compute the vector of zeta and eta - values that form first v surface
+void compute_zev( 
+        const thrust::host_vector<double>& etaV,
+        const thrust::host_vector<double>& v_vec,
+        thrust::host_vector<double>& eta, 
+        const dg::Grid2d<double>& g2d
+        ) 
+{
+    Interpolate iter( thrust::host_vector<double>( etaV.size(), 0), etaV, g2d);
+    eta.resize( v_vec.size());
+    thrust::host_vector<double> eta_old(v_vec.size(), 0), eta_diff( eta_old);
+    thrust::host_vector<double> begin( 2, 0), end(begin), temp(begin);
+    begin[0] = 0, begin[1] = 0;
+    unsigned steps = 1;
+    double eps = 1e10, eps_old=2e10;
+    while( (eps < eps_old||eps > 1e-7) && eps > 1e-14)
+    {
+        //begin is left const
+        eps_old = eps, eta_old = eta;
+        dg::stepperRK17( iter, begin, end, 0, v_vec[0], steps);
+        eta[0] = end[1];
+        for( unsigned i=1; i<v_vec.size(); i++)
+        {
+            temp = end;
+            dg::stepperRK17( iter, temp, end, v_vec[i-1], v_vec[i], steps);
+            eta[i] = end[1];
+        }
+        temp = end;
+        //dg::stepperRK17( iter, temp, end, v_vec[v_vec.size()-1], 2.*M_PI, steps);
+        //compute error in R,Z only
+        dg::blas1::axpby( 1., eta, -1., eta_old, eta_diff);
+        double er = dg::blas1::dot( eta_diff, eta_diff);
+        double ar = dg::blas1::dot( eta, eta);
+        eps =  sqrt( er / ar);
+        std::cout << "rel. error is "<<eps<<" with "<<steps<<" steps\n";
+        //std::cout << "abs. error is "<<sqrt( (end[0]-begin[0])*(end[0]-begin[0]) + (end[1]-begin[1])*(end[1]-begin[1]))<<"\n";
+        steps*=2;
+    }
+}
+
+void construct_grid( 
+        const thrust::host_vector<double>& uZeta, //2d Zeta component
+        const thrust::host_vector<double>& uEta,  //2d Eta component
+        const thrust::host_vector<double>& u_vec,  //1d u values
+        const thrust::host_vector<double>& zeta_init, //1d intial values
+        const thrust::host_vector<double>& eta_init, //1d intial values
+        thrust::host_vector<double>& zeta, 
+        thrust::host_vector<double>& eta, 
+        const dg::Grid2d<double>& g2d
+    )
+{
+    Interpolate inter( uZeta, uEta, g2d);
+    unsigned N = 1;
+    double eps = 1e10, eps_old=2e10;
+    std::vector<thrust::host_vector<double> > begin(2); 
+    begin[0] = zeta_init, begin[1] = eta_init;
+    //now we have the starting values 
+    std::vector<thrust::host_vector<double> > end(begin), temp(begin);
+    unsigned sizeU = u_vec.size(), sizeV = zeta_init.size();
+    unsigned size2d = sizeU*sizeV;
+    zeta.resize(size2d), eta.resize(size2d);
+    double u0=0, u1 = u_vec[0];
+    thrust::host_vector<double> zeta_old(zeta), zeta_diff( zeta), eta_old(eta), eta_diff(eta);
+    while( (eps < eps_old || eps > 1e-6) && eps > 1e-13)
+    {
+        zeta_old = zeta, eta_old = eta; eps_old = eps; 
+        temp = begin;
+        //////////////////////////////////////////////////
+        for( unsigned i=0; i<sizeU; i++)
+        {
+            u0 = i==0?0:u_vec[i-1], u1 = u_vec[i];
+            //////////////////////////////////////////////////
+            dg::stepperRK17( inter, temp, end, u0, u1, N);
+            for( unsigned j=0; j<sizeV; j++)
+            {
+                 unsigned idx = j*sizeU+i;
+                 zeta[idx] = end[0][j], eta[idx] = end[1][j];
+            }
+            //////////////////////////////////////////////////
+            temp = end;
+        }
+        dg::blas1::axpby( 1., zeta, -1., zeta_old, zeta_diff);
+        dg::blas1::axpby( 1., eta, -1., eta_old, eta_diff);
+        dg::blas1::pointwiseDot( zeta_diff, zeta_diff, zeta_diff);
+        dg::blas1::pointwiseDot( 1., eta_diff, eta_diff, 1., zeta_diff);
+        eps = sqrt( dg::blas1::dot( zeta_diff, zeta_diff)/sizeU/sizeV); 
+        std::cout << "Effective Absolute diff error is "<<eps<<" with "<<N<<" steps\n"; 
+        N*=2;
+    }
+
+}
+
+template< class Geometry, class container>
+void transform( 
+        const container u_zeta, 
+        const container u_eta,
+        thrust::host_vector<double> u_x, 
+        thrust::host_vector<double> u_y, 
+        const Geometry& g2d)
+{
+    u_x.resize( u_zeta.size()), u_y.resize( u_zeta.size());
+    thrust::host_vector<double> uh_zeta, uh_eta;
+    dg::blas1::transfer( u_zeta, uh_zeta);
+    dg::blas1::transfer( u_eta, uh_eta);
+    dg::blas1::pointwiseDot( uh_zeta, g2d.xr(), u_x);
+    dg::blas1::pointwiseDot( 1., uh_eta, g2d.yr(), 1., u_x);
+    dg::blas1::pointwiseDot( uh_zeta, g2d.xz(), u_y);
+    dg::blas1::pointwiseDot( 1., uh_eta, g2d.yz(), 1., u_y);
+}
+
+}//namespace detail
 
 //container must be compliant in blas1::transfer function
 template <class IMatrix, class Matrix, class container>
 struct Hector
 {
     template< class Psi, class PsiX, class PsiY, class LaplacePsi>
-    Hector( Psi psi, PsiX psiX, PsiY psiY, LaplacePsi laplacePsi, double psi0, double psi1, double X0, double Y0, unsigned n, unsigned Nx, unsigned Ny, double eps) : 
-        g2d_(psi, psiX, psiY, psi0, psi1, X0, Y0, n, Nx, Ny)
+    Hector( Psi psi, PsiX psiX, PsiY psiY, LaplacePsi laplacePsi, double psi0, double psi1, double X0, double Y0, unsigned n = 11, unsigned Nx = 2, unsigned Ny = 10, double eps_u = 1e-10) : 
+        g2d_(psi, psiX, psiY, laplacePsi, psi0, psi1, X0, Y0, n, Nx, Ny, dg::DIR)
     {
-        double eps = 1e10, eps_old = 2e10;
-        orthogonal::RingGrid2d<dg::DVec> g2d_old(gp, psi_0, psi_1, n, Nx, Ny, dg::DIR);
-        dg::Elliptic<orthogonal::RingGrid2d<container>, Matrix, container> ellipticD_old( g2d_old, dg::DIR, dg::PER, dg::not_normed, dg::centered);
+        //first construct u_
+        container u = construct_grid_and_u( psi, psiX, psiY, laplacePsi, psi0, psi1, X0, Y0, n, Nx, Ny, eps_u );
+        //now compute u_zeta and u_eta and transform to ux_ and uy_
+        Matrix dzeta = dg::create::dx( g2d_, dg::DIR);
+        Matrix deta = dg::create::dy( g2d_, dg::PER);
+        container u_zeta(u), u_eta(u);
+        dg::blas2::symv( dzeta, u, u_zeta);
+        dg::blas1::plus( u_zeta, 1.);
+        dg::blas2::symv( deta, u, u_eta);
 
-        container u_old = dg::evaluate( dg::zero, g2d_old);
-        container lapu = g2d_old.lapx();
-        dg::Invert<container > invert_old( u_old, n*n*Nx*Ny, 1e-10);
-        unsigned number = invert_old( ellipticD_old, u_old, lapu);
-        while( (eps < eps_old||eps > 1e-7) && eps > 1e-10)
-        {
-            eps = eps_old;
-            Nx*=2, Ny*=2;
-            orthogonal::RingGrid2d<container> g2d(gp, psi_0, psi_1, n, Nx, Ny,dg::DIR);
-            dg::Elliptic<orthogonal::RingGrid2d<container>, Matrix, container> ellipticD( g2d, dg::DIR, dg::PER, dg::not_normed, dg::centered);
-            lapu = g2d.lapx();
-            const container vol2d = dg::create::weights( g2d);
-            const IMatrix Q = dg::create::interpolation( g2d, g2d_old);
-            container u = dg::evaluate( dg::zero, g2d), u_diff( u);
-            dg::blas2::gemv( Q, u_old, u_diff);
+        //we actually don't need u but it makes a good testcase 
+        container zeta;
+        dg::blas1::transfer(dg::evaluate( dg::coo1, g2d_), zeta);
+        dg::blas1::axpby( +1., zeta, 1.,  u); //u = \tilde u + \zeta
 
-            dg::Invert<container > invert( u_diff, n*n*Nx*Ny, 1e-10);
-            number = invert( ellipticD, u, lapu);
-            dg::blas1::axpby( 1. ,u, -1., u_diff);
-            eps = sqrt( dg::blas2::dot( u_diff, vol2d, u_diff) / dg::blas2::dot( u, vol2d, u) );
-            //std::cout << "Nx "<<Nx<<" Ny "<<Ny<<" error "<<eps<<"\n";
-            g2d_old = g2d;
-            u_old = u;
-            g2d_ = g2d;
-        }
-        //Matrix dzeta = dg::create::dx( g2d_, dg::DIR);
-        //Matrix deta = dg::create::dy( g2d_, dg::DIR_NEU);
-        //container u_zeta(u), u_eta(u), v_zeta(v), v_zeta(v);
-        //container u_x(u), u_y(u), v_x(v), v_y(v);
-        //dg::blas2::symv( dzeta, u, u_zeta);
-        //dg::blas1::plus( u_zeta, 1.);
-        //dg::blas2::symv( deta, u, u_eta);
-        //dg::blas2::symv( dzeta, v, v_zeta);
-        //dg::blas2::symv( deta, v, v_eta);
-        //dg::blas1::plus( v_eta, 1.);
-        //dg::blas1::pointwiseDot( u_zeta, g2d.xr(), u_x); 
-        //dg::blas1::pointwiseDot( 1., u_eta, g2d.yr(), 1., u_x); 
-        //dg::blas1::pointwiseDot( u_zeta, g2d.xz(), u_y); 
-        //dg::blas1::pointwiseDot( 1., u_eta, g2d.yz(), 1., u_y); 
-        //dg::blas1::pointwiseDot( v_zeta, g2d.xr(), v_x); 
-        //dg::blas1::pointwiseDot( 1., u_eta, g2d.yr(), 1., v_x); 
-        //dg::blas1::pointwiseDot( v_zeta, g2d.xz(), v_y); 
-        //dg::blas1::pointwiseDot( 1., v_eta, g2d.yz(), 1., v_y); 
+        //now compute the components of the vector fields
+        container a;
+        dg::blas1::transfer( g2d_.g_yy(), a);
+        dg::blas1::pointwiseDivide( a, g2d_.g_xx(), a); // a^2=g^ee/g^zz
+        container zetaU=u_zeta, etaU=u_eta;
+        dg::blas1::pointwiseDot( zetaU, zetaU, zetaU); //u_z*u_z
+        dg::blas1::pointwiseDot(  etaU,  etaU,  etaU); //u_e*u_e
+        dg::blas1::pointwiseDot( 1., etaU, a, 1., zetaU); //u_z*u_z+a^2u_e*u_e
+        container den( zetaU); //denominator
+        dg::blas1::pointwiseDivide( u_zeta, den, zetaU); //u_z / denominator
+        dg::blas1::pointwiseDivide(  u_eta, den,  etaU); 
+        dg::blas1::pointwiseDot( a, etaU, etaU); //a^2*u_e / denominator
 
-        //container xdiff(u), ydiff(u);
-        //dg::blas1::axpby( 1., u_x, -1., v_y, xdiff);
-        //dg::blas1::axpby( 1., u_y, +1., v_x, ydiff);
-        //dg::blas2::dot( xdiff, vol2d, xdiff);
-        //dg::blas2::dot( ydiff, vol2d, ydiff);
+        container etaV(etaU), etaVinv(etaV);
+        dg::blas1::transform( a, a, dg::SQRT<double>());
+        dg::blas1::pointwiseDivide( a, u_zeta, etaV);
+        dg::blas1::pointwiseDivide( u_zeta, a, etaVinv);
+        thrust::host_vector<double> etaVinv_h;
+        dg::blas1::transfer( etaVinv, etaVinv_h);
+        //construct c0 and scale all vector components with it
+        c0_ = detail::construct_c0( etaVinv_h, g2d_);
+        dg::blas1::scal(  etaV, 1./c0_);
+        dg::blas1::scal( zetaU, 1./c0_);
+        dg::blas1::scal(  etaU, 1./c0_);
+        dg::blas1::scal( u_zeta, c0_);
+        dg::blas1::scal(  u_eta, c0_);
+        dg::blas1::scal(  u, c0_);
+        //transfer to host
+        dg::blas1::transfer( u, u_);
+        dg::blas1::transfer( u_zeta, u_zeta_);
+        dg::blas1::transfer( u_eta, u_eta_);
+        detail::transform( u_zeta, u_eta, ux_, uy_, g2d_);
+        dg::blas1::transfer( etaV, etaV_);
+        dg::blas1::transfer( etaU, etaU_);
+        dg::blas1::transfer( zetaU, zetaU_);
+        std::cout << "c0 is "<<c0_<<"\n";
     }
-    double lu() const {return g2d_.lx();}
+    double lu() const {return c0_*g2d_.lx();}
     double lv() const {return 2.*M_PI;}
-    void operator()( const thrust::host_vector<double>& u, 
-                     const thrust::host_vector<double>& v, 
+    void operator()( const thrust::host_vector<double>& u1d, 
+                     const thrust::host_vector<double>& v1d, 
                      thrust::host_vector<double>& x, 
                      thrust::host_vector<double>& y, 
                      thrust::host_vector<double>& ux, 
                      thrust::host_vector<double>& uy, 
                      thrust::host_vector<double>& vx, 
-                     thrust::host_vector<double>& vy);
-    private
+                     thrust::host_vector<double>& vy)
+    {
+        thrust::host_vector<double> eta_init, zeta, eta; 
+        compute_zev( etaV_, v1d, eta_init, g2d_);
+        thrust::host_vector<double> zeta_init( eta_init.size(), 0.); 
+        construct_grid( u_zeta_, u_eta_, zeta_init, eta_init, zeta, eta, g2d_);
+        dg::IHMatrix Q = dg::create::interpolation( zeta, eta, g2d_);
+
+        thrust::host_vector<double> u(u1d.size()*v1d.size());
+        x = y = ux = uy = vx = vy = u;  //resize 
+        dg::blas2::symv( Q, g2d_.r(), x);
+        dg::blas2::symv( Q, g2d_.z(), y);
+        dg::blas2::symv( Q, ux_, ux);
+        dg::blas2::symv( Q, uy_, uy);
+        dg::blas1::transfer( ux, vy);
+        dg::blas1::axpby( -1., uy, 0., vx);
+
+
+    }
+    private:
+    template< class Psi, class PsiX, class PsiY, class LaplacePsi>
+    container construct_grid_and_u( Psi psi, PsiX psiX, PsiY psiY, LaplacePsi laplacePsi, double psi0, double psi1, double X0, double Y0, unsigned n, unsigned Nx, unsigned Ny, double eps_u ) 
+    {
+        //first find u( \zeta, \eta)
+        double eps = 1e10, eps_old = 2e10;
+        orthogonal::RingGrid2d<dg::DVec> g2d_old(psi, psiX, psiY, laplacePsi, psi0, psi1, X0, Y0, n, Nx, Ny, dg::DIR);
+        dg::Elliptic<orthogonal::RingGrid2d<container>, Matrix, container> ellipticD_old( g2d_old, dg::DIR, dg::PER, dg::not_normed, dg::centered);
+
+        container u_old = dg::evaluate( dg::zero, g2d_old), u(u_old);
+        container lapu = g2d_old.lapx();
+        dg::Invert<container > invert_old( u_old, n*n*Nx*Ny, eps_u);
+        unsigned number = invert_old( ellipticD_old, u_old, lapu);
+        while( (eps < eps_old||eps > 1e-7) && eps > eps_u)
+        {
+            eps = eps_old;
+            Nx*=2, Ny*=2;
+            orthogonal::RingGrid2d<dg::DVec> g2d(psi, psiX, psiY, laplacePsi, psi0, psi1, X0, Y0, n, Nx, Ny, dg::DIR);
+            dg::Elliptic<orthogonal::RingGrid2d<container>, Matrix, container> ellipticD( g2d, dg::DIR, dg::PER, dg::not_normed, dg::centered);
+            lapu = g2d.lapx();
+            const container vol2d = dg::create::weights( g2d);
+            const IMatrix Q = dg::create::interpolation( g2d, g2d_old);
+            u = dg::evaluate( dg::zero, g2d);
+            container u_diff( u);
+            dg::blas2::gemv( Q, u_old, u_diff);
+
+            dg::Invert<container > invert( u_diff, n*n*Nx*Ny, 0.1*eps_u);
+            number = invert( ellipticD, u, lapu);
+            dg::blas1::axpby( 1. ,u, -1., u_diff);
+            eps = sqrt( dg::blas2::dot( u_diff, vol2d, u_diff) / dg::blas2::dot( u, vol2d, u) );
+            std::cout << "Nx "<<Nx<<" Ny "<<Ny<<" error "<<eps<<"\n";
+            g2d_old = g2d;
+            u_old = u;
+            g2d_ = g2d;
+            number++;//get rid of warning
+        }
+        return u;
+    }
+
+    double c0_;
+    thrust::host_vector<double> u_, u_zeta_, u_eta_, ux_, uy_;
+    thrust::host_vector<double> etaV_, zetaU_, etaU_;
     orthogonal::RingGrid2d<container> g2d_;
 
 };
--- inc/geometries/hector_b.cu
+++ inc/geometries/hector_b.cu
@@ -12,6 +12,7 @@
 #include "solovev.h"
 //#include "guenther.h"
 #include "orthogonal.h"
+#include "hector.h"
 
 
 
@@ -48,66 +49,20 @@ int main(int argc, char**argv)
     gp.display( std::cout);
     dg::Timer t;
     solovev::Psip psip( gp); 
+    solovev::PsipR psipR( gp); 
+    solovev::PsipZ psipZ( gp); 
+    solovev::LaplacePsip lap( gp); 
     std::cout << "Psi min "<<psip(gp.R_0, 0)<<"\n";
 
-    double eps = 1e10, eps_old = 2e10;
-    orthogonal::RingGrid2d<dg::DVec> g2d_old(gp, psi_0, psi_1, n, Nx, Ny, dg::DIR);
-    dg::Elliptic<orthogonal::RingGrid2d<dg::DVec>, dg::DMatrix, dg::DVec> ellipticD_old( g2d_old, dg::DIR, dg::PER, dg::not_normed, dg::centered);
-
-    dg::DVec u_old = dg::evaluate( dg::zero, g2d_old);
-    dg::DVec lapu = g2d_old.lapx();
-    dg::Invert<dg::DVec > invert_old( u_old, n*n*Nx*Ny, 1e-10);
-    unsigned number = invert_old( ellipticD_old, u_old, lapu);
-    while( (eps < eps_old||eps > 1e-7) && eps > eps_uv)
-    {
-        eps = eps_old;
-        Nx*=2, Ny*=2;
-        orthogonal::RingGrid2d<dg::DVec> g2d(gp, psi_0, psi_1, n, Nx, Ny,dg::DIR);
-        dg::Elliptic<orthogonal::RingGrid2d<dg::DVec>, dg::DMatrix, dg::DVec> ellipticD( g2d, dg::DIR, dg::PER, dg::not_normed, dg::centered);
-        lapu = g2d.lapx();
-        const dg::DVec vol2d = dg::create::weights( g2d);
-        const dg::IDMatrix Q = dg::create::interpolation( g2d, g2d_old);
-        dg::DVec u = dg::evaluate( dg::zero, g2d), u_diff( u);
-        dg::blas2::gemv( Q, u_old, u_diff);
-        dg::Invert<dg::DVec > invertU( u_diff, n*n*Nx*Ny, 1e-10);
-        number = invertU( ellipticD, u, lapu);
-        dg::blas1::axpby( 1. ,u, -1., u_diff);
-        eps = sqrt( dg::blas2::dot( u_diff, vol2d, u_diff) / dg::blas2::dot( u, vol2d, u) );
-        std::cout << "Nx "<<Nx<<" Ny "<<Ny<<" error uv "<<eps<<"\n";
-        g2d_old = g2d;
-        u_old = u;
-    }
-    dg::HVec u,v;
-    dg::blas1::transfer( u_old, u); 
-    //dg::blas1::transfer( v_old, v); 
-    //dg::HMatrix dxDIRNEU = dg::create::dx( g2d_old, dg::DIR_NEU);
-    dg::HMatrix dxDIR = dg::create::dx( g2d_old, dg::DIR);
-    dg::HMatrix dyPER = dg::create::dy( g2d_old, dg::PER);
-    dg::HVec u_zeta(u), u_eta(u), v_zeta(v), v_eta(v);
-    //dg::HVec u_x(u), u_y(u), v_x(v), v_y(v);
-    dg::blas2::symv( dxDIR, u, u_zeta);
-    dg::blas1::plus( u_zeta, +1.);
-    dg::blas2::symv( dyPER, u, u_eta);
-    //dg::blas2::symv( dxDIRNEU, v, v_zeta);
-    //dg::blas2::symv( dyPER, v, v_eta);
-    //dg::blas1::plus( v_eta, +1.);
-    //dg::blas1::pointwiseDot( u_zeta, g2d_old.xr(), u_x); 
-    //dg::blas1::pointwiseDot( 1., u_eta, g2d_old.yr(), 1., u_x); 
-    //dg::blas1::pointwiseDot( u_zeta, g2d_old.xz(), u_y); 
-    //dg::blas1::pointwiseDot( 1., u_eta, g2d_old.yz(), 1., u_y); 
-    //dg::blas1::pointwiseDot( v_zeta, g2d_old.xr(), v_x); 
-    //dg::blas1::pointwiseDot( 1., v_eta, g2d_old.yr(), 1., v_x); 
-    //dg::blas1::pointwiseDot( v_zeta, g2d_old.xz(), v_y); 
-    //dg::blas1::pointwiseDot( 1., v_eta, g2d_old.yz(), 1., v_y); 
-
-    //dg::HVec xdiff(u), ydiff(u);
-    //dg::blas1::axpby( 1., u_x, -1., v_y, xdiff);
-    //dg::blas1::axpby( 1., u_y, +1., v_x, ydiff);
-    //const dg::HVec vol2d = dg::create::weights( g2d_old);
-    //std::cout << "rel conformity error X "<<sqrt(dg::blas2::dot( xdiff, vol2d, xdiff)/dg::blas2::dot( u_x, vol2d, u_x)) <<"\n";
-    //std::cout << "rel conformity error Y "<<sqrt(dg::blas2::dot( ydiff, vol2d, ydiff)/dg::blas2::dot( u_y, vol2d, u_y)) <<"\n";
+    hector::Hector<dg::IDMatrix, dg::DMatrix, dg::DVec> hector( psip, psipR, psipZ, lap, psi_0, psi_1, gp.R_0, 0.);
+    dg::Grid2d<double> guv( 0., hector.lu(), 0., hector.lv(), n, Nx, Ny );
+    dg::Grid1d<double> gu( 0., hector.lu(), n, Nx);
+    dg::Grid1d<double> gv( 0., hector.lv(), n, Ny);
+    dg::HVec u1d = dg::evaluate( dg::coo1, gu);
+    dg::HVec v1d = dg::evaluate( dg::coo1, gv);
 
     ///////////////////////////////FILE OUTPUT/////////////////////////////
+    /*
     g2d_old.display();
     int ncid;
     file::NC_Error_Handle err;
@@ -146,5 +101,6 @@ int main(int argc, char**argv)
     dg::blas1::transfer( u_eta, X);
     err = nc_put_var_double( ncid, errID, X.data());
     err = nc_close(ncid);
+    */
     return 0;
 }
