de5c2c74baa5d1777583bab8ccd1818bf4f4c859
mrheld@laptop <markus.held@uibk.ac.at>
2016-11-24 10:46:04 +0100

    added jsonlib

--- config/default.mk
+++ config/default.mk
@@ -10,4 +10,5 @@ OPT=-O3 # optimization flag
 NVCCARCH=-arch sm_20 #nvcc gpu compute capability
 OMPFLAG=-fopenmp #openmp flag for CC and MPICC
 LIBS=-lnetcdf -lhdf5 -lhdf5_hl # netcdf library for file output
+JSONLIB=-L$(HOME)/include/json/../../src/lib_json -ljsoncpp # json library for input parameters
 endif # INCLUDED
