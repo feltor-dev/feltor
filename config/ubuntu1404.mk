e4b511313dab54dad668f4995bae3f6fb2b661b5
leinkemmer <lukas.einkemmer@gmail.com>
2017-03-13 16:53:06 +0100

    adds config files for Ubuntu systems and fixes some compile time issues

---
+++ config/ubuntu1404.mk
@@ -0,0 +1,14 @@
+ifeq ($(OS),ubuntu1404)
+#default machine values
+INCLUDE = -I$(HOME)/include#  # cusp and thrust and the draw libraries
+INCLUDE += -I/usr/include/hdf5/serial
+GLFLAGS =$$(pkg-config --static --libs glfw3) #glfw3 installation
+CFLAGS += -fpermissive
+CC=g++ #C++ compiler
+MPICC=mpic++  #mpi compiler
+OPT=-O3 # optimization flag
+NVCCARCH=-arch sm_20 #nvcc gpu compute capability
+OMPFLAG=-fopenmp #openmp flag for CC and MPICC
+LIBS=-lnetcdf -lhdf5 -lhdf5_hl # netcdf library for file output
+JSONLIB=-L$(HOME)/include/json/../../src/lib_json -ljsoncpp # json library for input parameters
+endif # ubuntu1504
