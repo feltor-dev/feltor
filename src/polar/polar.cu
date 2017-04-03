1e1b87b4fc11a3ad98a3f0645d44367b0b3aca1a
Lukas Einkemmer <lukas.einkemmer@gmail.com>
2017-04-03 17:24:19 +0200

    adds conformal polar grid (log(r) grid)

--- src/polar/polar.cu
+++ src/polar/polar.cu
@@ -61,7 +61,7 @@ struct PolarGenerator
         zetaX.resize(size); zetaY.resize(size);
         etaX.resize(size); etaY.resize(size);
 
-        // the first coordinate has stride=1 (according to inc/dg/backend/evaluation.cuh:61)
+        // the first coordinate has stride=1
         for(int j=0;j<size_phi;j++)
             for(int i=0;i<size_r;i++) {
                 double r   = zeta1d[i] + r_min;
@@ -73,16 +73,7 @@ struct PolarGenerator
                 zetaX[i+size_r*j] = cos(phi);
                 zetaY[i+size_r*j] = sin(phi);
                 etaX[i+size_r*j] = -sin(phi)/r;
-                etaY[i+size_r*j] = -cos(phi)/r;
-/*
-                x[i+size_r*j] = r;
-                y[i+size_r*j] = phi;
-
-                zetaX[i+size_r*j] = 1.0;
-                zetaY[i+size_r*j] = 0.0;
-                etaX[i+size_r*j] = 0.0;
-                etaY[i+size_r*j] = 1.0;
-*/
+                etaY[i+size_r*j] =  cos(phi)/r;
             }
 
     }
@@ -102,6 +93,63 @@ struct func_r {
     }
 };
 
+
+struct LogPolarGenerator
+{
+    private:
+        double r_min, r_max;
+
+    public:
+
+    LogPolarGenerator(double _r_min, double _r_max) : r_min(_r_min), r_max(_r_max) {}
+
+    void operator()(
+         const thrust::host_vector<double>& zeta1d,
+         const thrust::host_vector<double>& eta1d,
+         thrust::host_vector<double>& x,
+         thrust::host_vector<double>& y,
+         thrust::host_vector<double>& zetaX,
+         thrust::host_vector<double>& zetaY,
+         thrust::host_vector<double>& etaX,
+         thrust::host_vector<double>& etaY) {
+
+        int size_r   = zeta1d.size();
+        int size_phi = eta1d.size();
+        int size     = size_r*size_phi;
+
+        x.resize(size); y.resize(size);
+        zetaX.resize(size); zetaY.resize(size);
+        etaX.resize(size); etaY.resize(size);
+
+        // the first coordinate has stride=1
+        for(int j=0;j<size_phi;j++)
+            for(int i=0;i<size_r;i++) {
+                double l   = zeta1d[i] + log(r_min);
+                double phi = eta1d[j];
+
+                x[i+size_r*j] = exp(l)*cos(phi);
+                y[i+size_r*j] = exp(l)*sin(phi);
+
+                zetaX[i+size_r*j] = cos(phi)*exp(-l);
+                zetaY[i+size_r*j] = sin(phi)*exp(-l);
+                etaX[i+size_r*j] = -sin(phi)*exp(-l);
+                etaY[i+size_r*j] =  cos(phi)*exp(-l);
+            }
+
+    }
+
+    double width() const{return log(r_max)-log(r_min);}
+    double height() const{return 2*M_PI;}
+    bool isOrthogonal() const{return true;}
+    bool isConformal()  const{return true;}
+};
+
+#ifdef LOG_POLAR
+    #define Generator LogPolarGenerator
+#else
+    #define Generator PolarGenerator
+#endif
+
 int main()
 {
     Timer t;
@@ -114,7 +162,7 @@ int main()
     }
 
     //Grid2d grid( 0, p.lx, 0, p.ly, p.n, p.Nx, p.Ny, p.bc_x, p.bc_y);
-    PolarGenerator generator(p.r_min, p.r_max);
+    Generator generator(p.r_min, p.r_max); // Generator is defined by the compiler
     Grid grid( generator, p.n, p.Nx, p.Ny, dg::DIR); // second coordiante is periodic by default
 
     DVec w2d( create::weights(grid));
@@ -128,8 +176,11 @@ int main()
 
     dg::Lamb lamb( p.posX, p.posY, p.R, p.U);
     HVec omega = evaluate ( lamb, grid);
-
+#if LOG_POLAR
+    DVec stencil = evaluate( one, grid);
+#else
     DVec stencil = evaluate( func_r(p.r_min), grid);
+#endif
     DVec y0( omega ), y1( y0);
 
     //make solver and stepper
