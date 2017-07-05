02b080d58efa242a255e14bcfad33fc5ad30ee9c
Matthias <MWiesenberger@gmx.de>
2017-07-05 19:06:30 +0200

    updated projection.cuh and projection_t

--- inc/dg/backend/projection.cuh
+++ inc/dg/backend/projection.cuh
@@ -1,174 +1,201 @@
 #pragma once
 #include <vector>
 #include <cusp/coo_matrix.h>
+#include <cusp/transpose.h>
 #include "grid.h"
 #include "interpolation.cuh"
-#include "matrix_traits_thrust.h"
-#include "../blas2.h"
 
 /*!@file 
   
-  contains the Difference Norm class that computes differences between vectors on different grids
+  contains creation of projection matrices
  */
 namespace dg{
 ///@addtogroup utilities
 ///@{
-/**
- * @brief Greatest common divisor
- *
- * @param a First number
- * @param b Second number
- *
- * @return greatest common divisor
- */
-unsigned gcd( unsigned a, unsigned b)
-{
-    unsigned r2 = std::max(a,b);
-    unsigned r1 = std::min(a,b);
-    while( r1!=0)
-    {
-        r2 = r2%r1;
-        std::swap( r1, r2);
-    }
-    return r2;
-}
-
-/**
- * @brief Least common multiple
- *
- * @param a Fist number
- * @param b Second number 
- *
- * @return Least common multiple
- */
-unsigned lcm( unsigned a, unsigned b)
-{
-    unsigned g = gcd( a,b);
-    return a/g*b;
-}
 
 namespace create{
 
 /**
- * @brief Create a 1D projection matrix onto a finer grid
+ * @brief Create the transpose of the interpolation matrix from new to old
  *
- * Grid space must be equal. Nx of the second grid must be a multiple of 
- * Nx of the first grid.
- * @param g1 Grid of the original vector
- * @param g2 Grid of the target vector
+ * @param g_new The new grid 
+ * @param g_old The old grid
  *
- * @return Projection matrix
+ * @return transposed interpolation matrix
+ * @note The boundaries of the old grid must lie within the boundaries of the new grid
  */
-cusp::coo_matrix< int, double, cusp::host_memory> projection( const Grid1d& g1, const Grid1d& g2)
+cusp::coo_matrix<int, double, cusp::host_memory> interpolationT( const Grid1d& g_new, const Grid1d& g_old)
 {
-    assert( g1.x0() == g2.x0()); assert( g1.x1() == g2.x1());
-    assert( g2.N() % g1.N() == 0);
-    return dg::create::interpolation( g2, g1);
+    cusp::coo_matrix<int, double, cusp::host_memory> temp = interpolation( g_old, g_new), A;
+    cusp::transpose( temp, A);
+    return A;
 }
 
 /**
- * @brief Create a 2D projection matrix onto a finer grid
+ * @brief Create a projection between two grids
  *
- * Grid space must be equal. Nx and Ny of the second grid must be multiples of 
- * Nx and Ny of the first grid.
- * @param g1 Grid of the original vector
- * @param g2 Grid of the target vector
+ * The projection matrix is the adjoint of the interpolation matrix
+ * This matrix can be applied to vectors defined on the old (fine) grid to obtain
+ * its values on the new (coarse) grid. 
+ * If the fine grid is a multiple of the coarse grid, the integral value
+ of the projected vector will be conserved and the difference in the L2 norm 
+ between old and new vector small. 
+ * 
+ * @param g_new The new (coarse) grid 
+ * @param g_old The old (fine) grid
  *
  * @return Projection matrix
+ * @note The boundaries of the old grid must lie within the boundaries of the new grid
+ @attention Projection only works if the number of cells in the
+ fine grid are multiple of the number of cells in the coarse grid
  */
-cusp::coo_matrix< int, double, cusp::host_memory> projection( const Grid2d& g1, const Grid2d& g2)
+cusp::coo_matrix< int, double, cusp::host_memory> projection( const Grid1d& g_new, const Grid1d& g_old)
 {
-    //TODO: projection in y direction needs permutation
-    assert( g1.x0() == g2.x0()); assert( g1.x1() == g2.x1());
-    assert( g1.y0() == g2.y0()); assert( g1.y1() == g2.y1());
-    //assert( g2.Nx() % g1.Nx() == 0);
-    //assert( g2.Ny() % g1.Ny() == 0);
-    return dg::create::interpolation( g2, g1);
+    if( g_old.N() % g_new.N() != 0) std::cerr << "ATTENTION: you project between incompatible grids!!\n";
+    //form the adjoint
+    thrust::host_vector<double> w_f = dg::create::weights( g_old);
+    thrust::host_vector<double> v_c = dg::create::inv_weights( g_new );
+    cusp::coo_matrix<int, double, cusp::host_memory> Wf( w_f.size(), w_f.size(), w_f.size());
+    cusp::coo_matrix<int, double, cusp::host_memory> Vc( v_c.size(), v_c.size(), v_c.size());
+    for( int i =0; i<(int)w_f.size(); i++)
+    {
+        Wf.row_indices[i] = Wf.column_indices[i] = i;
+        Wf.values[i] = w_f[i];
+    }
+    for( int i =0; i<(int)v_c.size(); i++)
+    {
+        Vc.row_indices[i] = Vc.column_indices[i] = i;
+        Vc.values[i] = v_c[i];
+    }
+    cusp::coo_matrix<int, double, cusp::host_memory> A = interpolationT( g_new, g_old), temp;
+    cusp::multiply( A, Wf, temp);
+    cusp::multiply( Vc, temp, A);
+    A.sort_by_row_and_column();
+    return A;
+}
+
+/**
+ * @brief Create the transpose of the interpolation matrix from new to old
+ *
+ * @param g_new The new grid 
+ * @param g_old The old grid
+ *
+ * @return transposed interpolation matrix
+ * @note The boundaries of the old grid must lie within the boundaries of the new grid
+ */
+cusp::coo_matrix<int, double, cusp::host_memory> interpolationT( const Grid2d& g_new, const Grid2d& g_old)
+{
+    cusp::coo_matrix<int, double, cusp::host_memory> temp = interpolation( g_old, g_new), A;
+    cusp::transpose( temp, A);
+    return A;
+}
+
+/**
+ * @brief Create a projection between two grids
+ *
+ * The projection matrix is the adjoint of the interpolation matrix
+ * This matrix can be applied to vectors defined on the old (fine) grid to obtain
+ * its values on the new (coarse) grid. 
+ * If the fine grid is a multiple of the coarse grid, the integral value
+ of the projected vector will be conserved and the difference in the L2 norm 
+ between old and new vector small. 
+ * 
+ * @param g_new The new grid 
+ * @param g_old The old grid
+ *
+ * @return transposed interpolation matrix
+ * @note The boundaries of the old grid must lie within the boundaries of the new grid
+ @attention Projection only works if the number of cells in the
+ fine grid are multiple of the number of cells in the coarse grid
+ */
+cusp::coo_matrix< int, double, cusp::host_memory> projection( const Grid2d& g_new, const Grid2d& g_old)
+{
+    if( g_old.Nx() % g_new.Nx() != 0) std::cerr << "ATTENTION: you project between incompatible grids in x!!\n";
+    if( g_old.Ny() % g_new.Ny() != 0) std::cerr << "ATTENTION: you project between incompatible grids in y!!\n";
+    //form the adjoint
+    thrust::host_vector<double> w_f = dg::create::weights( g_old);
+    thrust::host_vector<double> v_c = dg::create::inv_weights( g_new );
+    cusp::coo_matrix<int, double, cusp::host_memory> Wf( w_f.size(), w_f.size(), w_f.size());
+    cusp::coo_matrix<int, double, cusp::host_memory> Vc( v_c.size(), v_c.size(), v_c.size());
+    for( int i =0; i<(int)w_f.size(); i++)
+    {
+        Wf.row_indices[i] = Wf.column_indices[i] = i;
+        Wf.values[i] = w_f[i];
+    }
+    for( int i =0; i<(int)v_c.size(); i++)
+    {
+        Vc.row_indices[i] = Vc.column_indices[i] = i;
+        Vc.values[i] = v_c[i];
+    }
+    cusp::coo_matrix<int, double, cusp::host_memory> A = interpolationT( g_new, g_old), temp;
+    cusp::multiply( A, Wf, temp);
+    cusp::multiply( Vc, temp, A);
+    A.sort_by_row_and_column();
+    return A;
+}
+/**
+ * @brief Create the transpose of the interpolation matrix from new to old
+ *
+ * @param g_new The new grid 
+ * @param g_old The old grid
+ *
+ * @return transposed interpolation matrix
+ * @note The boundaries of the old grid must lie within the boundaries of the new grid
+ */
+cusp::coo_matrix<int, double, cusp::host_memory> interpolationT( const Grid3d& g_new, const Grid3d& g_old)
+{
+    cusp::coo_matrix<int, double, cusp::host_memory> temp = interpolation( g_old, g_new), A;
+    cusp::transpose( temp, A);
+    return A;
+}
+
+/**
+ * @brief Create a projection between two grids
+ *
+ * The projection matrix is the adjoint of the interpolation matrix
+ * This matrix can be applied to vectors defined on the old (fine) grid to obtain
+ * its values on the new (coarse) grid. 
+ * If the fine grid is a multiple of the coarse grid, the integral value
+ of the projected vector will be conserved and the difference in the L2 norm 
+ between old and new vector small. 
+ * 
+ * @param g_new The new grid 
+ * @param g_old The old grid
+ *
+ * @return transposed interpolation matrix
+ * @note The boundaries of the old grid must lie within the boundaries of the new grid
+ @attention Projection only works if the number of cells in the
+ fine grid are multiple of the number of cells in the coarse grid
+ */
+cusp::coo_matrix< int, double, cusp::host_memory> projection( const Grid3d& g_new, const Grid3d& g_old)
+{
+    if( g_old.Nx() % g_new.Nx() != 0) std::cerr << "ATTENTION: you project between incompatible grids in x!!\n";
+    if( g_old.Ny() % g_new.Ny() != 0) std::cerr << "ATTENTION: you project between incompatible grids in y!!\n";
+    //form the adjoint
+    thrust::host_vector<double> w_f = dg::create::weights( g_old);
+    thrust::host_vector<double> v_c = dg::create::inv_weights( g_new );
+    cusp::coo_matrix<int, double, cusp::host_memory> Wf( w_f.size(), w_f.size(), w_f.size());
+    cusp::coo_matrix<int, double, cusp::host_memory> Vc( v_c.size(), v_c.size(), v_c.size());
+    for( int i =0; i<(int)w_f.size(); i++)
+    {
+        Wf.row_indices[i] = Wf.column_indices[i] = i;
+        Wf.values[i] = w_f[i];
+    }
+    for( int i =0; i<(int)v_c.size(); i++)
+    {
+        Vc.row_indices[i] = Vc.column_indices[i] = i;
+        Vc.values[i] = v_c[i];
+    }
+    cusp::coo_matrix<int, double, cusp::host_memory> A = interpolationT( g_new, g_old), temp;
+    cusp::multiply( A, Wf, temp);
+    cusp::multiply( Vc, temp, A);
+    A.sort_by_row_and_column();
+    return A;
 }
 
 
 }//namespace create
 
-
-//eventuell könnte man zwei Projektionsmatrizen malnehmen um eine kleinere zu erhalten
-/**
- * @brief Class to perform comparison of dG vectors on different grids
- *
- * it basically interpolates values from the rougher grid to values on the finer grid and then uses the existing methods to compute the norm
- * @tparam container
- */
-template <typename container>
-struct DifferenceNorm
-{
-    /**
-     * @brief Construct from two different grids
-     *
-     * @param g1 first grid
-     * @param g2 second grid
-     */
-    DifferenceNorm( const Grid2d& g1, const Grid2d& g2)
-    {
-        //find common grid
-        Grid2d gC(    g1.x0(), g1.x1(), g1.y0(), g1.y1(), 
-                            std::min( g1.n(), g2.n()), 
-                            lcm( g1.Nx(), g2.Nx()), 
-                            lcm( g1.Ny(), g2.Ny()) );
-        p1 = dg::create::interpolation( gC, g1);
-        p2 = dg::create::interpolation( gC, g2);
-        w2d = dg::create::weights( gC); v11 = w2d, v22 = w2d;
-        wg1 = dg::create::weights( g1); 
-        wg2 = dg::create::weights( g2); 
-    }
-    /**
-     * @brief Compute difference of two vectors
-     *
-     * \f[ ||v_1 - v_2|| = \sqrt{ \int (v_1-v_2)^2 dV} \f]
-     * @param v1
-     * @param v2
-     *
-     * @return 
-     */
-    double operator()( const container& v1, const container& v2)
-    {
-        double f2, g2, fg;
-        f2 = blas2::dot( wg1, v1);
-        g2 = blas2::dot( wg2, v2);
-
-        blas2::gemv( p1, v1, v11);
-        blas2::gemv( p2, v2, v22);
-        fg = blas2::dot( v11, w2d, v22);
-        return sqrt( f2 - 2.*fg + g2);
-    }
-
-    /**
-     * @brief Compute the sum of two vectors
-     *
-     * \f[ ||v_1 + v_2|| = \sqrt{ \int (v_1+v_2)^2 dV} \f]
-     * @param v1
-     * @param v2
-     *
-     * @return 
-     */
-    double sum( const container& v1, const container& v2)
-    {
-        double f2, g2, fg;
-        f2 = blas2::dot( wg1, v1);
-        g2 = blas2::dot( wg2, v2);
-
-        blas2::gemv( p1, v1, v11);
-        blas2::gemv( p2, v2, v22);
-        fg = blas2::dot( v11, w2d, v22);
-        return sqrt( f2 + 2.*fg + g2);
-    }
-  private:
-    typedef typename container::value_type value_type;
-    typedef typename thrust::iterator_system<typename container::iterator>::type MemorySpace;
-    typedef cusp::csr_matrix<int, double, MemorySpace> Matrix;
-
-    container wg1, wg2, w2d;
-    container v11, v22;
-    Matrix p1, p2;
-};
 ///@}
 
 
