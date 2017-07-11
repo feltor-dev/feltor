54b4af0f9a9e234398c74450d573c0ad2a7dde40
Matthias <MWiesenberger@gmx.de>
2017-07-11 23:25:23 +0200

    made toroidal slab with curvature

--- inc/geometries/toroidal.h
+++ inc/geometries/toroidal.h
@@ -11,12 +11,11 @@ namespace toroidal{
 /**
  * @brief Models a slab toroidal field (models aTokamakMagneticField)
  *
- * \f$ \psi_p = 1\f$ and \f$ I = R \f$.
- *
+ * \f$ B=\frac{R_0}{R}\f$, \f$ \psi_p = 1\f$ and \f$ I = 1\f$.
  */
 struct MagneticField
 {
-    MagneticField(): R_0(1.), psip(1), psipR(0), psipZ(0), psipRR(0), psipRZ(0), psipZZ(0), laplacePsip(0), ipol(1,0), ipolR(1), ipolZ(0){}
+    MagneticField(double R0): R_0(R0), psip(1), psipR(0), psipZ(0), psipRR(0), psipRZ(0), psipZZ(0), laplacePsip(0), ipol(1), ipolR(0), ipolZ(0){}
     double R_0;
     dg::CONSTANT psip;
     dg::CONSTANT psipR;
@@ -25,10 +24,11 @@ struct MagneticField
     dg::CONSTANT psipRZ;
     dg::CONSTANT psipZZ;
     dg::CONSTANT laplacePsip;
-    dg::LinearX ipol;
+    dg::CONSTANT ipol;
     dg::CONSTANT ipolR;
     dg::CONSTANT ipolZ;
 };
+
 }//namespace toroidal
 }//namespace geo
 }//namespace dg
