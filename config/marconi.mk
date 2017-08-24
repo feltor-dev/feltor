92eb32612a4269110e97910065271f6cbd8a7432
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2017-08-24 15:55:47 +0200

    update marconi.mk also in newds branch

--- config/marconi.mk
+++ config/marconi.mk
@@ -5,7 +5,7 @@ INCLUDE += -I$(NETCDF_INC) -I$(HDF5_INC)
 GLFLAGS  = -lm
 CC=icc
 MPICC=mpiicc
-OPT=-O3 -xHost
+OPT=-O3 -xHost -restrict # overwritten for mic in devices.mk
 #MPICFLAGS+= -DMPICH_IGNORE_CXX_SEEK
 OMPFLAG=-qopenmp
 JSONLIB=-L$(HOME)/include/json/../../src/lib_json -ljsoncpp # json library for input parameters
@@ -22,5 +22,10 @@ endif
 #module load netcdf/4.4.1--intelmpi--2017--binary 
 
 
+###########configure mic jobs with#########################
+#mcdram=cache:numa=quadrant
+#export KMP_AFFINITY=scatter #important
+#export OM_NUM_THREADS=68
+#qsub -I -qxfuaknldebug -A FUA21_FELTOR -l select=1:ncpus=68:mcdram=cach:numa=quadrant -l walltime=0:20:00
 
 
