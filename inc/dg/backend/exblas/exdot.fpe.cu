451820598469a6716502a3c17c542473908d9ea1
Matthias <mattwi@fysik.dtu.dk>
2017-12-07 00:31:27 +0100

    final settings for gpu, made dynamic memory static

--- inc/dg/backend/exblas/exdot.fpe.cu
+++ inc/dg/backend/exblas/exdot.fpe.cu
@@ -23,9 +23,9 @@ static constexpr double DELTASCALE = double(1ull << DIGITS); // Assumes KRX>0
 static constexpr uint WARP_COUNT               = 16 ; //# of sub superaccs
 static constexpr uint WARP_SIZE                = 16 ; 
 static constexpr uint WORKGROUP_SIZE           = (WARP_COUNT * WARP_SIZE); //# threads per block
-static constexpr uint PARTIAL_SUPERACCS_COUNT  = 128; //# of groups; each has a partial SuperAcc (somehow does not work for 128???)
+static constexpr uint PARTIAL_SUPERACCS_COUNT  = 256; //# of groups; each has a partial SuperAcc (should not be larger than 512)
 //Kernel paramters for EXDOTComplete
-static constexpr uint MERGE_SUPERACCS_SIZE     = 128; //# of sa each block merges
+static constexpr uint MERGE_SUPERACCS_SIZE     = 64; //# of sa each block merges
 static constexpr uint MERGE_WORKGROUP_SIZE     = 64;  //we need only 39 of those
 
 
@@ -85,22 +85,8 @@ __device__ long long int xadd( long long int *sa, long long int x, unsigned char
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// Rounding functions
+// Normalize functions
 ////////////////////////////////////////////////////////////////////////////////
-__host__ __device__
-double OddRoundSumNonnegative_gpu(double th, double tl) {
-    union {
-        double d;
-        long long int l;
-    } thdb;
-
-    thdb.d = th + tl;
-    // - if the mantissa of th is odd, there is nothing to do
-    // - otherwise, round up as both tl and th are positive
-    // in both cases, this means setting the msb to 1 when tl>0
-    thdb.l |= (tl != 0.0);
-    return thdb.d;
-}
 
 __device__
 int Normalize( long long int *accumulator, int *imin, int *imax) {
@@ -139,52 +125,6 @@ int NormalizeT( long long int *accumulator, int *imin, int *imax) {
     accumulator[(*imax)*WARP_COUNT] += carry_in << DIGITS;
 
     return carry_in < 0;
-}
-
-__device__
-double Round( long long int *accumulator) {
-    int imin = 0;
-    int imax = 38;
-    int negative = Normalize(accumulator, &imin, &imax);
-
-    //Find leading word
-    int i;
-    //Skip zeroes
-    for (i = imax; accumulator[i] == 0 && i >= imin; --i) {
-    }
-    if (negative) {
-        //Skip ones
-        for(; (accumulator[i] & ((1l << DIGITS) - 1)) == ((1l << DIGITS) - 1) && i >= imin; --i) {
-        }
-    }
-    if (i < 0)
-        return 0.0;
-
-    long long int hiword = negative ? ((1l << DIGITS) - 1) - accumulator[i] : accumulator[i];
-    double rounded = (double) hiword;
-    double hi = ldexp(rounded, (i - F_WORDS) * DIGITS);
-    if (i == 0)
-        return negative ? -hi : hi;  // Correct rounding achieved
-    hiword -= (long long int) rint(rounded);
-    double mid = ldexp((double) hiword, (i - F_WORDS) * DIGITS);
-
-    //Compute sticky
-    long long int sticky = 0;
-    for (int j = imin; j != i - 1; ++j)
-        sticky |= negative ? (1l << DIGITS) - accumulator[j] : accumulator[j];
-
-    long long int loword = negative ? (1l << DIGITS) - accumulator[i - 1] : accumulator[i - 1];
-    loword |= !!sticky;
-    double lo = ldexp((double) loword, (i - 1 - F_WORDS) * DIGITS);
-
-    //Now add3(hi, mid, lo)
-    //No overlap, we have already normalized
-    if (mid != 0)
-        lo = OddRoundSumNonnegative_gpu(mid, lo);
-
-    //Final rounding
-    hi = hi + lo;
-    return negative ? -hi : hi;
 }
 
 
@@ -269,40 +209,24 @@ __global__ void ExDOT(
     //Read data from global memory and scatter it to sub-superaccs
     double a[NBFPE] = {0.0};
     for(uint pos = blockIdx.x*blockDim.x+threadIdx.x; pos < NbElements; pos += gridDim.x*blockDim.x) {
-            double x = d_a[pos]*d_b[pos];
+        double r = 0.0;
+        double x = TwoProductFMA(d_a[pos], d_b[pos], &r);
+
+        #pragma unroll
+        for(uint i = 0; i != NBFPE; ++i) {
+            double s;
+            a[i] = KnuthTwoSum(a[i], x, &s);
+            x = s;
+        }
+        if (x != 0.0) {
+            Accumulate(l_workingBase, x);
+            // Flush FPEs to superaccs
             #pragma unroll
             for(uint i = 0; i != NBFPE; ++i) {
-                double s;
-                a[i] = KnuthTwoSum(a[i], x, &s);
-                x = s;
+                Accumulate(l_workingBase, a[i]);
+                a[i] = 0.0;
             }
-            if (x != 0.0) {
-                Accumulate(l_workingBase, x);
-                // Flush FPEs to superaccs
-                #pragma unroll
-                for(uint i = 0; i != NBFPE; ++i) {
-                    Accumulate(l_workingBase, a[i]);
-                    a[i] = 0.0;
-                }
-            }
-        //double r = 0.0;
-        //double x = TwoProductFMA(d_a[pos], d_b[pos], &r);
-
-        //#pragma unroll
-        //for(uint i = 0; i != NBFPE; ++i) {
-        //    double s;
-        //    a[i] = KnuthTwoSum(a[i], x, &s);
-        //    x = s;
-        //}
-        //if (x != 0.0) {
-        //    Accumulate(l_workingBase, x);
-        //    // Flush FPEs to superaccs
-        //    #pragma unroll
-        //    for(uint i = 0; i != NBFPE; ++i) {
-        //        Accumulate(l_workingBase, a[i]);
-        //        a[i] = 0.0;
-        //    }
-        //}
+        }
 
         //if (r != 0.0) {//add the rest r in the same manner
         //    #pragma unroll
@@ -322,7 +246,7 @@ __global__ void ExDOT(
         //    }
         //}
     }
-	//Flush FPEs to superaccs
+    //Flush FPEs to superaccs
     #pragma unroll
     for(uint i = 0; i != NBFPE; ++i)
         Accumulate(l_workingBase, a[i]);
@@ -330,12 +254,12 @@ __global__ void ExDOT(
 
     //Merge sub-superaccs into work-group partial-accumulator ( ATTENTION: PartialSuperacc is transposed!)
     uint pos = threadIdx.x;
-//if(pos < WARP_COUNT) {
-//        int imin = 0;
-//        int imax = 38;
-//    NormalizeT( l_workingBase, &imin, &imax);
-//}
-//    __syncthreads();
+    if(pos < WARP_COUNT) {
+            int imin = 0;
+            int imax = 38;
+        NormalizeT( l_workingBase, &imin, &imax);
+    }
+    __syncthreads();
 
     if (pos < BIN_COUNT) {
         long long int sum = 0;
@@ -371,15 +295,20 @@ __global__ void ExDOT(
     //Read data from global memory and scatter it to sub-superaccs
     double a[NBFPE] = {0.0};
     for(uint pos = blockIdx.x*blockDim.x+threadIdx.x; pos < NbElements; pos += gridDim.x*blockDim.x) {
-            double x = d_a[pos]*d_c[pos]*d_b[pos];
+        //double x2 = d_a[pos]*d_c[pos]*d_b[pos];
+        double r  = 0.0, r2 = 0.0;
+        double x  = TwoProductFMA(d_a[pos], d_b[pos], &r);
+        double x2 = TwoProductFMA(x , d_c[pos], &r2);
+
+        if( x2 != 0.0) {//accumulate x2
             #pragma unroll
             for(uint i = 0; i != NBFPE; ++i) {
                 double s;
-                a[i] = KnuthTwoSum(a[i], x, &s);
-                x = s;
+                a[i] = KnuthTwoSum(a[i], x2, &s);
+                x2 = s;
             }
-            if (x != 0.0) {
-                Accumulate(l_workingBase, x);
+            if (x2 != 0.0) {
+                Accumulate(l_workingBase, x2);
                 // Flush FPEs to superaccs
                 #pragma unroll
                 for(uint i = 0; i != NBFPE; ++i) {
@@ -387,28 +316,7 @@ __global__ void ExDOT(
                     a[i] = 0.0;
                 }
             }
-        //double r  = 0.0, r2 = 0.0;
-        //double x  = TwoProductFMA(d_a[pos], d_b[pos], &r);
-        //double x2 = TwoProductFMA(x , d_c[pos], &r2);
-
-
-        //if( x2 != 0.0) {//accumulate x2
-        //    #pragma unroll
-        //    for(uint i = 0; i != NBFPE; ++i) {
-        //        double s;
-        //        a[i] = KnuthTwoSum(a[i], x2, &s);
-        //        x2 = s;
-        //    }
-        //    if (x2 != 0.0) {
-        //        Accumulate(l_workingBase, x2);
-        //        // Flush FPEs to superaccs
-        //        #pragma unroll
-        //        for(uint i = 0; i != NBFPE; ++i) {
-        //            Accumulate(l_workingBase, a[i]);
-        //            a[i] = 0.0;
-        //        }
-        //    }
-        //}
+        }
         //if (r2 != 0.0) {//add the rest r2 
         //    #pragma unroll
         //    for(uint i = 0; i != NBFPE; ++i) {
@@ -428,6 +336,7 @@ __global__ void ExDOT(
         //}
 
         //if (r != 0.0) {//add the rest r*c in the same manner
+        //    r2 = 0.0;
         //    x2 = TwoProductFMA(r , d_c[pos], &r2);
         //    if( x2 != 0.0) {//accumulate x2
         //        #pragma unroll
@@ -473,6 +382,12 @@ __global__ void ExDOT(
 
     //Merge sub-superaccs into work-group partial-accumulator ( ATTENTION: PartialSuperacc is transposed!)
     uint pos = threadIdx.x;
+    if(pos<WARP_COUNT){
+            int imin = 0;
+            int imax = 38;
+            NormalizeT(l_workingBase, &imin, &imax);
+        }
+    __syncthreads();
     if (pos < BIN_COUNT) {
         long long int sum = 0;
 
@@ -495,7 +410,6 @@ __global__ void ExDOT(
 ////////////////////////////////////////////////////////////////////////////////
 __global__
 void ExDOTComplete(
-     double *d_Res,
      long long int *d_PartialSuperaccs
 ) {
     uint lid = threadIdx.x;
@@ -525,22 +439,16 @@ void ExDOTComplete(
             sum += d_PartialSuperaccs[i * BIN_COUNT + lid];
 
         d_PartialSuperaccs[lid] = sum;
-
-        //__syncthreads();
-        //if (lid == 0)
-        //    d_Res[0] = Round(d_PartialSuperaccs);
     }
 }
 
 __host__
 std::vector<int64_t> exdot_gpu(unsigned size, const double* x1_ptr, const double* x2_ptr)
 {
-    thrust::device_vector<long long int> d_PartialSuperaccsV( PARTIAL_SUPERACCS_COUNT*BIN_COUNT); //39 columns and PSC rows
+    static thrust::device_vector<long long int> d_PartialSuperaccsV( PARTIAL_SUPERACCS_COUNT*BIN_COUNT, 0.0); //39 columns and PSC rows
     long long int *d_PartialSuperaccs = thrust::raw_pointer_cast( d_PartialSuperaccsV.data());
     ExDOT<<<PARTIAL_SUPERACCS_COUNT, WORKGROUP_SIZE>>>( d_PartialSuperaccs, x1_ptr, x2_ptr,size);
-    thrust::device_vector<double> r(1,0);
-    double *r_ptr = thrust::raw_pointer_cast( r.data());
-    ExDOTComplete<<<PARTIAL_SUPERACCS_COUNT/MERGE_SUPERACCS_SIZE, MERGE_WORKGROUP_SIZE>>>( r_ptr, d_PartialSuperaccs );
+    ExDOTComplete<<<PARTIAL_SUPERACCS_COUNT/MERGE_SUPERACCS_SIZE, MERGE_WORKGROUP_SIZE>>>( d_PartialSuperaccs );
     std::vector<int64_t> h_Superacc(BIN_COUNT);
     cudaMemcpy( &h_Superacc[0], d_PartialSuperaccs, BIN_COUNT*sizeof(long long int), cudaMemcpyDeviceToHost);
     return h_Superacc;
@@ -548,14 +456,12 @@ std::vector<int64_t> exdot_gpu(unsigned size, const double* x1_ptr, const double
 __host__
 std::vector<int64_t> exdot_gpu(unsigned size, const double* x1_ptr, const double* x2_ptr, const double* x3_ptr)
 {
-    thrust::device_vector<long long int> d_PartialSuperaccsV( PARTIAL_SUPERACCS_COUNT*BIN_COUNT); //39 columns and PSC rows
+    static thrust::device_vector<long long int> d_PartialSuperaccsV( PARTIAL_SUPERACCS_COUNT*BIN_COUNT, 0.0); //39 columns and PSC rows
     long long int *d_PartialSuperaccs = thrust::raw_pointer_cast( d_PartialSuperaccsV.data());
     ExDOT<<<PARTIAL_SUPERACCS_COUNT, WORKGROUP_SIZE>>>( d_PartialSuperaccs, x1_ptr, x2_ptr, x3_ptr,size);
-    thrust::device_vector<double> r(1,0);
-    double *r_ptr = thrust::raw_pointer_cast( r.data());
-    ExDOTComplete<<<PARTIAL_SUPERACCS_COUNT/MERGE_SUPERACCS_SIZE, MERGE_WORKGROUP_SIZE>>>( r_ptr, d_PartialSuperaccs );
+    ExDOTComplete<<<PARTIAL_SUPERACCS_COUNT/MERGE_SUPERACCS_SIZE, MERGE_WORKGROUP_SIZE>>>( d_PartialSuperaccs );
 
-    std::vector<int64_t> h_Superacc(BIN_COUNT, 1);
+    std::vector<int64_t> h_Superacc(BIN_COUNT);
     cudaMemcpy( &h_Superacc[0], d_PartialSuperaccs, BIN_COUNT*sizeof(long long int), cudaMemcpyDeviceToHost);
     return h_Superacc;
 }
