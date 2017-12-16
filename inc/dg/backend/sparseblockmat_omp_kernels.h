3b934aec8d937b1af02693d7796f5d4a546eac9f
Matthias <mattwi@fysik.dtu.dk>
2017-12-16 13:57:57 +0100

    reverted back to original sparseblockmat_omp_kernels and added nowait clause

--- inc/dg/backend/sparseblockmat_omp_kernels.h
+++ inc/dg/backend/sparseblockmat_omp_kernels.h
@@ -52,7 +52,7 @@ void ell_multiply_kernel3( value_type alpha, value_type beta,
     {
         if( right_size==1)
         {
-            #pragma omp for nowait
+            #pragma omp for nowait 
             for( int s=0; s<left_size; s++)
             for( int i=0; i<num_rows; i++)
             {
@@ -92,7 +92,7 @@ void ell_multiply_kernel3( value_type alpha, value_type beta,
     }
     else
     {
-        #pragma omp for collapse(2) nowait
+#pragma omp for collapse(2) nowait
         for( int s=0; s<left_size; s++)
         for( int i=0; i<num_rows; i++)
         for( int k=0; k<3; k++)
@@ -126,99 +126,37 @@ void ell_multiply_kernel33( value_type alpha, value_type beta,
          const int num_rows, const int num_cols, 
          const int left_size, const int right_size, 
          const int* right_range,
-         const value_type * RESTRICT x, value_type * RESTRICT y, bool trivial
+         const value_type * RESTRICT x, value_type * RESTRICT y
          )
 {
+    bool trivial = true;
+    for( int i=1; i<num_rows-1; i++)
+        for( int d=0; d<3; d++)
+        {
+            if( data_idx[i*3+d] != d) trivial = false;
+            //if( cols_idx[i*3+d] != i+d-1) trivial = false;
+        }
     if( trivial)
     {
-    #pragma omp for nowait
-	for( int s=0; s<left_size; s++)
-	{
-		for( int i=0; i<1; i++)
-		{
-            //(s*num_cols+i+0-1)*3;
-            //(s*num_cols+i+1-1)*3;
-            //(s*num_cols+i+2-1)*3;
-            int J0 = s*num_cols+cols_idx[i*3+0]*3;
-            int J1 = s*num_cols+cols_idx[i*3+1]*3;
-            int J2 = s*num_cols+cols_idx[i*3+2]*3;
-			for( int k=0; k<3; k++)
-			{
-				int B0 = (data_idx[i*3+0]*3+k)*3;
-                int B1 = (data_idx[i*3+1]*3+k)*3;
-                int B2 = (data_idx[i*3+2]*3+k)*3;
-				for( int j=right_range[0]; j<right_range[1]; j++)
-				{
-				    value_type temp = 0;
-				    temp +=data[ B0+0]* x[(J0+0)*right_size+j];
-				    temp +=data[ B0+1]* x[(J0+1)*right_size+j];
-				    temp +=data[ B0+2]* x[(J0+2)*right_size+j];
-				
-				    temp +=data[ B1+0]* x[(J1+0)*right_size+j];
-				    temp +=data[ B1+1]* x[(J1+1)*right_size+j];
-				    temp +=data[ B1+2]* x[(J1+2)*right_size+j];
-				
-				    temp +=data[ B2+0]* x[(J2+0)*right_size+j];
-				    temp +=data[ B2+1]* x[(J2+1)*right_size+j];
-				    temp +=data[ B2+2]* x[(J2+2)*right_size+j];
-				    int I = ((s*num_rows + i)*3+k)*right_size+j;
-                    y[I]=alpha*temp+beta*y[I];
-				}
-			}
-		}
-	}
 	#pragma omp for collapse(2) nowait
 	for( int s=0; s<left_size; s++)
 	{
-		for( int i=1; i<num_rows-1; i++)
+		for( int i=0; i<num_rows; i++)
 		{
-            int J0 = s*num_cols+cols_idx[i*3+0]*3;
-            int J1 = s*num_cols+cols_idx[i*3+1]*3;
-            int J2 = s*num_cols+cols_idx[i*3+2]*3;
+            //int J0 = (i==0 || i==num_rows-1)?(s*num_cols+cols_idx[i*3+0])*3:(s*num_cols+i+0-1)*3;
+            //int J1 = (i==0 || i==num_rows-1)?(s*num_cols+cols_idx[i*3+1])*3:(s*num_cols+i+1-1)*3;
+            //int J2 = (i==0 || i==num_rows-1)?(s*num_cols+cols_idx[i*3+2])*3:(s*num_cols+i+2-1)*3;
+            int J0 = (i==0 || i==num_rows-1)?(s*num_cols+cols_idx[i*3+0])*3:(s*num_cols+cols_idx[i*3+0])*3;
+            int J1 = (i==0 || i==num_rows-1)?(s*num_cols+cols_idx[i*3+1])*3:(s*num_cols+cols_idx[i*3+1])*3;
+            int J2 = (i==0 || i==num_rows-1)?(s*num_cols+cols_idx[i*3+2])*3:(s*num_cols+cols_idx[i*3+2])*3;
 			for( int k=0; k<3; k++)
 			{
-				int B0 = (0*3+k)*3;
-                int B1 = (1*3+k)*3;
-                int B2 = (2*3+k)*3;
+				int B0 = (i==0 || i==num_rows-1)?(data_idx[i*3+0]*3+k)*3:(0*3+k)*3;
+                int B1 = (i==0 || i==num_rows-1)?(data_idx[i*3+1]*3+k)*3:(1*3+k)*3;
+                int B2 = (i==0 || i==num_rows-1)?(data_idx[i*3+2]*3+k)*3:(2*3+k)*3;
 #if defined(__INTEL_COMPILER)
 				#pragma vector nontemporal(y)
 #endif //__INTEL_COMPILER
-				for( int j=right_range[0]; j<right_range[1]; j++)
-				{
-				    value_type temp = 0;
-				    temp +=data[ B0+0]* x[(J0+0)*right_size+j];
-				    temp +=data[ B0+1]* x[(J0+1)*right_size+j];
-				    temp +=data[ B0+2]* x[(J0+2)*right_size+j];
-				
-				    temp +=data[ B1+0]* x[(J1+0)*right_size+j];
-				    temp +=data[ B1+1]* x[(J1+1)*right_size+j];
-				    temp +=data[ B1+2]* x[(J1+2)*right_size+j];
-				
-				    temp +=data[ B2+0]* x[(J2+0)*right_size+j];
-				    temp +=data[ B2+1]* x[(J2+1)*right_size+j];
-				    temp +=data[ B2+2]* x[(J2+2)*right_size+j];
-				    int I = ((s*num_rows + i)*3+k)*right_size+j;
-                    y[I]=alpha*temp+beta*y[I];
-				}
-			}
-		}
-	}
-#pragma omp for nowait
-	for( int s=0; s<left_size; s++)
-	{
-		for( int i=num_rows-1; i<num_rows; i++)
-		{
-            //(s*num_cols+i+0-1)*3;
-            //(s*num_cols+i+1-1)*3;
-            //(s*num_cols+i+2-1)*3;
-            int J0 = s*num_cols+cols_idx[i*3+0]*3;
-            int J1 = s*num_cols+cols_idx[i*3+1]*3;
-            int J2 = s*num_cols+cols_idx[i*3+2]*3;
-			for( int k=0; k<3; k++)
-			{
-				int B0 = (data_idx[i*3+0]*3+k)*3;
-                int B1 = (data_idx[i*3+1]*3+k)*3;
-                int B2 = (data_idx[i*3+2]*3+k)*3;
 				for( int j=right_range[0]; j<right_range[1]; j++)
 				{
 				    value_type temp = 0;
@@ -251,11 +189,20 @@ void ell_multiply_kernel32( value_type alpha, value_type beta,
          const int num_rows, const int num_cols,
          const int left_size, const int right_size, 
          const int * RESTRICT right_range,
-         const value_type * RESTRICT x, value_type * RESTRICT y,
-         bool directional
+         const value_type * RESTRICT x, value_type * RESTRICT y
          )
 {
-    if( directional)
+    bool forward = true, backward = true;
+    for( int i=1; i<num_rows-1; i++)
+        for( int d=0; d<2; d++)
+        {
+            if( data_idx[i*2+d] != d) forward = backward = false;
+            //if( cols_idx[i*2+d] != i+d-1) backward = false;
+            //if( cols_idx[i*2+d] != i+d) forward = false;
+        }
+    //int diff = -1;
+    //if(forward ) diff = 0;
+    if( forward || backward )
     {
 #pragma omp for  nowait
     for( int s=0; s<left_size; s++)
@@ -332,17 +279,23 @@ void ell_multiply_kernel33x( value_type alpha, value_type beta,
          const value_type * RESTRICT data, const int * RESTRICT cols_idx, const int * RESTRICT data_idx, 
          const int num_rows, const int num_cols,
          const int left_size,
-         const value_type * RESTRICT x, value_type * RESTRICT y,
-         bool trivial
+         const value_type * RESTRICT x, value_type * RESTRICT y
          )
 {
+    bool trivial = true;
+    for( int i=1; i<num_rows-1; i++)
+        for( int d=0; d<3; d++)
+        {
+            if( data_idx[i*3+d] != d) trivial = false;
+            if( cols_idx[i*3+d] != i+d-1) trivial = false;
+        }
     if( trivial)
     {
-        //value_type data_[27];
-        //for( int k=0; k<3; k++)
-        //for( int i=0; i<3; i++)
-        //for( int j=0; j<3; j++)
-        //    data_[(i*3+k)*3+j] = data[(k*3+i)*3+j];
+        value_type data_[27];
+        for( int k=0; k<3; k++)
+        for( int i=0; i<3; i++)
+        for( int j=0; j<3; j++)
+            data_[(i*3+k)*3+j] = data[(k*3+i)*3+j];
 #pragma omp for nowait
     for( int s=0; s<left_size; s++)
     {
@@ -372,21 +325,21 @@ void ell_multiply_kernel33x( value_type alpha, value_type beta,
     }
     for( int i=1; i<num_rows-1; i++)
     {
-        //int J0 = (s*num_cols+i+0-1)*3;
-        int J0 = (s*num_cols+cols_idx[i*3+0])*3;
+        //int J0 = (s*num_cols+cols_idx[i*3+0])*3;
+        int J0 = (s*num_cols+i+0-1)*3;
         for( int k=0; k<3; k++)
         {
             value_type temp = 0;
             int B0 = 9*k;
-            temp +=data[ B0+0]* x[(J0+0)];
-            temp +=data[ B0+1]* x[(J0+1)];
-            temp +=data[ B0+2]* x[(J0+2)];
-            temp +=data[ B0+3]* x[(J0+3)];
-            temp +=data[ B0+4]* x[(J0+4)];
-            temp +=data[ B0+5]* x[(J0+5)];
-            temp +=data[ B0+6]* x[(J0+6)];
-            temp +=data[ B0+7]* x[(J0+7)];
-            temp +=data[ B0+8]* x[(J0+8)];
+            temp +=data_[ B0+0]* x[(J0+0)];
+            temp +=data_[ B0+1]* x[(J0+1)];
+            temp +=data_[ B0+2]* x[(J0+2)];
+            temp +=data_[ B0+3]* x[(J0+3)];
+            temp +=data_[ B0+4]* x[(J0+4)];
+            temp +=data_[ B0+5]* x[(J0+5)];
+            temp +=data_[ B0+6]* x[(J0+6)];
+            temp +=data_[ B0+7]* x[(J0+7)];
+            temp +=data_[ B0+8]* x[(J0+8)];
 
             int I = ((s*num_rows + i)*3+k);
             y[I]=alpha*temp+beta*y[I];
@@ -432,13 +385,20 @@ void ell_multiply_kernel32x( value_type alpha, value_type beta,
          const value_type * RESTRICT data, const int * RESTRICT cols_idx, const int * RESTRICT data_idx, 
          const int num_rows, const int num_cols,
          const int left_size, 
-         const value_type * RESTRICT x, value_type * RESTRICT y,
-         bool directional
+         const value_type * RESTRICT x, value_type * RESTRICT y
          )
 {
+    bool forward = true, backward = true;
+    for( int i=1; i<num_rows-1; i++)
+        for( int d=0; d<2; d++)
+        {
+            if( data_idx[i*2+d] != d) forward = backward = false;
+            //if( cols_idx[i*2+d] != i+d-1) backward = false;
+            if( cols_idx[i*2+d] != cols_idx[i*2]+d) forward = backward = false;
+        }
     //int diff = -1;
     //if(forward ) {diff = 0; }
-    if( directional )
+    if( forward || backward )
     {
         value_type data_[18];
         for( int k=0; k<2; k++)
@@ -526,16 +486,16 @@ void EllSparseBlockMatDevice<value_type>::launch_multiply_kernel( value_type alp
         if( blocks_per_line == 3)
         {
             if( right_size == 1)
-                ell_multiply_kernel33x<value_type> ( alpha, beta, data_ptr, cols_ptr, block_ptr, num_rows, num_cols, left_size, x_ptr,y_ptr,m_trivial);
+                ell_multiply_kernel33x<value_type> ( alpha, beta, data_ptr, cols_ptr, block_ptr, num_rows, num_cols, left_size, x_ptr,y_ptr);
             else
-                ell_multiply_kernel33<value_type> ( alpha, beta, data_ptr, cols_ptr, block_ptr, num_rows, num_cols, left_size, right_size, right_range_ptr,  x_ptr,y_ptr, m_trivial);
+                ell_multiply_kernel33<value_type> ( alpha, beta, data_ptr, cols_ptr, block_ptr, num_rows, num_cols, left_size, right_size, right_range_ptr,  x_ptr,y_ptr);
         }
         else if( blocks_per_line == 2)
         {
             if( right_size == 1)
-                ell_multiply_kernel32x<value_type> ( alpha, beta, data_ptr, cols_ptr, block_ptr, num_rows, num_cols, left_size,  x_ptr,y_ptr, m_directional);
+                ell_multiply_kernel32x<value_type> ( alpha, beta, data_ptr, cols_ptr, block_ptr, num_rows, num_cols, left_size,  x_ptr,y_ptr);
             else
-                ell_multiply_kernel32<value_type> ( alpha, beta, data_ptr, cols_ptr, block_ptr, num_rows, num_cols, left_size, right_size, right_range_ptr, x_ptr,y_ptr, m_directional);
+                ell_multiply_kernel32<value_type> ( alpha, beta, data_ptr, cols_ptr, block_ptr, num_rows, num_cols, left_size, right_size, right_range_ptr,  x_ptr,y_ptr);
         }
         else
             ell_multiply_kernel3<value_type>(alpha, beta,  
