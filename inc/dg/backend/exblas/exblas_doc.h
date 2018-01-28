707e76764ef7f38cee6e5833933f4bc68ee0f48a
Matthias <mattwi@fysik.dtu.dk>
2018-01-28 15:28:28 +0100

    added exblas_doc.h

---
+++ inc/dg/backend/exblas/exblas_doc.h
@@ -0,0 +1,15 @@
+#error Documentation only
+/*! @mainpage
+ *
+ * This version of exblas is an adaption of the original exblas library by Roman Iakymchuk and Sylvain Collange  for FELTOR
+ */
+/*! @namespace exblas
+ * @brief This is the namespace for all functions and 
+ * classes defined and used in the exblas library
+ * @namespace gpu
+ * @namespace cpu
+ */
+/*! 
+ * @defgroup highlevel High-level functions
+ * @defgroup lowlevel Low-level functions to be used in internal algorithms
+ */
