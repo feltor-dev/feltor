a3df78018bcdd8d197ccde37ad03713812d1b59b
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2018-02-03 17:41:50 +0100

    adding fimf-arch-consistency=true to marconi makes evaluation_t pass

--- config/marconi.mk
+++ config/marconi.mk
@@ -5,7 +5,7 @@ MPICC=mpiicc
 OPT=-O3 -xHost  # overwritten for mic in devices.mk
 #MPICFLAGS+= -DMPICH_IGNORE_CXX_SEEK
 OMPFLAG=-qopenmp
-CFLAGS=-Wall -std=c++11 -restrict -fp-model precise #-fimf-arch-consistency=true #-mfma  #flags for CC
+CFLAGS=-Wall -std=c++11 -restrict -fp-model precise -fimf-arch-consistency=true #-mfma  #flags for CC
 
 INCLUDE += -I$(HOME)/include # cusp, thrust
 INCLUDE += -I$(NETCDF_INC) -I$(HDF5_INC)
