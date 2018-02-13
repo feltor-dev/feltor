184d447a455450b687b7ed87771f46a30cd37297
Matthias Wiesenberger <Matthias.Wiesenberger@uibk.ac.at>
2018-02-13 22:45:02 +0100

    changed y derivative slightly to increase parallel size

--- inc/dg/backend/sparseblockmat_omp_kernels.h
+++ inc/dg/backend/sparseblockmat_omp_kernels.h
@@ -193,36 +193,33 @@ void ell_multiply_kernel( value_type alpha, value_type beta,
     else // right_size != 1
     {
     value_type dprivate[blocks_per_line*n];
-#pragma omp for collapse(2) nowait
+    int J[blocks_per_line];
+#pragma omp for collapse(3) nowait
     for( int s=0; s<left_size; s++)
     for( int i=0; i<num_rows; i++)
+    for( int k=0; k<n; k++)
     {
-        int J[blocks_per_line];
         for( int d=0; d<blocks_per_line; d++)
-            J[d] = (s*num_cols+cols_idx[i*blocks_per_line+d])*n;
-        for( int k=0; k<n; k++)
         {
+            J[d] = (s*num_cols+cols_idx[i*blocks_per_line+d])*n;
+            int B = (data_idx[i*blocks_per_line+d]*n+k)*n;
+            for(int q=0; q<n; q++)
+                dprivate[d*n+q] = data[B+q];
+        }
+#pragma omp SIMD //very important for KNL
+        for( int j=right_range[0]; j<right_range[1]; j++)
+        {
+            int I = ((s*num_rows + i)*n+k)*right_size+j;
+            y[I]*= beta;
             for( int d=0; d<blocks_per_line; d++)
             {
-                int B = (data_idx[i*blocks_per_line+d]*n+k)*n;
-                for(int q=0; q<n; q++)
-                    dprivate[d*n+q] = data[B+q];
-            }
-#pragma omp SIMD //very important for KNL
-            for( int j=right_range[0]; j<right_range[1]; j++)
-            {
-                int I = ((s*num_rows + i)*n+k)*right_size+j;
-                y[I]*= beta;
-                for( int d=0; d<blocks_per_line; d++)
-                {
-                    value_type temp = 0;
-                    int Jd = J[d];
-                    for( int q=0; q<n; q++) //multiplication-loop
-                        temp = DG_FMA( dprivate[ d*n+q],
-                                    x[(Jd+q)*right_size+j],
-                                    temp);
-                    y[I] = DG_FMA(alpha, temp, y[I]);
-                }
+                value_type temp = 0;
+                int Jd = J[d];
+                for( int q=0; q<n; q++) //multiplication-loop
+                    temp = DG_FMA( dprivate[ d*n+q],
+                                x[(Jd+q)*right_size+j],
+                                temp);
+                y[I] = DG_FMA(alpha, temp, y[I]);
             }
         }
     }
