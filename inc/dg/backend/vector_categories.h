f45eddd8f6cd5d2ffa5a147baf7132cd0874ad19
mattwi <mattwi@fysik.dtu.dk>
2018-05-05 15:07:27 +0200

    some thoughts on compatible vectors in Documentation

--- inc/dg/backend/vector_categories.h
+++ inc/dg/backend/vector_categories.h
@@ -10,11 +10,24 @@ namespace dg{
 /**
  * @brief Vector Tag base class, indicates the basic Vector/container concept
  *
- * The vector tag indicates how the data in the vector has to be accessed. For example
- * how do we get the pointer to the first element? Is there a contiguous chunk of memory
- * or is it a Vector of Vectors?
+ * The vector tag has three functions.
+First, it indicates the fundamental datatype a vector class contains (typically a double).
+ Second, it describes how the data in a Vector type is layed out in memory. We distinguish between a simple, contiguous chunk of data in a shared memory system (dg::SharedVectorTag), a dataset that is
+part of a larger dataset on a distributed memory system (dg::MPIVectorTag), and
+a dataset that consists of a number of subsets (dg::VectorVectorTag).
+Both the MPIVectorTag and the VectorVectorTag allow recursion, that is
+for example a VectorVector can consist itself of many shared vectors or of many
+VectorVector again. The innermost type must always be a shared vector however.
+  The third function of the Vector tag is to describe how the data in the vector has to be accessed.For example
+ * how do we get the pointer to the first element, the size, or how to access the MPI communicator? This is described in Derived Tags from the fundamental
+Tags, e.g. the ThrustVectorTag.
  * @note in any case we assume that the class has a default constructor, is copyable/assignable and has a \c size and a \c swap member function
  * @note \c dg::VectorTraits<Vector> has member typedefs \c value_type, \c execution_policy, \c vector_category
+@attention Two instances of two different Vector types are usable in the same \c blas function if
+  - their tags derive from the same VectorTag class except AnyVectorTag ( the data size
+    and type, layout and communicator, is the same, but the access to it may differ)
+  - their execution policy tag is exactly the same
+
  */
 struct AnyVectorTag{};
 ///@}
@@ -44,7 +57,7 @@ struct MPIVectorTag     : public AnyVectorTag {};
  * @brief This tag indicates composition/recursion.
  *
  * This Tag indicates that a class is composed of an array of containers, i.e. a container of containers.
- * We assume that the bracket operator [] is defined to access the inner elements.
+ * We assume that the bracket \c operator[] is defined to access the inner elements and the \c size() function returns the number of elements.
  * @note The class must typedef \c value_type and VectorTraits must be specialized for this type.
  * @note Examples are \c std::vector<T> and \c std::array<T,N> where T is a non-primitive data type and N is the size of the array
  */
@@ -53,7 +66,7 @@ struct VectorVectorTag  : public AnyVectorTag {};
 struct ArrayVectorTag   : public VectorVectorTag{}; //!< \c std::array of containers
 
 /**
- * @brief Indicate that thrust - like members are available
+ * @brief Indicate that thrust - like member functions are available
  *
  * In detail these must be
  *  - resize()
