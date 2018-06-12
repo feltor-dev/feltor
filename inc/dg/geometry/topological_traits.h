78441a2c3fea216dad3af360b11c44ef55a8d3f9
mattwi <mattwi@fysik.dtu.dk>
2018-06-12 23:55:14 +0200

    Introduce real type in basic grids
    This is to enable the use of either float or double in the future and
    also contributes to the solution of the multiple translation unit
    problem.

--- inc/dg/geometry/topological_traits.h
+++ inc/dg/geometry/topological_traits.h
@@ -7,6 +7,7 @@ template <class Topology>
 struct TopologyTraits{
     typedef typename Topology::memory_category memory_category; //!<either shared or distributed
     typedef typename Topology::dimensionality dimensionality; //!< two-dimensional or three-dimensional
+    typedef typename Topology::value_type value_type; //!< value type
 };
 
 //memory categories
