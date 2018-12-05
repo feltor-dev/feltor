7762bfdb6044c25f95a6e5b20febc5472eacb0bc
Matthias <mattwi@fysik.dtu.dk>
2018-12-05 18:18:30 +0100

    Add convergence tables to ds
    also add the very useful python run script to generate the data

---
+++ inc/geometries/run_ds.py
@@ -0,0 +1,31 @@
+import json, sys, yaml
+import numpy as np
+import subprocess
+
+program='ds_t' # 'ds_t' 'ds_guenther_t' 'ds_curv_t'
+for m in [1,10,50]: # [1,10,50] [1,10,50] [1,10,100,1000]
+    file_name = 'out_'+program+str(m)+'.json'
+    f = open(file_name, "w")
+    print(file_name)
+    y = []
+    for i in [0,1,2,3,4,5]:
+        if program=='ds_curv_t' :
+            NR = 2
+            NZ = 30*np.round( 2**(2*i/3))
+            NP = 5*2**i
+            mR = 1
+            mZ = m
+        else :
+            NP = 5*2**i
+            NR = 6*np.round( 2**(2*i/3))
+            NZ=NR
+            mR=mZ=m
+        s = "%i %i %i %i %i %i "% (3,NR,NZ,NP,mR,mZ)
+        print( i, NR, NZ, NP)
+        p1 = subprocess.Popen(["echo",s+'./'+program], stdout = subprocess.PIPE)
+        p2 = subprocess.Popen(['./'+program], stdin=p1.stdout, stdout = subprocess.PIPE)
+        p1.stdout.close()
+        y.append( yaml.safe_load( p2.stdout.read()))
+
+    json.dump( y, f, sort_keys=True, indent=2)
+    f.close()
