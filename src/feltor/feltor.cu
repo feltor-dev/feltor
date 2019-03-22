966c4739f60ec02bc823e8d988561f5362b54d9a
mattwi <mattwi@fysik.dtu.dk>
2019-03-22 21:23:30 +0100

    Correct Doku of nrmb_correction in CG

--- src/feltor/feltor.cu
+++ src/feltor/feltor.cu
@@ -132,7 +132,9 @@ int main( int argc, char* argv[])
     dg::blas1::axpby( 1., dg::construct<dg::DVec>(ntilde), 1., y0[0][0]);
     std::cout << "initialize ni" << std::endl;
     if( p.initphi == "zero")
+    {
         feltor.initializeni( y0[0][0], y0[0][1]);
+    }
     else if( p.initphi == "balance")
         dg::blas1::copy( y0[0][0], y0[0][1]); //set N_i = n_e
     else
@@ -140,6 +142,7 @@ int main( int argc, char* argv[])
 
     dg::blas1::copy( 0., y0[1][0]); //set we = 0
     dg::blas1::copy( 0., y0[1][1]); //set Wi = 0
+    std::cout << "Initialize Timestepper" << std::endl;
 
     ////////////////////////create timer and timestepper
     //
@@ -151,6 +154,7 @@ int main( int argc, char* argv[])
             dg::CylindricalGrid3d, dg::IDMatrix, dg::DMatrix, dg::DVec>
         > karniadakis( grid, p, mag);
     karniadakis.init( feltor, im, time, y0, p.dt);
+    std::cout << "Done!" << std::endl;
 
     //dg::Adaptive< dg::ERKStep<std::array<std::array<dg::DVec,2>,2>> > adaptive(
     //    "Bogacki-Shampine-4-2-3", y0);
