ae9793eeb7c39b20a580d2be06f1f44d766647b0
Matthias <mattwi@fysik.dtu.dk>
2019-11-18 15:26:04 +0100

    Aslaks BICGSTAB and LGMRES methods

--- inc/dg/cg2d_t.cu
+++ inc/dg/cg2d_t.cu
@@ -2,6 +2,8 @@
 #include <iomanip>
 
 #include "cg.h"
+#include "bicgstabl.h"
+#include "lgmres.h"
 #include "elliptic.h"
 
 const double lx = 2.*M_PI;
@@ -11,6 +13,37 @@ const double ly = 2.*M_PI;
 double fct(double x, double y){ return sin(y)*sin(x);}
 double laplace_fct( double x, double y) { return 2*sin(y)*sin(x);}
 double initial( double x, double y) {return sin(0);}
+
+template<class Matrix, class Container>
+void solve( std::string solver, Matrix& A, Container& x, const Container& b, const dg::Grid2d& grid)
+{
+    unsigned n = grid.n(), Nx = grid.Nx(), Ny = grid.Ny();
+    if( "cheby" == solver)
+    {
+        dg::ChebyshevIteration<Container> cheby( x);
+        double lmin = 1+1, lmax = n*n*Nx*Nx + n*n*Ny*Ny; //Eigenvalues of Laplace
+        double hxhy = lx*ly/(n*n*Nx*Ny);
+        lmin *= hxhy, lmax *= hxhy; //we multiplied the matrix by w2d
+        //std::cout << "Type number of Chebyshev iterations\n";
+        unsigned num_iter =100;
+        //std::cin >> num_iter;
+        cheby.solve( A, x, b, lmin, lmax, num_iter);
+        std::cout << "After "<<num_iter<<" Chebyshev iterations we have:\n";
+    }
+    if( "bicgstabl" == solver)
+    {
+        dg::BICGSTABl<Container> bicg( x, 100, 2);
+        unsigned num_iter = bicg.solve( A, x, b, A.precond(), A.inv_weights(), 1e-6);
+        std::cout << "After "<<num_iter<<" BICGSTABl iterations we have:\n";
+    }
+    if( "lgmres" == solver)
+    {
+        dg::LGMRES<Container> lgmres( x, 30, 4, 10000);
+        unsigned num_iter = lgmres.solve( A, x, b, A.precond(), A.inv_weights(), 1e-6);
+        std::cout << "After "<<num_iter<<" LGMRES iterations we have:\n";
+    }
+
+}
 
 int main()
 {
@@ -46,8 +79,9 @@ int main()
 
     // use inverse volume as preconditioner in solution method
     const double eps = 1e-6;
-    std::cout << "Number of pcg iterations "<< pcg( A, x, b, v2d, eps)<<std::endl;
+    unsigned num_iter = pcg( A, x, b, v2d, eps);
 //! [doxygen]
+    std::cout << "Number of pcg iterations "<< num_iter<<std::endl;
     std::cout << "For a precision of "<< eps<<std::endl;
     //compute error
     dg::HVec error( solution);
@@ -67,32 +101,28 @@ int main()
     res.d = sqrt(dg::blas2::dot( w2d, resi));
     std::cout << "L2 Norm of Residuum is        " << res.d<<"\t"<<res.i << std::endl;
     //Fehler der Integration des Sinus ist vernachlässigbar (vgl. evaluation_t)
-    dg::blas1::copy( 0., x);
-    dg::ChebyshevIteration<dg::HVec> cheby( copyable_vector);
-    double lmin = 1+1, lmax = n*n*Nx*Nx + n*n*Ny*Ny; //Eigenvalues of Laplace
-    double hxhy = lx*ly/(n*n*Nx*Ny);
-    lmin *= hxhy, lmax *= hxhy; //we multiplied the matrix by w2d
-    std::cout << "Type number of Chebyshev iterations\n";
-    unsigned num_iter;
-    std::cin >> num_iter;
-    cheby.solve( A, x, b, lmin, lmax, num_iter);
-    std::cout << "After "<<num_iter<<" Chebyshev iterations we have:\n";
 
-    dg::blas1::copy( solution, error);
-    dg::blas1::axpby( 1.,x,-1.,error);
+    std::vector<std::string> solvers{ "cheby", "bicgstabl", "lgmres"};
+    for(auto solver : solvers)
+    {
+        dg::blas1::copy( 0., x);
+        solve( solver, A, x, b, grid);
+        dg::blas1::copy( solution, error);
+        dg::blas1::axpby( 1.,x,-1.,error);
 
-    dg::blas1::copy( b, resi);
-    dg::blas2::symv(  A, x, Ax);
-    dg::blas1::axpby( 1.,Ax,-1.,resi);
+        dg::blas1::copy( b, resi);
+        dg::blas2::symv(  A, x, Ax);
+        dg::blas1::axpby( 1.,Ax,-1.,resi);
 
-    res.d = sqrt(dg::blas2::dot( w2d, x));
-    std::cout << "L2 Norm of x0 is              " << res.d<<"\n";
-    res.d = sqrt(dg::blas2::dot(w2d , solution));
-    std::cout << "L2 Norm of Solution is        " << res.d<<"\n";
-    res.d = sqrt(dg::blas2::dot(w2d , error));
-    std::cout << "L2 Norm of Error is           " << res.d<<"\n";
-    res.d = sqrt(dg::blas2::dot( w2d, resi));
-    std::cout << "L2 Norm of Residuum is        " << res.d<<"\n";
+        res.d = sqrt(dg::blas2::dot( w2d, x));
+        std::cout << "L2 Norm of x0 is              " << res.d<<"\n";
+        res.d = sqrt(dg::blas2::dot(w2d , solution));
+        std::cout << "L2 Norm of Solution is        " << res.d<<"\n";
+        res.d = sqrt(dg::blas2::dot(w2d , error));
+        std::cout << "L2 Norm of Error is           " << res.d<<"\n";
+        res.d = sqrt(dg::blas2::dot( w2d, resi));
+        std::cout << "L2 Norm of Residuum is        " << res.d<<"\n\n";
+    }
 
 
     return 0;
