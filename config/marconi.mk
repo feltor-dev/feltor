590ba7638260a9ea5cc259ceeb714c6cffe1a292
Matthias <mattwi@fysik.dtu.dk>
2020-05-05 21:40:33 +0200

    Changed default C++ version to C++-14
    This is the default as of gcc-6 so it's time to adapt
    Let's see how far we get and then actually use C++-14
    language features!

--- config/marconi.mk
+++ config/marconi.mk
@@ -2,10 +2,10 @@
 ifeq ($(strip $(HPC_SYSTEM)),marconi)
 CC=icc
 MPICC=mpiicc -mt_mpi
-OPT=-O3 -xHost  # overwritten for mic in devices.mk
+OPT=-O3 -xHost  # overwritten for skl in devices.mk
 #MPICFLAGS+= -DMPICH_IGNORE_CXX_SEEK
 OMPFLAG=-qopenmp
-CFLAGS=-Wall -std=c++11 -restrict -fp-model precise -fimf-arch-consistency=true #-mfma  #flags for CC
+CFLAGS=-Wall -std=c++14 -restrict -fp-model precise -fimf-arch-consistency=true #-mfma  #flags for CC
 
 INCLUDE += -I$(HOME)/include # cusp, thrust
 INCLUDE += -I$(NETCDF_INC) -I$(HDF5_INC)
@@ -20,15 +20,10 @@ endif
 #LIBS    +=-L$(NETCDF_LIB) -lnetcdf -lcurl
 #endif
 #############################modules to load in .bashrc#######################
-#module load profile/base                         
-#module load intel/pe-xe-2017--binary             
-#module load intelmpi/2017--binary                
-#module load szip/2.1--gnu--6.1.0                 
-#module load zlib/1.2.8--gnu--6.1.0               
-#module load hdf5/1.8.17--intelmpi--2017--binary  
-#module load netcdf/4.4.1--intelmpi--2017--binary 
-
-
-###########configure mic jobs#########################
-#export KMP_AFFINITY=scatter #important
-#srun --partition=knl_fua_prod --constraint="cache" --qos=knl_qos_fuadbg --account=FUA22_FELTOR --nodes=1 --time=0:30:00 --pty /bin/bash
+#module load profile/base
+#module load intel/pe-xe-2017--binary
+#module load intelmpi/2017--binary
+#module load szip/2.1--gnu--6.1.0
+#module load zlib/1.2.8--gnu--6.1.0
+#module load hdf5/1.8.17--intelmpi--2017--binary
+#module load netcdf/4.4.1--intelmpi--2017--binary
