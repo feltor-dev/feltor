23a005879fb92d529329057f75be3d157b24e978
Matthias <mattwi@fysik.dtu.dk>
2021-02-18 00:06:45 +0100

    Second Fix of MPI Curvilinear perp grid
    - the error was that the perp metric contained the 3rd dimension
    (is this an indication of a mis-design? Or a mis-use of the
    tensor::volume function?)

--- inc/geometries/curvilinear.h
+++ inc/geometries/curvilinear.h
@@ -89,7 +89,7 @@ struct RealCurvilinearGrid2d : public dg::aRealGeometry2d<real_type>
 {
     ///@copydoc hide_grid_parameters2d
     RealCurvilinearGrid2d( const aRealGenerator2d<real_type>& generator, unsigned n, unsigned Nx, unsigned Ny, dg::bc bcx=dg::DIR, bc bcy=dg::PER):
-        dg::aRealGeometry2d<real_type>( 0, generator.width(), 0., generator.height(), n, Nx, Ny, bcx, dg::PER), handle_(generator)
+        dg::aRealGeometry2d<real_type>( 0, generator.width(), 0., generator.height(), n, Nx, Ny, bcx, dg::PER), m_handle(generator)
     {
         construct( n,Nx,Ny);
     }
@@ -101,7 +101,7 @@ struct RealCurvilinearGrid2d : public dg::aRealGeometry2d<real_type>
     explicit RealCurvilinearGrid2d( RealCurvilinearProductGrid3d<real_type> g);
 
     ///read access to the generator
-    const aRealGenerator2d<real_type>& generator() const{return *handle_;}
+    const aRealGenerator2d<real_type>& generator() const{return *m_handle;}
     virtual RealCurvilinearGrid2d* clone()const override final{return new RealCurvilinearGrid2d(*this);}
     private:
     virtual void do_set(unsigned new_n, unsigned new_Nx, unsigned new_Ny) override final
@@ -111,15 +111,15 @@ struct RealCurvilinearGrid2d : public dg::aRealGeometry2d<real_type>
     }
     void construct( unsigned n, unsigned Nx, unsigned Ny);
     virtual SparseTensor<thrust::host_vector<real_type> > do_compute_jacobian( ) const override final{
-        return jac_;
+        return m_jac;
     }
     virtual SparseTensor<thrust::host_vector<real_type>> do_compute_metric( ) const override final{
-        return metric_;
+        return m_metric;
     }
-    virtual std::vector<thrust::host_vector<real_type>> do_compute_map()const override final{return map_;}
-    dg::SparseTensor<thrust::host_vector<real_type>> jac_, metric_;
-    std::vector<thrust::host_vector<real_type>> map_;
-    dg::ClonePtr<aRealGenerator2d<real_type>> handle_;
+    virtual std::vector<thrust::host_vector<real_type>> do_compute_map()const override final{return m_map;}
+    dg::SparseTensor<thrust::host_vector<real_type>> m_jac, m_metric;
+    std::vector<thrust::host_vector<real_type>> m_map;
+    dg::ClonePtr<aRealGenerator2d<real_type>> m_handle;
 };
 
 
@@ -138,15 +138,15 @@ struct RealCurvilinearProductGrid3d : public dg::aRealProductGeometry3d<real_typ
     RealCurvilinearProductGrid3d( const aRealGenerator2d<real_type>& generator, unsigned n, unsigned Nx, unsigned Ny, unsigned Nz, bc bcx=dg::DIR, bc bcy=dg::PER, bc bcz=dg::PER):
         dg::aRealProductGeometry3d<real_type>( 0, generator.width(), 0., generator.height(), 0., 2.*M_PI, n, Nx, Ny, Nz, bcx, bcy, bcz)
     {
-        map_.resize(3);
-        handle_ = generator;
+        m_map.resize(3);
+        m_handle = generator;
         constructPerp( n, Nx, Ny);
         constructParallel(Nz);
     }
 
 
     ///@copydoc RealCurvilinearGrid2d::generator()const
-    const aRealGenerator2d<real_type> & generator() const{return *handle_;}
+    const aRealGenerator2d<real_type> & generator() const{return *m_handle;}
     virtual RealCurvilinearProductGrid3d* clone()const override final{return new RealCurvilinearProductGrid3d(*this);}
     private:
     virtual RealCurvilinearGrid2d<real_type>* do_perp_grid() const override final;
@@ -159,22 +159,22 @@ struct RealCurvilinearProductGrid3d : public dg::aRealProductGeometry3d<real_typ
     //construct phi and lift rest to 3d
     void constructParallel(unsigned Nz)
     {
-        map_[2]=dg::evaluate(dg::cooZ3d, *this);
+        m_map[2]=dg::evaluate(dg::cooZ3d, *this);
         unsigned size = this->size();
         unsigned size2d = this->n()*this->n()*this->Nx()*this->Ny();
         //resize for 3d values
         for( unsigned r=0; r<6;r++)
-            jac_.values()[r].resize(size);
-        map_[0].resize(size);
-        map_[1].resize(size);
+            m_jac.values()[r].resize(size);
+        m_map[0].resize(size);
+        m_map[1].resize(size);
         //lift to 3D grid
         for( unsigned k=1; k<Nz; k++)
             for( unsigned i=0; i<size2d; i++)
             {
                 for(unsigned r=0; r<6; r++)
-                    jac_.values()[r][k*size2d+i] = jac_.values()[r][(k-1)*size2d+i];
-                map_[0][k*size2d+i] = map_[0][(k-1)*size2d+i];
-                map_[1][k*size2d+i] = map_[1][(k-1)*size2d+i];
+                    m_jac.values()[r][k*size2d+i] = m_jac.values()[r][(k-1)*size2d+i];
+                m_map[0][k*size2d+i] = m_map[0][(k-1)*size2d+i];
+                m_map[1][k*size2d+i] = m_map[1][(k-1)*size2d+i];
             }
     }
     //construct 2d plane
@@ -184,22 +184,22 @@ struct RealCurvilinearProductGrid3d : public dg::aRealProductGeometry3d<real_typ
         dg::Grid1d gY1d( this->y0(), this->y1(), n, Ny);
         thrust::host_vector<real_type> x_vec = dg::evaluate( dg::cooX1d, gX1d);
         thrust::host_vector<real_type> y_vec = dg::evaluate( dg::cooX1d, gY1d);
-        jac_ = SparseTensor< thrust::host_vector<real_type>>( x_vec);//unit tensor
-        jac_.values().resize( 6);
-        handle_->generate( x_vec, y_vec, map_[0], map_[1], jac_.values()[2], jac_.values()[3], jac_.values()[4], jac_.values()[5]);
-        jac_.idx(0,0) = 2, jac_.idx(0,1) = 3, jac_.idx(1,0)=4, jac_.idx(1,1) = 5;
+        m_jac = SparseTensor< thrust::host_vector<real_type>>( x_vec);//unit tensor
+        m_jac.values().resize( 6);
+        m_handle->generate( x_vec, y_vec, m_map[0], m_map[1], m_jac.values()[2], m_jac.values()[3], m_jac.values()[4], m_jac.values()[5]);
+        m_jac.idx(0,0) = 2, m_jac.idx(0,1) = 3, m_jac.idx(1,0)=4, m_jac.idx(1,1) = 5;
     }
     virtual SparseTensor<thrust::host_vector<real_type> > do_compute_jacobian( ) const override final{
-        return jac_;
+        return m_jac;
     }
     virtual SparseTensor<thrust::host_vector<real_type> > do_compute_metric( ) const override final
     {
-        return detail::square( jac_, map_[0], handle_->isOrthogonal());
+        return detail::square( m_jac, m_map[0], m_handle->isOrthogonal());
     }
-    virtual std::vector<thrust::host_vector<real_type> > do_compute_map()const override final{return map_;}
-    std::vector<thrust::host_vector<real_type> > map_;
-    SparseTensor<thrust::host_vector<real_type> > jac_;
-    dg::ClonePtr<aRealGenerator2d<real_type>> handle_;
+    virtual std::vector<thrust::host_vector<real_type> > do_compute_map()const override final{return m_map;}
+    std::vector<thrust::host_vector<real_type> > m_map;
+    SparseTensor<thrust::host_vector<real_type> > m_jac;
+    dg::ClonePtr<aRealGenerator2d<real_type>> m_handle;
 };
 
 using CurvilinearGrid2d         = dg::geo::RealCurvilinearGrid2d<double>;
@@ -215,19 +215,21 @@ using CurvilinearProductGrid3d  = CurvilinearProductGrid3d ;
 ///@cond
 template<class real_type>
 RealCurvilinearGrid2d<real_type>::RealCurvilinearGrid2d( RealCurvilinearProductGrid3d<real_type> g):
-    dg::aRealGeometry2d<real_type>( g.x0(), g.x1(), g.y0(), g.y1(), g.n(), g.Nx(), g.Ny(), g.bcx(), g.bcy() ), handle_(g.generator())
+    dg::aRealGeometry2d<real_type>( g.x0(), g.x1(), g.y0(), g.y1(), g.n(), g.Nx(), g.Ny(), g.bcx(), g.bcy() ), m_handle(g.generator())
 {
     g.set( this->n(), this->Nx(), this->Ny(), 1); //shouldn't trigger 2d grid generator
-    map_=g.map();
-    jac_=g.jacobian();
-    metric_=g.metric();
-    dg::blas1::copy( 1., metric_.values()[3]); //set pp to 1
-    map_.pop_back();
+    m_map=g.map();
+    m_jac=g.jacobian();
+    m_metric=g.metric();
+    // we rely on the fact that the 3d grid uses square to compute its metric
+    // so the (2,2) entry is value 3 that we need to set to 1
+    dg::blas1::copy( 1., m_metric.values()[3]);
+    m_map.pop_back();
 }
 template<class real_type>
 void RealCurvilinearGrid2d<real_type>::construct( unsigned n, unsigned Nx, unsigned Ny)
 {
-    RealCurvilinearProductGrid3d<real_type> g( *handle_, n,Nx,Ny,1,this->bcx());
+    RealCurvilinearProductGrid3d<real_type> g( *m_handle, n,Nx,Ny,1,this->bcx());
     *this = RealCurvilinearGrid2d<real_type>(g);
 }
 template<class real_type>
--- inc/geometries/mpi_curvilinear.h
+++ inc/geometries/mpi_curvilinear.h
@@ -30,7 +30,7 @@ struct RealCurvilinearMPIGrid2d : public dg::aRealMPIGeometry2d<real_type>
     /// @param comm a two-dimensional Cartesian communicator
     /// @note the paramateres given in the constructor are global parameters
     RealCurvilinearMPIGrid2d( const aRealGenerator2d<real_type>& generator, unsigned n, unsigned Nx, unsigned Ny, dg::bc bcx, dg::bc bcy, MPI_Comm comm):
-        dg::aRealMPIGeometry2d<real_type>( 0, generator.width(), 0., generator.height(), n, Nx, Ny, bcx, bcy, comm), handle_(generator)
+        dg::aRealMPIGeometry2d<real_type>( 0, generator.width(), 0., generator.height(), n, Nx, Ny, bcx, bcy, comm), m_handle(generator)
     {
         //generate global 2d grid and then reduce to local
         RealCurvilinearGrid2d<real_type> g(generator, n, Nx, Ny);
@@ -40,11 +40,11 @@ struct RealCurvilinearMPIGrid2d : public dg::aRealMPIGeometry2d<real_type>
     explicit RealCurvilinearMPIGrid2d( const RealCurvilinearProductMPIGrid3d<real_type>& g);
 
     ///read access to the generator
-    const aRealGenerator2d<real_type>& generator() const{return *handle_;}
+    const aRealGenerator2d<real_type>& generator() const{return *m_handle;}
     virtual RealCurvilinearMPIGrid2d* clone()const override final{return new RealCurvilinearMPIGrid2d(*this);}
     virtual RealCurvilinearGrid2d<real_type>* global_geometry()const override final{
         return new RealCurvilinearGrid2d<real_type>(
-                *handle_,
+                *m_handle,
                 global().n(), global().Nx(), global().Ny(),
                 global().bcx(), global().bcy());
     }
@@ -54,7 +54,7 @@ struct RealCurvilinearMPIGrid2d : public dg::aRealMPIGeometry2d<real_type>
     virtual void do_set( unsigned new_n, unsigned new_Nx, unsigned new_Ny) override final
     {
         dg::aRealMPITopology2d<real_type>::do_set(new_n, new_Nx, new_Ny);
-        RealCurvilinearGrid2d<real_type> g( *handle_, new_n, new_Nx, new_Ny);
+        RealCurvilinearGrid2d<real_type> g( *m_handle, new_n, new_Nx, new_Ny);
         divide_and_conquer(g);//distribute to processes
     }
     void divide_and_conquer(const RealCurvilinearGrid2d<real_type>& g_)
@@ -65,30 +65,31 @@ struct RealCurvilinearMPIGrid2d : public dg::aRealMPIGeometry2d<real_type>
         for( unsigned i=0; i<3; i++)
             for( unsigned j=0; j<3; j++)
             {
-                metric_.idx(i,j) = metric.idx(i,j);
-                jac_.idx(i,j) = jacobian.idx(i,j);
+                m_metric.idx(i,j) = metric.idx(i,j);
+                m_jac.idx(i,j) = jacobian.idx(i,j);
             }
-        jac_.values().resize( jacobian.values().size());
+        // Here we set the communicator implicitly
+        m_jac.values().resize( jacobian.values().size());
         for( unsigned i=0; i<jacobian.values().size(); i++)
-            jac_.values()[i] = global2local( jacobian.values()[i], *this);
-        metric_.values().resize( metric.values().size());
+            m_jac.values()[i] = global2local( jacobian.values()[i], *this);
+        m_metric.values().resize( metric.values().size());
         for( unsigned i=0; i<metric.values().size(); i++)
-            metric_.values()[i] = global2local( metric.values()[i], *this);
-        map_.resize(map.size());
+            m_metric.values()[i] = global2local( metric.values()[i], *this);
+        m_map.resize(map.size());
         for( unsigned i=0; i<map.size(); i++)
-            map_[i] = global2local( map[i], *this);
+            m_map[i] = global2local( map[i], *this);
     }
 
     virtual SparseTensor<MPI_Vector<thrust::host_vector<real_type>>> do_compute_jacobian( ) const override final{
-        return jac_;
+        return m_jac;
     }
     virtual SparseTensor<MPI_Vector<thrust::host_vector<real_type>>> do_compute_metric( ) const override final{
-        return metric_;
+        return m_metric;
     }
-    virtual std::vector<MPI_Vector<thrust::host_vector<real_type>>> do_compute_map()const override final{return map_;}
-    dg::SparseTensor<MPI_Vector<thrust::host_vector<real_type>>> jac_, metric_;
-    std::vector<MPI_Vector<thrust::host_vector<real_type>>> map_;
-    dg::ClonePtr<aRealGenerator2d<real_type>> handle_;
+    virtual std::vector<MPI_Vector<thrust::host_vector<real_type>>> do_compute_map()const override final{return m_map;}
+    dg::SparseTensor<MPI_Vector<thrust::host_vector<real_type>>> m_jac, m_metric;
+    std::vector<MPI_Vector<thrust::host_vector<real_type>>> m_map;
+    dg::ClonePtr<aRealGenerator2d<real_type>> m_handle;
 };
 
 /**
@@ -105,9 +106,9 @@ struct RealCurvilinearProductMPIGrid3d : public dg::aRealProductMPIGeometry3d<re
     /// @note the paramateres given in the constructor are global parameters
     RealCurvilinearProductMPIGrid3d( const aRealGenerator2d<real_type>& generator, unsigned n, unsigned Nx, unsigned Ny, unsigned Nz, bc bcx, bc bcy, bc bcz, MPI_Comm comm):
         dg::aRealProductMPIGeometry3d<real_type>( 0, generator.width(), 0., generator.height(), 0., 2.*M_PI, n, Nx, Ny, Nz, bcx, bcy, bcz, comm),
-        handle_( generator)
+        m_handle( generator)
     {
-        map_.resize(3);
+        m_map.resize(3);
         RealCurvilinearMPIGrid2d<real_type> g(generator,n,Nx,Ny, bcx, bcy, this->get_perp_comm());
         constructPerp( g);
         constructParallel(this->local().Nz());
@@ -115,11 +116,11 @@ struct RealCurvilinearProductMPIGrid3d : public dg::aRealProductMPIGeometry3d<re
 
 
     ///read access to the generator
-    const aRealGenerator2d<real_type>& generator() const{return *handle_;}
+    const aRealGenerator2d<real_type>& generator() const{return *m_handle;}
     virtual RealCurvilinearProductMPIGrid3d* clone()const{return new RealCurvilinearProductMPIGrid3d(*this);}
     virtual RealCurvilinearProductGrid3d<real_type>* global_geometry()const{
         return new RealCurvilinearProductGrid3d<real_type>(
-                *handle_,
+                *m_handle,
                 global().n(), global().Nx(), global().Ny(), global().Nz(),
                 global().bcx(), global().bcy(), global().bcz());
     }
@@ -132,20 +133,20 @@ struct RealCurvilinearProductMPIGrid3d : public dg::aRealProductMPIGeometry3d<re
         dg::aRealMPITopology3d<real_type>::do_set(new_n, new_Nx, new_Ny, new_Nz);
         if( !( new_n == this->n() && new_Nx == global().Nx() && new_Ny == global().Ny() ) )
         {
-            RealCurvilinearMPIGrid2d<real_type> g( *handle_,new_n,new_Nx,new_Ny, this->bcx(), this->bcy(), this->get_perp_comm());
+            RealCurvilinearMPIGrid2d<real_type> g( *m_handle,new_n,new_Nx,new_Ny, this->bcx(), this->bcy(), this->get_perp_comm());
             constructPerp( g);
         }
         constructParallel(this->local().Nz());
     }
     void constructPerp( RealCurvilinearMPIGrid2d<real_type>& g2d)
     {
-        jac_=g2d.jacobian();
-        map_=g2d.map();
+        m_jac=g2d.jacobian();
+        m_map=g2d.map();
     }
     void constructParallel( unsigned localNz )
     {
-        map_.resize(3);
-        map_[2]=dg::evaluate(dg::cooZ3d, *this);
+        m_map.resize(3);
+        m_map[2]=dg::evaluate(dg::cooZ3d, *this);
         unsigned size = this->local().size();
         unsigned size2d = this->n()*this->n()*this->local().Nx()*this->local().Ny();
         //resize for 3d values
@@ -153,55 +154,66 @@ struct RealCurvilinearProductMPIGrid3d : public dg::aRealProductMPIGeometry3d<re
         exblas::mpi_reduce_communicator( comm, &comm_mod, &comm_mod_reduce);
         for( unsigned r=0; r<6;r++)
         {
-            jac_.values()[r].data().resize(size);
-            jac_.values()[r].set_communicator( comm, comm_mod, comm_mod_reduce);
+            m_jac.values()[r].data().resize(size);
+            m_jac.values()[r].set_communicator( comm, comm_mod, comm_mod_reduce);
         }
-        map_[0].data().resize(size);
-        map_[0].set_communicator( comm, comm_mod, comm_mod_reduce);
-        map_[1].data().resize(size);
-        map_[1].set_communicator( comm, comm_mod, comm_mod_reduce);
+        m_map[0].data().resize(size);
+        m_map[0].set_communicator( comm, comm_mod, comm_mod_reduce);
+        m_map[1].data().resize(size);
+        m_map[1].set_communicator( comm, comm_mod, comm_mod_reduce);
         //lift to 3D grid
         for( unsigned k=1; k<localNz; k++)
             for( unsigned i=0; i<size2d; i++)
             {
                 for(unsigned r=0; r<6; r++)
-                    jac_.values()[r].data()[k*size2d+i] = jac_.values()[r].data()[(k-1)*size2d+i];
-                map_[0].data()[k*size2d+i] = map_[0].data()[(k-1)*size2d+i];
-                map_[1].data()[k*size2d+i] = map_[1].data()[(k-1)*size2d+i];
+                    m_jac.values()[r].data()[k*size2d+i] = m_jac.values()[r].data()[(k-1)*size2d+i];
+                m_map[0].data()[k*size2d+i] = m_map[0].data()[(k-1)*size2d+i];
+                m_map[1].data()[k*size2d+i] = m_map[1].data()[(k-1)*size2d+i];
             }
     }
     virtual SparseTensor<MPI_Vector<thrust::host_vector<real_type>>> do_compute_jacobian( ) const override final{
-        return jac_;
+        return m_jac;
     }
     virtual SparseTensor<MPI_Vector<thrust::host_vector<real_type>>> do_compute_metric( ) const override final{
-        return detail::square( jac_, map_[0], handle_->isOrthogonal());
+        return detail::square( m_jac, m_map[0], m_handle->isOrthogonal());
     }
-    virtual std::vector<MPI_Vector<thrust::host_vector<real_type>>> do_compute_map()const override final{return map_;}
-    dg::SparseTensor<MPI_Vector<thrust::host_vector<real_type>>> jac_;
-    std::vector<MPI_Vector<thrust::host_vector<real_type>>> map_;
-    ClonePtr<dg::geo::aRealGenerator2d<real_type>> handle_;
+    virtual std::vector<MPI_Vector<thrust::host_vector<real_type>>> do_compute_map()const override final{return m_map;}
+    dg::SparseTensor<MPI_Vector<thrust::host_vector<real_type>>> m_jac;
+    std::vector<MPI_Vector<thrust::host_vector<real_type>>> m_map;
+    ClonePtr<dg::geo::aRealGenerator2d<real_type>> m_handle;
 };
 ///@cond
 template<class real_type>
 RealCurvilinearMPIGrid2d<real_type>::RealCurvilinearMPIGrid2d( const RealCurvilinearProductMPIGrid3d<real_type>& g):
     dg::aRealMPIGeometry2d<real_type>( g.global().x0(), g.global().x1(), g.global().y0(), g.global().y1(), g.global().n(), g.global().Nx(), g.global().Ny(), g.global().bcx(), g.global().bcy(), g.get_perp_comm() ),
-    handle_(g.generator())
+    m_handle(g.generator())
 {
-    map_=g.map();
-    jac_=g.jacobian();
-    metric_=g.metric();
+    m_map=g.map();
+    m_jac=g.jacobian();
+    m_metric=g.metric();
     //now resize to 2d
-    map_.pop_back();
+    m_map.pop_back();
     unsigned s = this->local().size();
-    for( unsigned i=0; i<jac_.values().size(); i++)
-        jac_.values()[i].data().resize(s);
-    for( unsigned i=0; i<metric_.values().size(); i++)
-        metric_.values()[i].data().resize(s);
-    for( unsigned i=0; i<map_.size(); i++)
-        map_[i].data().resize(s);
-    jac_.set_communicator( this->communicator());
-    metric_.set_communicator( this->communicator());
-    map_.set_communicator( this->communicator());
+    MPI_Comm comm = g.get_perp_comm(), comm_mod, comm_mod_reduce;
+    exblas::mpi_reduce_communicator( comm, &comm_mod, &comm_mod_reduce);
+    for( unsigned i=0; i<m_jac.values().size(); i++)
+    {
+        m_jac.values()[i].data().resize(s);
+        m_jac.values()[i].set_communicator( comm, comm_mod, comm_mod_reduce);
+    }
+    for( unsigned i=0; i<m_metric.values().size(); i++)
+    {
+        m_metric.values()[i].data().resize(s);
+        m_metric.values()[i].set_communicator( comm, comm_mod, comm_mod_reduce);
+    }
+    // we rely on the fact that the 3d grid uses square to compute its metric
+    // so the (2,2) entry is value 3 that we need to set to 1
+    dg::blas1::copy( 1., m_metric.values()[3]);
+    for( unsigned i=0; i<m_map.size(); i++)
+    {
+        m_map[i].data().resize(s);
+        m_map[i].set_communicator( comm, comm_mod, comm_mod_reduce);
+    }
 }
 ///@endcond
 //
