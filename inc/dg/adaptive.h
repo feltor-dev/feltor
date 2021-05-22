ecd598fe76d8b4b720fd917bcd7ee2f50ad846b2
Matthias <mattwi@fysik.dtu.dk>
2021-05-22 17:41:37 +0200

    Fix Adaptive default constructor
    - parameter pack in constructor lead to MPI errors if default
    constructed

--- inc/dg/adaptive.h
+++ inc/dg/adaptive.h
@@ -179,6 +179,7 @@ struct Adaptive
     using stepper_type = Stepper;
     using container_type = typename Stepper::container_type; //!< the type of the vector class in use by \c Stepper
     using value_type = typename Stepper::value_type; //!< the value type of the time variable defined by \c Stepper (float or double)
+    Adaptive(){}
     /*!@brief Allocate workspace and construct stepper
      * @param ps All parameters are forwarded to the constructor of \c Stepper
      * @tparam StepperParams Type of parameters (deduced by the compiler)
