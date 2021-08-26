3f7339df9918b4a3ef1181b41a2342cb365e92b3
Matthias <mattwi@fysik.dtu.dk>
2021-08-26 09:58:45 +0200

    Add velocity-unstaggered in navier-stokes

--- src/navier_stokes/navier_stokes.h
+++ src/navier_stokes/navier_stokes.h
@@ -318,6 +318,104 @@ void Explicit<Geometry, IMatrix, Matrix, Container>::operator()(
                     m_minus, m_velocityST[1], m_plus, 1., yp[1][1]);
         }
     }
+    else if( "velocity-unstaggered" == advection)
+    {
+        dg::blas1::copy( y[0][1], m_density[0]);
+        dg::blas1::copy( y[0][1], m_density[1]);
+
+        dg::blas1::copy( y[1][1], m_velocity[0]);
+        dg::blas1::copy( y[1][1], m_velocity[1]);
+
+        dg::blas1::copy( 0., yp);
+
+        {
+        // Compute dsN and staggered density
+        m_faST( dg::geo::zeroMinus, m_density[1], m_minusSTN[1]);
+        m_faST( dg::geo::einsPlus,  m_density[1], m_plusSTN[1]);
+        update_parallel_bc_1st( m_minusSTN[1], m_plusSTN[1], m_p.bcxN, m_p.bcxN ==
+                dg::DIR ? m_p.nbc : 0.);
+        dg::geo::ds_slope( m_faST, 1., m_minusSTN[1], m_plusSTN[1], 0., m_dsN[1]);
+        dg::geo::ds_average( m_faST, 1., m_minusSTN[1], m_plusSTN[1], 0.,
+                m_densityST[1]);
+
+        // Compute dsU and velocity
+        m_faST( dg::geo::zeroMinus, m_velocity[1], m_minusSTU[1]);
+        m_faST( dg::geo::einsPlus,  m_velocity[1], m_plusSTU[1]);
+        update_parallel_bc_1st( m_minusSTU[1], m_plusSTU[1], m_p.bcxU, 0.);
+        dg::geo::ds_slope( m_faST, 1., m_minusSTU[1], m_plusSTU[1], 0., m_dsU[1]);
+        dg::geo::ds_average( m_faST, 1., m_minusSTU[1], m_plusSTU[1], 0.,
+                m_velocityST[1]);
+        }
+
+        // compute qhat
+        compute_parallel_flux( m_velocityST[1], m_minusSTN[1], m_plusSTN[1],
+                m_dsN[1], m_divNUb[1], m_p.slope_limiter);
+        m_faST( dg::geo::zeroPlus, m_divNUb[1], m_plus);
+        m_faST( dg::geo::einsMinus, m_divNUb[1], m_minus);
+        // We always use NEU for the fluxes for now
+        update_parallel_bc_1st( m_minus, m_plus, dg::NEU, 0.);
+        // Now compute divNUb and grad U^2/2
+        dg::geo::ds_slope( m_faST, 1., m_minus, m_plus, 0, m_divNUb[1]);
+        // Here the choice seems not to matter
+        dg::geo::ds_average( m_faST, 1., m_minus, m_plus, 0, m_temp0);
+        dg::blas1::pointwiseDot( 1., m_divb, m_temp0, 1., m_divNUb[1]);
+        //dg::blas1::pointwiseDot( 1., m_divb, m_density[1], m_velocity[1], 1.,
+        //        m_divNUb[1]);
+        dg::blas1::axpby( -1., m_divNUb[1], 1., yp[0][1]);
+
+        // compute fhat
+        compute_parallel_flux( m_velocityST[1], m_minusSTU[1], m_plusSTU[1],
+                m_dsU[1], m_temp0, m_p.slope_limiter);
+        m_faST( dg::geo::zeroPlus, m_temp0, m_plus);
+        m_faST( dg::geo::einsMinus, m_temp0, m_minus);
+        update_parallel_bc_1st( m_minus, m_plus, dg::NEU, 0.);
+        dg::geo::ds_slope( m_faST, -0.5, m_minus, m_plus, 1, yp[1][1]);
+        //m_fa( dg::geo::einsMinus, m_velocity[1], m_minus);
+        //m_fa( dg::geo::einsPlus,  m_velocity[1], m_plus);
+        //update_parallel_bc_2nd( m_fa, m_minus, m_velocity[1], m_plus,
+        //        m_p.bcxU, 0.);
+        //dg::geo::ds_centered( m_fa, 1., m_minus, m_velocity[1],
+        //        m_plus, 0., m_dsU[1]);
+        //dg::blas1::pointwiseDot( -1., m_velocity[1], m_dsU[1], 1., yp[1][1]);
+
+        // density m_dsN
+        m_fa( dg::geo::einsMinus, m_density[1], m_minus);
+        m_fa( dg::geo::einsPlus,  m_density[1], m_plus);
+        update_parallel_bc_2nd( m_fa, m_minus, m_density[1], m_plus,
+                m_p.bcxN, m_p.bcxN == dg::DIR ? m_p.nbc : 0.);
+        dg::geo::ds_centered( m_fa, 1., m_minus, m_density[1],
+                m_plus, 0., m_dsN[1]);
+        // parallel pressure gradient
+        dg::blas1::pointwiseDivide( -m_p.tau[1]/m_p.mu[1], m_dsN[1],
+                m_density[1], 1., yp[1][1]);
+        //m_faST( dg::geo::einsMinus, m_densityST[1], m_minus);
+        //m_faST( dg::geo::zeroPlus, m_densityST[1], m_plus);
+        //update_parallel_bc_1st( m_minus, m_plus, m_p.bcxN, m_p.bcxN ==
+        //        dg::DIR ? m_p.nbc : 0.);
+
+        //double tau = m_p.tau[1], mu = m_p.mu[1];
+        //dg::blas1::subroutine( [tau, mu ]DG_DEVICE ( double& UDot,
+        //            double QN, double PN, double hm, double hp)
+        //        {
+        //            UDot -= tau/mu*(PN-QN)/(hm+hp)/(hm+hp)*(hm/PN + hp/QN);
+        //        },
+        //        yp[1][1], m_minus, m_plus, m_faST.hm(),
+        //        m_faST.hp()
+        //);
+        // Add parallel viscosity
+        if( m_p.nu_parallel_u[1] > 0)
+        {
+            m_fa_diff( dg::geo::einsMinus, m_velocity[1], m_minus);
+            m_fa_diff( dg::geo::einsPlus, m_velocity[1], m_plus);
+            update_parallel_bc_2nd( m_fa_diff, m_minus, m_velocity[1],
+                    m_plus, m_p.bcxU, 0.);
+            dg::geo::dssd_centered( m_divb, m_fa_diff, m_p.nu_parallel_u[1],
+                    m_minus, m_velocity[1], m_plus, 0., m_temp0);
+            dg::blas1::pointwiseDivide( 1., m_temp0, m_density[1], 1., yp[1][1]);
+        }
+        // to get the diffusion right
+        dg::blas1::copy( m_velocity[1], m_velocityST[1]);
+    }
     //-------------Add regularization----------------------------//
     if( "log-staggered" == advection)
     {
