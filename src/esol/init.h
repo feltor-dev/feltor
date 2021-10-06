91dba1b69e7112f32065078d902e66d033778bf9
Henrik Jantti <hja048@uit.no>
2021-10-06 14:08:36 +0200

    Added line in Makefile to fix hdf5 issues. Added sin as init option. Added backgroundamp and ky parameters for sin init option.

--- src/esol/init.h
+++ src/esol/init.h
@@ -31,6 +31,17 @@ std::array<dg::x::DVec,2> initial_conditions(
        dg::blas1::plus(y0[0],-1.0*(p.bgprofamp + p.profamp));
        f.gamma1inv_y(y0[0], y0[1]);
     }
+    else if( initial== "sin")
+    {
+       y0[0] = dg::evaluate( dg::SinY(p.amp,p.bamp,p.ky) , grid);
+       y0[1] = dg::evaluate( dg::PolynomialRectangle(p.lx*p.xfac_d, p.sigma_d,p.lx*(1-p.xfac_d), p.sigma_d), grid); 
+       dg::blas1::pointwiseDot(y0[1],y0[0],y0[0]);
+       dg::blas1::plus(y0[0],1.0);
+       y0[1] = dg::evaluate( dg::TanhProfX(p.lx*p.xfac_sep, p.sigma_p,-1.0, p.bgprofamp,p.profamp), grid);
+       dg::blas1::pointwiseDot(y0[1],y0[0],y0[0]);
+       dg::blas1::plus(y0[0],-1.0*(p.bgprofamp + p.profamp));
+       f.gamma1inv_y(y0[0], y0[1]);
+    }
     else if ( initial =="bath")
     {
         y0[0] = dg::evaluate( dg::BathRZ( 16, 16, grid.x0(),grid.y0(), 30., 5., p.amp), grid);
