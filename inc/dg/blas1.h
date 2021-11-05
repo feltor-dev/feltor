4b198c725efaead91313bc944dec926dbcebdb65
Matthias <mattwi@fysik.dtu.dk>
2021-11-05 16:15:34 +0100

    Remove blas1_array.h
    -was unused

--- inc/dg/blas1.h
+++ inc/dg/blas1.h
@@ -8,7 +8,6 @@
 #include "backend/tensor_traits_std.h"
 #include "backend/blas1_dispatch_scalar.h"
 #include "backend/blas1_dispatch_shared.h"
-//#include "backend/blas1_array.h"
 #include "backend/tensor_traits_cusp.h"
 #ifdef MPI_VERSION
 #include "backend/mpi_vector.h"
