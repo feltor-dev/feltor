1e6e6ffc991c75bf56cf28fb5cbe3e05946aa25f
Matthias <mattwi@fysik.dtu.dk>
2022-02-01 20:03:09 +0100

    Add matrix.h include header

---
+++ inc/dg/matrix/matrix.h
@@ -0,0 +1,3 @@
+#pragma once
+#define _MATRIX_INCLUDED_BY_DG_
+#include "../../matrix/matrix.h"
---
+++ inc/matrix/matrix.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef _MATRIX_INCLUDED_BY_DG_
+#pragma message( "The inclusion of matrix/matrix.h is deprecated. Please use dg/matrix/matrix.h")
+#endif //_INCLUDED_BY_DG_
+
+#include "functors.h"
+#include "lanczos.h"
+#include "matrixfunction.h"
+#include "matrixsqrt.h"
+#include "polarization.h"
+#include "polarization_init.h"
+#include "sqrt_cauchy.h"
+#include "sqrt_ode.h"
+#include "tensorelliptic.h"
+#include "tridiaginv.h"
