cb1331018acdb55b47df484ad2d57376e5ece92d
Matthias <mattwi@fysik.dtu.dk>
2022-04-28 18:20:08 +0200

    Add preconditioner benchmark
    results are somewhat disppointing
    speedup does not offset additionl time for application
    nested iterations still wins

--- inc/dg/precond_t.cu
+++ inc/dg/precond_t.cu
@@ -17,14 +17,17 @@ int main()
     double eps = 1e-3;
     std::cout << "Type size of matrix (100), max number nnz per row (10), absolute threshold (1e-3)\n";
     std::cin >> size >> nnz >> eps;
+    thrust::host_vector<double> weights( size, 1.), d(weights);
+    for( int i=0; i<(int)size; i++)
+        weights[i] = (1.+(i%3)/2.)/10.;
     dg::Operator<double> op( size, 0.);
-    op( 0,0) = 2, op( 0, 1) = -1.;
+    op( 0,0) = 2/weights[0], op( 0, 1) = -1./weights[0];
     for( int i=1; i<(int)size-1; i++)
     {
-        op( i, i+1) = op( i, i-1) = -1.;
-        op(i,i) = 2.;
+        op( i, i+1) = op( i, i-1) = -1./weights[i];
+        op(i,i) = 2./weights[i];
     }
-    op( size-1, size-1) = 2., op( size-1, size-2) = -1.;
+    op( size-1, size-1) = 2./weights[size-1], op( size-1, size-2) = -1./weights[size-1];
 
     // Here compute the inverse
     auto inverse = dg::create::inverse( op);
@@ -50,7 +53,6 @@ int main()
             ainv.values[i*size+k] = inverse(i,k);
         }
     csr_mat a_csr = a, z;
-    thrust::host_vector<double> weights( size, 1.), d(weights);
 
     //std::cout << "Matrix A\n";
     //cusp::print( a_csr);
@@ -71,20 +73,24 @@ int main()
     cusp::transpose( z, zT);
     // create D^{-1}
     coo_mat dinv( size, size, size);
+    coo_mat wei( size, size, size);
     for( unsigned i=0; i<size; i++)
     {
         dinv.row_indices[i] = dinv.column_indices[i] = i;
+        wei.row_indices[i] = wei.column_indices[i] = i;
         dinv.values[i] = 1./d[i];
+        wei.values[i] = weights[i];
     }
     cusp::multiply( zT, dinv, zTD);
     cusp::multiply( zTD, z, zT);
-    zT.sort_by_row_and_column();
+    cusp::multiply( zT, wei, dinv);
+    dinv.sort_by_row_and_column();
     //std::cout << "Numerical inverse:\n";
     //cusp::print( zT);
     //std::cout << "Actual inverse:\n";
     //cusp::print( ainv);
     // Compute matrix norm between difference
-    cusp::subtract( ainv, zT, zTD);
+    cusp::subtract( ainv, dinv, zTD);
     double norm_err = 0.0;
     double norm_ainv = 0.0;
     for( unsigned u=0; u<zTD.values.size(); u++)
