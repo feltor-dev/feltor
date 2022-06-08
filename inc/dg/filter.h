bdd4cba1a8949aa56ba9cc6462c36cfd56eb7760
Matthias <mattwi@fysik.dtu.dk>
2022-06-08 16:49:08 +0200

    Implement and test Median filter

---
+++ inc/dg/filter.h
@@ -0,0 +1,106 @@
+#pragma once
+#include "dg/topology/functions.h"
+#include "dg/backend/config.h"
+namespace dg{
+
+
+
+/**
+ * @brief Compute (lower) Median of input numbers
+ *
+ * The (lower) median of N numbers is the N/2 (rounded up) largest number
+ */
+struct CSRMedianFilter
+{
+    template<class real_type>
+    DG_DEVICE
+    void operator()( unsigned i, real_type* y, const int* row_offsets,
+            const int* column_indices, const real_type* values,
+            const real_type* x)
+    {
+        // http://ndevilla.free.fr/median/median/index.html
+        // ignore the values array ...
+        int n = row_offsets[i+1]-row_offsets[i];
+        if( n == 3)
+        {
+            real_type p[3];
+            int k = row_offsets[i];
+            for( int l = 0; l<3; l++)
+                p[l] =  x[column_indices[k+l]];
+            pix_sort(p[0],p[1]) ; pix_sort(p[1],p[2]) ; pix_sort(p[0],p[1]) ;
+            y[i] = (p[1]) ;
+        }
+        else if ( n == 5)
+        {
+            real_type p[5];
+            int k = row_offsets[i];
+            for( int l = 0; l<5; l++)
+                p[l] =  x[column_indices[k+l]];
+            pix_sort(p[0],p[1]) ; pix_sort(p[3],p[4]) ; pix_sort(p[0],p[3]) ;
+            pix_sort(p[1],p[4]) ; pix_sort(p[1],p[2]) ; pix_sort(p[2],p[3]) ;
+            pix_sort(p[1],p[2]) ; y[i] = (p[2]) ;
+        }
+        else if( n == 9)
+        {
+            real_type p[9];
+            int k = row_offsets[i];
+            for( int l = 0; l<9; l++)
+                p[l] =  x[column_indices[k+l]];
+
+            pix_sort(p[1], p[2]) ; pix_sort(p[4], p[5]) ; pix_sort(p[7], p[8]) ;
+            pix_sort(p[0], p[1]) ; pix_sort(p[3], p[4]) ; pix_sort(p[6], p[7]) ;
+            pix_sort(p[1], p[2]) ; pix_sort(p[4], p[5]) ; pix_sort(p[7], p[8]) ;
+            pix_sort(p[0], p[3]) ; pix_sort(p[5], p[8]) ; pix_sort(p[4], p[7]) ;
+            pix_sort(p[3], p[6]) ; pix_sort(p[1], p[4]) ; pix_sort(p[2], p[5]) ;
+            pix_sort(p[4], p[7]) ; pix_sort(p[4], p[2]) ; pix_sort(p[6], p[4]) ;
+            pix_sort(p[4], p[2]) ; y[i] = (p[4]) ;
+        }
+        else
+        {
+            int less, greater, equal;
+            real_type  min, max, guess, maxltguess, mingtguess;
+
+            min = max = x[column_indices[row_offsets[i]]] ;
+            for (int k=row_offsets[i]+1 ; k<row_offsets[i+1] ; k++) {
+                if (x[column_indices[k]]<min) min=x[column_indices[k]];
+                if (x[column_indices[k]]>max) max=x[column_indices[k]];
+            }
+
+            while (1) {
+                guess = (min+max)/2;
+                less = 0; greater = 0; equal = 0;
+                maxltguess = min ;
+                mingtguess = max ;
+                for (int k=row_offsets[i]; k<row_offsets[i+1]; k++) {
+                    if (x[column_indices[k]]<guess) {
+                        less++;
+                        if (x[column_indices[k]]>maxltguess)
+                            maxltguess = x[column_indices[k]] ;
+                    } else if (x[column_indices[k]]>guess) {
+                        greater++;
+                        if (x[column_indices[k]]<mingtguess)
+                            mingtguess = x[column_indices[k]] ;
+                    } else equal++;
+                }
+                if (less <= (n+1)/2 && greater <= (n+1)/2) break ;
+                else if (less>greater) max = maxltguess ;
+                else min = mingtguess;
+            }
+            if (less >= (n+1)/2) y[i] = maxltguess;
+            else if (less+equal >= (n+1)/2) y[i] = guess;
+            else y[i] =  mingtguess;
+        }
+    }
+    private:
+    template<class real_type>
+    DG_DEVICE void pix_sort( real_type& a, real_type& b)
+    {
+        if( a > b) // swap
+        {
+            real_type tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+};
+}//namespace dg
---
+++ inc/dg/filter_t.cu
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "filter.h"
+
+
+int main()
+{
+    std::vector<double> x = {7,2,3, -20,4,7, 2.3,2.1,4, 4,15,-2};
+    int row_offsets[2] = {0, 3};
+    std::vector<int> column_indices = {0,1,2,3,4,5,6,7,8,9,10,11,12};
+    double result;
+    dg::CSRMedianFilter filter;
+    for( unsigned i=1; i<12; i++)
+    {
+        row_offsets[1] = i;
+        filter( 0, &result, row_offsets, &column_indices[0], &x[0], &x[0]);
+        std::cout << "Median of [";
+        int k=0;
+        for( k=0; k<row_offsets[1]-1; k++)
+            std::cout << x[k]<<", ";
+        std::cout<<x[k]<<"] is "<<result<<std::endl;
+    }
+}
