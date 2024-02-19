752ea0997bac285fb7be5ed646c63e3c918bdcd7
Matthias <mattwi@fysik.dtu.dk>
2024-02-19 15:20:23 +0100

    Write and test easy_input.h

---
+++ inc/file/easy_input.h
@@ -0,0 +1,307 @@
+#pragma once
+
+#include <exception>
+#include <netcdf.h>
+#include "dg/topology/grid.h"
+#ifdef MPI_VERSION
+#include "dg/backend/mpi_vector.h"
+#include "dg/topology/mpi_grid.h"
+#endif //MPI_VERSION
+#include "nc_error.h"
+
+/*!@file
+ *
+ * Define variable readers
+ */
+
+// Variable ids are persistent once created
+// Attribute ids can change if one deletes attributes
+namespace dg
+{
+namespace file
+{
+/**
+ * @defgroup netcdf NetCDF utilities
+ * \#include "dg/file/nc_utilities.h" (link -lnetcdf -lhdf5[_serial] -lhdf5[_serial]_hl)
+ *
+ * @addtogroup netcdf
+ * @{
+ */
+
+///@cond
+namespace detail
+{
+
+template<class T>
+inline int get_var_T( int ncid, int varID, T* data)
+{
+    assert( false && "Type not supported!\n" );
+    return NC_EBADTYPE;
+}
+template<>
+inline int get_var_T<float>( int ncid, int varID, float* data){
+    return nc_get_var_float( ncid, varID, data);
+}
+template<>
+inline int get_var_T<double>( int ncid, int varID, double* data){
+    return nc_get_var_double( ncid, varID, data);
+}
+template<>
+inline int get_var_T<int>( int ncid, int varID, int* data){
+    return nc_get_var_int( ncid, varID, data);
+}
+template<>
+inline int get_var_T<unsigned>( int ncid, int varID, unsigned* data){
+    return nc_get_var_uint( ncid, varID, data);
+}
+template<class T>
+inline int get_vara_T( int ncid, int varID, const size_t* startp, const size_t* countp, T* data)
+{
+    assert( false && "Type not supported!\n" );
+    return NC_EBADTYPE;
+}
+template<>
+inline int get_vara_T<float>( int ncid, int varID, const size_t* startp, const size_t* countp, float* data){
+    return nc_get_vara_float( ncid, varID, startp, countp, data);
+}
+template<>
+inline int get_vara_T<double>( int ncid, int varID, const size_t* startp, const size_t* countp, double* data){
+    return nc_get_vara_double( ncid, varID, startp, countp, data);
+}
+template<>
+inline int get_vara_T<int>( int ncid, int varID, const size_t* startp, const size_t* countp, int* data){
+    return nc_get_vara_int( ncid, varID, startp, countp, data);
+}
+template<>
+inline int get_vara_T<unsigned>( int ncid, int varID, const size_t* startp, const size_t* countp, unsigned* data){
+    return nc_get_vara_uint( ncid, varID, startp, countp, data);
+}
+#ifdef MPI_VERSION
+
+template<class host_vector, class MPITopology>
+void get_vara_detail(int ncid, int varid, unsigned slice, const MPITopology& grid, MPI_Vector<host_vector>& data, bool vara, bool parallel = false)
+{
+    MPI_Comm comm = grid.communicator();
+    int local_root_rank = dg::mpi_comm_global2local_rank(comm);
+    if (local_root_rank == MPI_UNDEFINED)
+        return;
+    unsigned grid_ndims = grid.ndim();
+    auto count = shape( grid.local());
+    std::reverse( count.begin(), count.end());
+    if( vara)
+        count.insert( count.begin(), 1);
+    int rank, size;
+    MPI_Comm_rank( comm, &rank);
+    MPI_Comm_size( comm, &size);
+    std::vector<size_t> start(vara ? grid_ndims+1 : grid_ndims,0);
+    if( vara)
+        start[0] = slice;
+    file::NC_Error_Handle err;
+    data.communicator() = grid.communicator();
+    if( parallel)
+    {
+        int coords[grid_ndims];
+        MPI_Cart_coords( comm, rank, grid_ndims, coords);
+        for( unsigned i=0; i<grid_ndims; i++)
+            start[vara ? i+1 : i] = count[ vara ? i+1 : i]*coords[grid_ndims-1-i];
+        err = detail::get_vara_T( ncid, varid, &start[0], &count[0],
+            data.data().data());
+    }
+    else
+    {
+        MPI_Status status;
+        size_t local_size = data.data().size();
+        std::vector<int> coords( grid_ndims*size);
+        for( int rrank=0; rrank<size; rrank++)
+            MPI_Cart_coords( comm, rrank, grid_ndims, &coords[grid_ndims*rrank]);
+        if( rank == local_root_rank )
+        {
+            host_vector to_send( data.data());
+            for( int rrank=0; rrank<size; rrank++)
+            {
+                for ( unsigned i=0; i<grid_ndims; i++)
+                    start[vara ? i+1 : i] = count[ vara ? i+1 : i]*coords[grid_ndims*rrank + grid_ndims-1-i];
+                if(rrank!=rank)
+                {
+                    err = detail::get_vara_T( ncid, varid, &start[0], &count[0],
+                        to_send.data()); // read data to send
+                    MPI_Send( to_send.data(), local_size, dg::getMPIDataType<get_value_type<host_vector>>(),
+                          rrank, rrank, comm, &status);
+                }
+                else // read own data
+                    err = detail::get_vara_T( ncid, varid, &start[0], &count[0], data.data().data());
+            }
+        }
+        else
+            MPI_Recv( data.data().data(), local_size, dg::getMPIDataType<get_value_type<host_vector>>(),
+                      local_root_rank, rank, comm);
+        MPI_Barrier( comm);
+    }
+}
+#endif // MPI_VERSION
+} // namespace detail
+///@endcond
+
+/**
+* @brief Convenience wrapper around \c nc_get_var
+*
+* The purpose of this function is mainly to simplify input in an MPI environment and to provide
+* the same interface also in a shared memory system for uniform programming.
+* This version is for a time-independent variable,
+* i.e. readas a single variable in one go and is actually equivalent
+* to \c nc_get_var. The dimensionality is given by the grid.
+* @note This function throws a \c dg::file::NC_Error if an error occurs
+* @tparam host_vector Type with \c data() member that returns pointer to first
+* element in CPU (host) adress space, meaning it cannot be a GPU vector.
+* @param ncid NetCDF file or group ID
+* @param varid Variable ID
+* @param grid The grid from which to construct \c start and \c count variables to forward to \c nc_get_vara
+* @param data contains the read data on return (must be of size \c grid.size() )
+* @copydoc hide_parallel_param
+* @copydoc hide_master_comment
+*/
+template<class host_vector, class Topology>
+void get_var( int ncid, int varid, const Topology& grid,
+    host_vector& data, bool parallel = false)
+{
+    file::NC_Error_Handle err;
+    err = detail::get_var_T( ncid, varid, data.data());
+}
+
+/**
+* @brief Convenience wrapper around \c nc_get_vara()
+*
+* The purpose of this function is mainly to simplify input in an MPI environment and to provide
+* the same interface also in a shared memory system for uniform programming.
+* This version is for a time-dependent variable,
+* i.e. reads a single time-slice from the file.
+* The dimensionality is given by the grid.
+* @note This function throws a \c dg::file::NC_Error if an error occurs
+* @tparam host_vector Type with \c data() member that returns pointer to first
+* element in CPU (host) adress space, meaning it cannot be a GPU vector
+* @param ncid NetCDF file or group ID
+* @param varid Variable ID
+* @param slice The number of the time-slice to read (first element of the \c startp array in \c nc_get_vara)
+* @param grid The grid from which to construct \c start and \c count variables to forward to \c nc_get_vara
+* @param data contains the read data on return (must be of size \c grid.size() )
+* @copydoc hide_parallel_param
+* @copydoc hide_master_comment
+*/
+template<class host_vector, class Topology>
+void get_vara( int ncid, int varid, unsigned slice, const Topology& grid,
+    host_vector& data, bool parallel = false)
+{
+    file::NC_Error_Handle err;
+    auto cc = shape( grid);
+    std::reverse( cc.begin(), cc.end());
+    cc.insert( cc.begin(), 1);
+    std::vector<size_t> count( cc.begin(), cc.end());
+    std::vector<size_t> start( count.size(), 0);
+    start[0] = slice;
+    err = detail::get_vara_T( ncid, varid, &start[0], &count[0], data.data());
+}
+// scalar data
+
+/**
+ * @brief Read a scalar from the netcdf file
+ *
+ * @note This function throws a \c dg::file::NC_Error if an error occurs
+ * @param ncid NetCDF file or group ID
+ * @param varid Variable ID
+ * @param grid a Tag to signify scalar input (and help the compiler choose this
+ * function over the array output function)
+ * @param data The (single) datum read from file.
+ * @param parallel This parameter is ignored in both serial and MPI versions.
+ * In an MPI program all processes can call this function but only the master thread reads.
+ * @copydoc hide_master_comment
+ */
+template<class T, class real_type>
+void get_var( int ncid, int varid, const RealGrid0d<real_type>& grid,
+    T& data, bool parallel = false)
+{
+    file::NC_Error_Handle err;
+    err = detail::get_var_T( ncid, varid, &data);
+}
+/**
+ * @brief Read a scalar to the netcdf file
+ *
+ * @note This function throws a \c dg::file::NC_Error if an error occurs
+ * @param ncid NetCDF file or group ID
+ * @param varid Variable ID
+ * @param slice The number of the time-slice to read (first element of the \c startp array in \c nc_put_vara)
+ * @param grid a Tag to signify scalar ouput (and help the compiler choose this function over the array output function)
+ * @param data The (single) datum to read.
+ * @param parallel This parameter is ignored in both serial and MPI versions.
+ * In an MPI program all processes can call this function but only the master thread reads.
+ * @copydoc hide_master_comment
+ */
+template<class T, class real_type>
+void get_vara( int ncid, int varid, unsigned slice, const RealGrid0d<real_type>& grid,
+    T& data, bool parallel = false)
+{
+    file::NC_Error_Handle err;
+    size_t count = 1;
+    size_t start = slice; // convert to size_t
+    err = detail::get_vara_T( ncid, varid, &start, &count, &data);
+}
+
+
+#ifdef MPI_VERSION
+
+///@copydoc get_var()
+template<class host_vector, class MPITopology>
+void get_var(int ncid, int varid, const MPITopology& grid,
+    dg::MPI_Vector<host_vector>& data, bool parallel = false)
+{
+    detail::get_vara_detail( ncid, varid, 0, grid, data, false, parallel);
+}
+
+///@copydoc get_vara()
+template<class host_vector, class MPITopology>
+void get_vara(int ncid, int varid, unsigned slice,
+    const MPITopology& grid, dg::MPI_Vector<host_vector>& data,
+    bool parallel = false)
+{
+    detail::get_vara_detail( ncid, varid, 0, grid, data, true, parallel);
+}
+
+// scalar data
+
+///@copydoc get_var()
+template<class T, class real_type>
+void get_var( int ncid, int varid, const RealMPIGrid0d<real_type>& grid,
+    T& data, bool parallel = false)
+{
+    int rank;
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
+    if( parallel)
+    {
+        get_var( ncid, varid, dg::RealGrid0d<real_type>(), data, parallel);
+        return;
+    }
+    if( rank == 0)
+        get_var( ncid, varid, dg::RealGrid0d<real_type>(), data, parallel);
+    MPI_Bcast( &data, 1, dg::getMPIDataType<T>, 0, MPI_COMM_WORLD);
+}
+
+///@copydoc get_vara()
+template<class T, class real_type>
+void get_vara( int ncid, int varid, unsigned slice, const RealMPIGrid0d<real_type>& grid,
+    T& data, bool parallel = false)
+{
+    int rank;
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
+    if( parallel )
+    {
+        get_vara( ncid, varid, slice, dg::RealGrid0d<real_type>(), data, parallel);
+        return;
+    }
+    if( rank == 0)
+        get_vara( ncid, varid, slice, dg::RealGrid0d<real_type>(), data, parallel);
+    MPI_Bcast( &data, 1, dg::getMPIDataType<T>, 0, MPI_COMM_WORLD);
+}
+#endif //MPI_VERSION
+
+///@}
+}//namespace file
+}//namespace dg
---
+++ inc/file/easy_input_t.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <netcdf.h>
+#include <cmath>
+
+#ifdef WITH_MPI
+#include <mpi.h>
+#endif
+
+#include "dg/algorithm.h"
+#define _FILE_INCLUDED_BY_DG_
+#include "easy_dims.h"
+#include "easy_input.h"
+
+double function( double x, double y, double z){return sin(x)*sin(y)*cos(z);}
+double function( double x, double y){return sin(x)*sin(y);}
+
+int main(int argc, char* argv[])
+{
+#ifdef WITH_MPI
+    dg::mpi_init( argc, argv);
+    int rank, size;
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
+    MPI_Comm_size( MPI_COMM_WORLD, &size);
+    MPI_Comm comm;
+    //create a grid and some data
+    if( size != 4){ std::cerr << "Please run with 4 threads!\n"; return -1;}
+    std::stringstream ss;
+    ss<< "2 1 2";
+    dg::mpi_init3d( dg::PER, dg::PER, dg::PER, comm, ss);
+#endif
+#ifdef WITH_MPI
+    std::string filename = "testmpi.nc";
+#else
+    std::string filename = "test.nc";
+#endif
+    DG_RANK0 std::cout << "READ A TIME-DEPENDENT SCALAR, SCALAR FIELD AND SUB-FIELDS TO NETCDF4 FILE "
+                       << filename<<"\n";
+    DG_RANK0 std::cout << "THEN READ IT BACK IN AND TEST EQUALITY\n";
+    double Tmax=2.*M_PI;
+    double NT = 10;
+    double h = Tmax/NT;
+    double x0 = 0., x1 = 2.*M_PI;
+    dg::x::CartesianGrid3d grid( x0,x1,x0,x1,x0,x1,3,10,10,20, dg::PER, dg::PER, dg::PER
+#ifdef WITH_MPI
+    , comm
+#endif
+    );
+    dg::ClonePtr<dg::x::aGeometry2d> perp_grid_ptr = grid.perp_grid();
+    dg::x::Grid2d grid2d = (dg::x::Grid2d)*perp_grid_ptr;
+    dg::x::HVec data = dg::evaluate( function, grid);
+
+    //create NetCDF File
+    int ncid;
+    dg::file::NC_Error_Handle err;
+    DG_RANK0 err = nc_open( filename.data(), 0, &ncid);
+
+    int dim_ids[4], tvarID;
+    bool exists = dg::file::check_dimensions( ncid, dim_ids, &tvarID, grid);
+    if( ! exists)
+        std::cerr << "Dimensions do not exist!\n";
+
+    int scalarID, arrayID, subArrayID;
+    DG_RANK0 err = nc_inq_varid( ncid, "scalar", &scalarID);
+    DG_RANK0 err = nc_inq_varid( ncid, "array", &arrayID);
+    DG_RANK0 err = nc_inq_varid( ncid, "sub-array", &subArrayID);
+
+    int staticArrayID;
+    DG_RANK0 err = nc_inq_varid( ncid, "static-array", &staticArrayID);
+    dg::file::get_var( ncid, staticArrayID, grid, data);
+
+    for(unsigned i=0; i<=NT; i++)
+    {
+        DG_RANK0 std::cout<<"Read timestep "<<i<<"\n";
+        double time = i*h;
+        const size_t Tstart = i;
+        data = dg::evaluate( function, grid);
+        auto sliced_data = dg::split( data, grid); // a vector of views
+        dg::blas1::scal( data, cos( time));
+        double energy;
+        //read scalar data point
+        dg::file::get_vara( ncid, scalarID, Tstart, dg::x::Grid0d(), energy);
+        //read array
+        dg::x::HVec temp(data);
+        dg::file::get_vara( ncid, arrayID, Tstart, grid, temp);
+        //read sub array
+        dg::file::get_vara( ncid, subArrayID, Tstart, grid2d, sliced_data[0]);
+        //read time
+        dg::file::get_vara( ncid, tvarID, Tstart, dg::x::Grid0d(), time);
+        DG_RANK0 std::cout << "At time "<<time<<"\n";
+    }
+
+    DG_RANK0 err = nc_close(ncid);
+#ifdef WITH_MPI
+    MPI_Finalize();
+#endif
+    return 0;
+}
