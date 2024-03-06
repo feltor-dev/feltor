4c3d83ed80cff7939654a86212de00cd528e1119
Matthias <mattwi@fysik.dtu.dk>
2024-03-06 15:14:19 +0100

    Bump c++ to 17
    Let's see how it goes

--- config/m100.mk
+++ config/m100.mk
@@ -3,11 +3,11 @@ ifeq ($(strip $(HPC_SYSTEM)),m100)
 #MPICC=mpixlC  #mpi compiler
 #CFLAGS=-Wall -std=c++1y -DWITHOUT_VCL -mcpu=power9 -qstrict# -mavx -mfma #flags for CC
 #OMPFLAG=-qsmp=omp
-CFLAGS=-Wall -std=c++14 -DWITHOUT_VCL -mcpu=power9 # -mavx -mfma #flags for CC
+CFLAGS=-Wall -std=c++17 -DWITHOUT_VCL -mcpu=power9 # -mavx -mfma #flags for CC
 OPT=-O3 # optimization flags for host code
 NVCC=nvcc #CUDA compiler
 NVCCARCH=-arch sm_70 -Xcudafe "--diag_suppress=code_is_unreachable --diag_suppress=initialization_not_reachable" #nvcc gpu compute capability
-NVCCFLAGS= -std=c++14 -Xcompiler "-mcpu=power9 -Wall" --extended-lambda# -mavx -mfma" #flags for NVCC
+NVCCFLAGS= -std=c++17 -Xcompiler "-mcpu=power9 -Wall" --extended-lambda# -mavx -mfma" #flags for NVCC
 
 INCLUDE += -I$(NETCDF_INC) -I$(HDF5_INC) -I$(JSONCPP_INC)
 INCLUDE += -L$(BOOST_INC) -I$(LAPACK_INC)
