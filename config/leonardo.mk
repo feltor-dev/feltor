91683e14221b174e82a91cb8f89ff5f6b9598f3b
Matthias <mattwi@fysik.dtu.dk>
2024-03-08 15:11:18 +0100

    Replace m100 with leonardo config file

---
+++ config/leonardo.mk
@@ -0,0 +1,29 @@
+ifeq ($(strip $(HPC_SYSTEM)),leonardo)
+CFLAGS=-Wall -std=c++17 -mavx -mfma #flags for CC
+OPT=-O3 # optimization flags for host code
+NVCC=nvcc #CUDA compiler
+NVCCARCH=-arch sm_80 -Xcudafe "--diag_suppress=code_is_unreachable --diag_suppress=initialization_not_reachable" #nvcc gpu compute capability
+NVCCFLAGS= -std=c++17 -Xcompiler "-Wall -mavx -mfma" --extended-lambda# -mavx -mfma" #flags for NVCC
+
+INCLUDE += -I$(NETCDF_C_INC) -I$(HDF5_INC)
+INCLUDE += -I$(BOOST_INC) #-I$(LAPACK_INC)
+JSONLIB= -ljsoncpp # json library for input parameters
+LIBS    =-L$(HDF5_LIB) -lhdf5 -lhdf5_hl
+LIBS    +=-L$(NETCDF_C_LIB) -lnetcdf -lcurl
+#LAPACKLIB = -L$(LAPACK_LIB) -llapacke
+endif
+#########################Modules to load ##################
+# module load gcc/11.3.0
+# module load cuda/11.8
+# 
+# module load spack
+# # spack list jsoncpp
+# # spack spec -Il jsoncpp
+# # spack install jsoncpp %gcc@11.3.0
+# spack load jsoncpp
+# module load openmpi/4.1.4--gcc--11.3.0-cuda-11.8
+# module load boost/1.80.0--gcc--11.3.0
+# module load hdf5/1.12.2--gcc--11.3.0
+# module load netcdf-c/4.9.0--gcc--11.3.0
+
+
