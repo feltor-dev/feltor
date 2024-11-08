fb5967c33bc5f908bd537cd6c3a192fe97d2cd52
Matthias <mattwi@fysik.dtu.dk>
2024-11-08 16:17:06 +0100

    Rewrite DLT and test file (THIS BREAKS CODE)
    no construction any more
    at the price of an insignificant memory allocation per call i.e. per
    evaluate (which is insignificant because evaluate already allocates lots
    of memory)

--- inc/dg/topology/dlt_t.cu
+++ inc/dg/topology/dlt_t.cu
@@ -8,24 +8,60 @@
 int main()
 {
     //Test if forward * backwards gives a Delta function and print abscissas and weights
-    unsigned n;
-    std::cout << "Type # of polynomial coefficients n! \n";
-    std::cin >> n;
-    dg::DLT<double> dlt( n);
+    for( unsigned n=1; n<21; n++)
+    {
+        std::cout << "# of polynomial coefficients n! \t"<<n<<"\n";
+        dg::Operator<double> forward( dg::DLT<double>::forward(n));
+        dg::Operator<double> backward( dg::DLT<double>::backward(n));
+        auto abs = dg::DLT<double>::abscissas(n);
+        auto weights = dg::DLT<double>::weights(n);
+        auto delta = forward*backward;
+        std::cout << "Forward * Backward should give Delta: \n";
+        for( unsigned k=0; k<n; k++)
+        for( unsigned l=0; l<n; l++)
+        {
+            if( k == l)
+            {
+                if( fabs(delta(k,l) -1)  > 1e-15)
+                {
+                    std::cerr << "FAILED\n";
+                    return -1;
+                }
+            }
+            else
+            {
+                if( fabs(delta(k,l) )  > 1e-15)
+                {
+                    std::cerr << "FAILED\n";
+                    return -1;
+                }
+            }
+        }
+        std::cout << std::fixed<<std::setprecision(0);
+        std::cout << forward*backward;
+        std::cout << std::setprecision(16);
 
-    dg::Operator<double> forward( dlt.forward());
-    dg::Operator<double> backward( dlt.backward());
-    std::cout << "Forward * Backward should give Delta: \n";
-    std::cout << std::setprecision(2);
-    std::cout << forward*backward;
-    std::cout << std::setprecision(16);
-    std::cout << "Abscissas:\n";
-    for( unsigned i=0; i<n; i++)
-        std::cout << dlt.abscissas()[i]<<" ";
-    std::cout << std::endl;
-    std::cout << "Weights:\n";
-    for( unsigned i=0; i<n; i++)
-        std::cout << dlt.weights()[i]<<" ";
-    std::cout << std::endl;
+        std::cout << "Abscissas:\n";
+        double sum =0;
+        for( unsigned i=0; i<n; i++)
+        {
+            sum += abs[i];
+            std::cout << abs[i]<<" ";
+        }
+        std::cout << std::endl;
+        std::cout << std::setprecision(6);
+        std::cout<< "Sum of all abscissas\t"<<sum<<" (0)\n\n";
+        std::cout << std::setprecision(16);
+        std::cout << "Weights:\n";
+        sum = 0;
+        for( unsigned i=0; i<n; i++)
+        {
+            sum += weights[i];
+            std::cout << weights[i]<<" ";
+        }
+        std::cout << std::endl;
+        std::cout << std::setprecision(6);
+        std::cout<< "Sum of all weights\t"<<sum<<" (2)\n\n";
+    }
     return 0;
 }
