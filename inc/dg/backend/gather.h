4e910ab0e6b6393b613443591bf885ac8ad4353c
Matthias <mattwi@fysik.dtu.dk>
2024-12-05 11:28:19 +0100

    Make scatter memory allocation optional
    only allocate buffers if scatter is actually called

--- inc/dg/backend/gather.h
+++ inc/dg/backend/gather.h
@@ -83,17 +83,100 @@ void findUniqueIndices(
 }// namespace detail
 ///@endcond
 
-// TODO document or make non-public
-// Given an index map construct the associate gather matrix and its transpose
-// Represents the matrix \f$ G_{ij} = \delta_{g[i]j}\f$ and its transpose
+/*! @brief Gather sparse matrix \f$ G_{ij} = \delta_{g(i) j}\f$ and its transpose
+ *
+ * A gather matrix is a coo sparse matrix format where all values are 1
+ * the row indices are simply the row \f$ r_i = i\f$ and the column indices
+ * are given by the gather map \f$ c_i = g(i)\f$.
+ *
+ * The **transpose** of the gather matrix is a scatter matrix where the column indices
+ * are simiply the column \f$ c_i =i\f$ and the row index is the gather map
+ * \f$ r_i = g(i)\f$.
+ * @tparam Vector storage class for indices, must have the execution policy that
+ * is later used for gather/scatter operations
+ */
 template<template<class >class Vector>
 struct LocalGatherMatrix
 {
+    /// Empty
     LocalGatherMatrix() = default;
 
-    // index_map gives num_rows of G, so we need num_cols as well
+    /*! @brief Construct from column indices / gather map
+     * @param index_map each index is a column index
+     */
     LocalGatherMatrix( const thrust::host_vector<int>& index_map)
-    : m_idx( index_map)
+    : m_idx( index_map) { }
+
+    template<template<class> class OtherVector>
+    friend class LocalGatherMatrix;
+    /*! @brief Copy from another storage class
+     * @param src Copy from
+     */
+    template<template<class> class OtherVector>
+    LocalGatherMatrix( const LocalGatherMatrix<OtherVector>& src)
+    : m_idx ( src.m_idx) { }
+
+    /// Index map from constructor
+    const Vector<int>& index_map() const{ return m_idx;}
+
+    /// \f$ w = \alpha G v + \beta w \f$
+    template<class ContainerType0, class ContainerType1>
+    void gather(dg::get_value_type<ContainerType0> alpha, const ContainerType0& store,
+                dg::get_value_type<ContainerType1> beta, ContainerType1& buffer) const
+    {
+        thrust::gather( m_idx.begin(), m_idx.end(), store.begin(), buffer.begin());
+        unsigned size = buffer.size();
+        using value_type= dg::get_value_type<ContainerType0>;
+        dg::blas2::detail::doParallelFor( SharedVectorTag(),
+            [size, alpha, beta]DG_DEVICE( unsigned i, const value_type* x,
+                const int* idx, value_type* y) {
+                y[i] = alpha*x[idx[i]] + beta*y[i];
+            }, size, store, m_idx, buffer);
+    }
+    /// \f$ v = v + S w\f$
+    template<class ContainerType0, class ContainerType1>
+    void scatter_plus(const ContainerType0& buffer, ContainerType1& store) const
+    {
+        if( ! m_allocated) // 1st time scatter is called
+            prepare_scatter();
+        // This is essentially an implementation for a coo format
+        // sparse matrix vector multiplication
+        // Note a S w + b v is not possible efficiently in one go
+        using value_type= dg::get_value_type<ContainerType0>;
+        auto scatter_lambda = []DG_DEVICE( unsigned i, const value_type* x,
+            const int* idx, value_type* y) { y[idx[i]] += x[i]; };
+        if( !m_reduction) // this is fast
+        {
+            unsigned size = buffer.size();
+            dg::blas2::detail::doParallelFor( SharedVectorTag(),
+                scatter_lambda, size, buffer, m_idx, store);
+        }
+        else
+        {
+            // this is not efficient (needs to load/store 3x)
+            // (We assume this case is more of an edge case)
+            // 1. Gather into sorted indices
+            m_reduction_buffer.template set<value_type>( m_reduction_buffer_size);
+            thrust::gather( m_gather2reduction.begin(), m_gather2reduction.end(),
+                buffer.begin(), m_reduction_buffer.template get<value_type>().begin());
+
+            // 2. Reduce multiple sorted indices
+            m_scatter_buffer.template set<value_type>( m_scatter_buffer_size);
+            thrust::reduce_by_key( m_red_keys.begin(), m_red_keys.end(),
+                m_reduction_buffer.template get<value_type>().begin(),
+                m_unique_keys.begin(), // keys output
+                m_scatter_buffer.template get<value_type>().begin()); // values out
+
+            // 3. Scatter into target
+            unsigned size = m_scatter_buffer_size;
+            dg::blas2::detail::doParallelFor( SharedVectorTag(),
+                scatter_lambda, size, m_scatter_buffer.template get<value_type>(),
+                m_scatter2target, store);
+        }
+    }
+    private:
+    Vector<int> m_idx; // this fully defines the matrix ( so it's the only non-mutable)
+    void prepare_scatter( ) const
     {
         // For the scatter operation we need to catch the case that multiple
         // values scatter to the same place
@@ -105,87 +188,24 @@ struct LocalGatherMatrix
         // a scatter buffer.
         // Finally we can scatter the values from there after setting explicit 0s
 
-        thrust::host_vector<int> sort_map, reduction_keys, buffer_idx, unique_indices;
+        Vector<int> sort_map, reduction_keys, buffer_idx, unique_indices;
         detail::findUniqueIndices<thrust::host_vector>(
-            index_map, sort_map, reduction_keys, buffer_idx, unique_indices);
-        if( unique_indices.size() == index_map.size())
-        {
-            m_reduction = false;
-            m_scatter_buffer_size = 0;
-            m_reduction_buffer_size = 0;
-        }
-        else // we need to first gather into sorted buffer -> reduce -> scatter
+            m_idx, sort_map, reduction_keys, buffer_idx, unique_indices);
+        if( unique_indices.size() != m_idx.size())
         {
             m_reduction = true;
             m_scatter_buffer_size = unique_indices.size();
-            m_reduction_buffer_size = index_map.size();
+            m_reduction_buffer_size = m_idx.size();
             m_scatter2target = unique_indices;
             m_red_keys = reduction_keys;
             m_gather2reduction = sort_map;
-        }
-    }
-
-    template<template<class> class OtherVector>
-    friend class LocalGatherMatrix;
-    template<template<class> class OtherVector>
-    LocalGatherMatrix( const LocalGatherMatrix<OtherVector>& src)
-    : m_idx ( src.m_idx), m_scatter2target( src.m_scatter2target), m_red_keys(
-    src.m_red_keys), m_gather2reduction( src.m_gater2reduction), m_reduction(
-    src.m_reduction), m_scatter_buffer_size( src.m_scatter_buffer_size),
-    m_reduction_buffer_size( src.m_reduction_buffer_size)
-    {}
-
-    const Vector<int>& index_map() const{ return m_idx;}
-
-    // w = Gv
-    template<class ContainerType0, class ContainerType1>
-    void gather( const ContainerType0& store, ContainerType1& buffer) const
-    {
-        thrust::gather( m_idx.begin(), m_idx.end(), store.begin(), buffer.begin());
-    }
-    // v = v + S w
-    template<class ContainerType0, class ContainerType1>
-    void scatter_plus( const ContainerType0& buffer, ContainerType1& store) const
-    {
-        using value_type= dg::get_value_type<ContainerType0>;
-        if( !m_reduction)
-        {
-            unsigned size = buffer.size();
-            dg::blas2::detail::doParallelFor( SharedVectorTag(),
-                [size]DG_DEVICE( unsigned i, const value_type* x,
-                    const int* idx, value_type* y) {
-                    y[idx[i]] += x[i];
-                }, size, buffer, m_idx, store);
-        }
-        else
-        {
-            // 1. Gather into sorted indices
-            m_reduction_buffer.template set<value_type>( m_reduction_buffer_size);
-            thrust::gather( m_gather2reduction.begin(), m_gather2reduction.end(),
-                buffer.begin(), m_reduction_buffer.template get<value_type>().begin());
-
-            // 2. Reduce multiple sorted indices
-            m_scatter_buffer.template set<value_type>( m_scatter_buffer_size);
             m_unique_keys.resize( m_scatter_buffer_size);
-            thrust::reduce_by_key( m_red_keys.begin(), m_red_keys.end(),
-                m_reduction_buffer.template get<value_type>().begin(),
-                m_unique_keys.begin(), // keys output
-                m_scatter_buffer.template get<value_type>().begin()); // values out
-
-            // 3. Scatter into target
-            unsigned size = m_scatter_buffer_size;
-            dg::blas2::detail::doParallelFor( SharedVectorTag(),
-                [size]DG_DEVICE( unsigned i, const value_type* x,
-                    const int* idx, value_type* y) {
-                    y[idx[i]] += x[i];
-                }, size, m_scatter_buffer.template get<value_type>(), m_scatter2target, store);
         }
+        m_allocated =true;
     }
-    private:
-    Vector<int> m_idx;
-    Vector<int> m_scatter2target, m_red_keys, m_gather2reduction; // may be empty
-    bool m_reduction = false;
-    unsigned m_scatter_buffer_size = 0, m_reduction_buffer_size = 0;
+    mutable bool m_reduction = false, m_allocated = false;
+    mutable unsigned m_scatter_buffer_size, m_reduction_buffer_size;
+    mutable Vector<int> m_scatter2target, m_red_keys, m_gather2reduction; // may be empty
     mutable Vector<int> m_unique_keys;
     mutable detail::AnyVector<Vector> m_scatter_buffer, m_reduction_buffer;
 };
--- inc/dg/backend/gather_t.cu
+++ inc/dg/backend/gather_t.cu
@@ -41,7 +41,7 @@ int main()
     dg::LocalGatherMatrix<thrust::host_vector> gather(idx);
     std::vector<int> values = {0,1,2,3,4,5,6,7,8,9};
     std::vector<int> buffer(idx.size());
-    gather.gather( values, buffer);
+    gather.gather( 1., values, 0., buffer);
     for( unsigned i=0; i<idx.size(); i++)
         assert( buffer[i] == idx[i]);
     std::cout << "Gather PASSED\n";
