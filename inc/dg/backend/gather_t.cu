57d06218cfb8302cadf59238d5ea84f1fb7bc6e0
Matthias <mattwi@fysik.dtu.dk>
2024-12-16 17:40:29 +0100

    Rewrite gather.h

--- inc/dg/backend/gather_t.cu
+++ inc/dg/backend/gather_t.cu
@@ -4,79 +4,11 @@
 #include <cusp/multiply.h>
 #include <cusp/convert.h>
 #include <cusp/array1d.h>
-#include "../blas.h" // to include all tensor traits
 #include "gather.h"
 
 
 int main()
 {
-    thrust::host_vector<std::array<int,2>> gIdx = std::vector<std::array<int,2>>{
-    {1,0}, {2,6}, {5,6}, {0,2}, {0,1}, {2,1}, {2,3}, {1,1}, {5,6}, {1,0}, {5,0}, {0,2}};
-    std::cout<< "Values \n";
-    for( unsigned u=0; u<gIdx.size(); u++)
-        std::cout <<"{"<<gIdx[u][0]<<","<<gIdx[u][1]<<"} ";
-    std::cout<<"\n"<<std::endl;
-    for( unsigned test=0; test<2; test++)
-    {
-        dg::detail::Unique<std::array<int,2>> uni;
-        if( test == 0)
-        {
-            std::cout << "Order preserving TEST\n";
-            uni = dg::detail::find_unique_order_preserving(gIdx);
-        }
-        else
-        {
-            std::cout << "Stable sort TEST\n";
-            uni = dg::detail::find_unique_stable_sort( gIdx);
-        }
-        auto sortedgIdx = gIdx;
-        thrust::scatter( gIdx.begin(), gIdx.end(),
-                     uni.gather1.begin(), sortedgIdx.begin());
-        std::cout<< "Sorted indices \n";
-        for( unsigned u=0; u<sortedgIdx.size(); u++)
-            std::cout <<"{"<<sortedgIdx[u][0]<<","<<sortedgIdx[u][1]<<"} ";
-        std::cout<<std::endl;
-        std::cout<< "Unique values \n";
-        for( unsigned u=0; u<uni.unique.size(); u++)
-            std::cout <<"{"<<uni.unique[u][0]<<","<<uni.unique[u][1]<<"} ";
-        std::cout<<std::endl;
-        std::cout<< "Howmany Unique values \n";
-        for( unsigned u=0; u<uni.unique.size(); u++)
-            std::cout <<uni.howmany[u]<<" ";
-        std::cout<<std::endl;
-        auto num = gIdx; // consistency test
-        thrust::gather( uni.gather2.begin(), uni.gather2.end(),
-                    uni.unique.begin(), num.begin());
-        std::cout<< "Sorted indices \n";
-        for( unsigned u=0; u<sortedgIdx.size(); u++)
-        {
-            std::cout <<"{"<<num[u][0]<<","<<num[u][1]<<"} ";
-            assert( num[u] == sortedgIdx[u]);
-        }
-        std::cout<<std::endl;
-
-        num = gIdx; // consistency test
-        auto sort_map = uni.gather1;
-        thrust::gather( uni.gather1.begin(), uni.gather1.end(),
-                    uni.gather2.begin(), sort_map.begin());
-        thrust::gather( sort_map.begin(), sort_map.end(),
-                    uni.unique.begin(), num.begin());
-        for( unsigned u=0; u<gIdx.size(); u++)
-            assert( num[u] == gIdx[u]);
-        std::cout << "Gather PASSED\n\n";
-    }
-
-    std::cout << "Test gIdx2unique_idx\n";
-    thrust::host_vector<int> bufferIdx;
-    auto recv_map = dg::gIdx2unique_idx( gIdx, bufferIdx);
-
-    for ( auto& idx : recv_map)
-    {
-        std::cout << "Receive ";
-        for( unsigned u=0; u<idx.second.size(); u++)
-            std::cout << "{"<<idx.first<<","<<idx.second[u]<<"} ";
-        std::cout << "\n";
-    }
 
     std::cout << "Test Local Gather Matrix\n";
     std::vector<int> idx = {7,4,5,2,4,9};
