33268b683e9009bd241ef25ba9820f9cadbaafaa
Matthias <mattwi@fysik.dtu.dk>
2024-12-20 21:38:34 +0100

    Remove MPI support for limiter_stencil
    We maybe could re-introduce non-overlapping comm format again!?

--- inc/dg/topology/stencil_mpit.cu
+++ inc/dg/topology/stencil_mpit.cu
@@ -8,6 +8,8 @@
 #include "../blas2.h"
 
 
+const double lx = 2.*M_PI;
+const double ly = 2.*M_PI;
 int main(int argc, char* argv[])
 {
     MPI_Init( &argc, &argv);
@@ -15,13 +17,19 @@ int main(int argc, char* argv[])
     MPI_Comm_rank( MPI_COMM_WORLD, &rank);
     MPI_Comm_size( MPI_COMM_WORLD, &size);
 
+    // TODO This is a terrible test -> how to know when it succeeded?
+
     if(rank==0)std::cout << "Test window stencil\n";
     std::vector<dg::bc> bcs = {dg::DIR, dg::NEU, dg::PER};
     for( auto bc : bcs)
     {
+        int dims2d[2] = {0,0};
+        MPI_Dims_create( size, 2, dims2d);
         MPI_Comm comm2d;
-        dg::mpi_init2d( bc, bc, comm2d);
-        dg::MPIGrid2d g2d( 0,1, 0,2, 3, 4, 2, bc, bc, comm2d);
+        std::stringstream ss2d;
+        ss2d<< dims2d[0]<<" "<<dims2d[1];
+        dg::mpi_init2d( bc, bc, comm2d, ss2d);
+        dg::MPIGrid2d g2d( 0,lx, 0,ly, 3, 4, 2, bc, bc, comm2d);
         auto x = dg::evaluate( dg::one, g2d), y(x);
         if(rank==0)std::cout << "Test "<<dg::bc2str( bc)<<" boundary:\n";
         auto stencil = dg::create::window_stencil( {3,3}, g2d, bc, bc);
@@ -29,10 +37,10 @@ int main(int argc, char* argv[])
         auto l2d = g2d.local();
         for( int r = 0; r<size; r++)
         {
-            for( unsigned i=0; i<l2d.Ny()*l2d.ny(); i++)
+            for( unsigned i=0; i<l2d.shape(1); i++)
             {
-                for( unsigned k=0; k<l2d.Nx()*l2d.nx(); k++)
-                    if(rank==r)std::cout << y.data()[i*l2d.Nx()*l2d.nx()+k] << " ";
+                for( unsigned k=0; k<l2d.shape(0); k++)
+                    if(rank==r)std::cout << y.data()[i*l2d.shape(0)+k] << " ";
                 if(rank==r)std::cout<< std::endl;
             }
             MPI_Barrier(MPI_COMM_WORLD);
@@ -41,10 +49,10 @@ int main(int argc, char* argv[])
         dg::blas2::stencil( dg::CSRSymvFilter(), stencil, x, y);
         for( int r = 0; r<size; r++)
         {
-            for( unsigned i=0; i<l2d.Ny()*l2d.ny(); i++)
+            for( unsigned i=0; i<l2d.shape(1); i++)
             {
-                for( unsigned k=0; k<l2d.Nx()*l2d.nx(); k++)
-                    if(rank==r)std::cout << y.data()[i*l2d.Nx()*l2d.nx()+k] << " ";
+                for( unsigned k=0; k<l2d.shape(0); k++)
+                    if(rank==r)std::cout << y.data()[i*l2d.shape(0)+k] << " ";
                 if(rank==r)std::cout<< std::endl;
             }
             MPI_Barrier(MPI_COMM_WORLD);
