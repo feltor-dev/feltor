99149d879213682cfda65213a4db06fa26eac63a
Matthias <mattwi@fysik.dtu.dk>
2024-12-20 21:48:03 +0100

    Fix mpi_projection and make better projection mpit

--- inc/dg/topology/mpi_projection.h
+++ inc/dg/topology/mpi_projection.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "dg/backend/typedefs.h"
+#include "dg/backend/index.h"
 #include "dg/backend/mpi_matrix.h"
-#include "dg/backend/mpi_collective.h"
+#include "dg/backend/mpi_gather.h"
 #include "mpi_grid.h"
 #include "projection.h"
 
@@ -21,7 +22,7 @@ namespace dg
         g_new.local(), g_old.global(), method);
     // mat is row distributed
     // mat has local row and global column indices
-    auto mpi_mat = dg::convert(  mat, g_old);
+    auto mpi_mat = dg::make_mpi_matrix(  mat, g_old);
 @endcode
  * @tparam ConversionPolicy (can be one of the MPI %grids ) has to have the members:
  *  - <tt> bool global2localIdx(unsigned,unsigned&,unsigned&) const; </tt>
@@ -38,43 +39,84 @@ namespace dg
  * @ingroup mpi_structures
  */
 template<class ConversionPolicy, class real_type>
-dg::MIHMatrix_t<real_type> convert( const dg::IHMatrix_t<real_type>& global, const ConversionPolicy& policy)
+dg::MIHMatrix_t<real_type> make_mpi_matrix(
+    const dg::IHMatrix_t<real_type>& global_cols, const ConversionPolicy& col_policy)
 {
-    //int rank;
-    //MPI_Comm_rank( policy.communicator(), &rank);
-    //// Our idea here is
-    //// 1. convert global.column_indices to local indices
-    //thrust::host_vector<std::array<int,2>> lIdx = gIdx2array( global.column_indices);
-    //// 2. get all the rows that have foreign indices and split into inner and outer matrix
-    //for( unsigned u=0; u<lIdx.size(); u++)
-    //    // ...
+    // Our approach here is:
+    // 1. Convert to coo format
+    // 2. mark rows in global_cols that are communicating
+    // we need to grab the entire row to ensure reproducibility (which is guaranteed by order of computations)
+    // 3. Convert inner rows to local and move outer rows to vectors (invalidating them in inner)
+    // 4. Use global columns vector to construct MPIGather
+    // 5. Use buffer index to construct local CooMat
+    int rank;
+    MPI_Comm_rank( col_policy.communicator(), &rank);
 
-
-    cusp::array1d<int, cusp::host_memory> buffer_idx;
-    auto recv_map = dg::gIdx2unique_idx( global.column_indices, buffer_idx, unique_global_idx);
-    dg::GeneralComm<dg::iHVec, thrust::host_vector<real_type>> comm( unique_global_idx, policy);
-    if( !comm.isCommunicating() )
+    const dg::IHMatrix_t<real_type>& A = global_cols;
+    std::vector<int> outer_row( A.num_rows, 0);
+    // 1st pass determine local rows
+    for(int i = 0; i < (int)A.num_rows; i++)
+    for (int jj = A.row_offsets[i]; jj < A.row_offsets[i+1]; jj++)
     {
-        cusp::array1d<int, cusp::host_memory> local_idx(global.column_indices), pids(local_idx);
-        bool success = true;
-        for(unsigned i=0; i<local_idx.size(); i++)
-            success = policy.global2localIdx(global.column_indices[i], local_idx[i], pids[i]);
-        assert( success);
-        dg::IHMatrix_t<real_type> local( global.num_rows, policy.local_size(), global.values.size());
-        // It is more memory efficient to leave the global communicator empty
-        comm = dg::GeneralComm< dg::iHVec, thrust::host_vector<real_type>>();
-        local.row_offsets=global.row_offsets;
-        local.column_indices=local_idx;
-        local.values=global.values;
-        return dg::MIHMatrix_t<real_type>( local, comm, dg::row_dist);
+        int lIdx=0, pid = 0;
+        assert( col_policy.global2localIdx( A.column_indices[jj], lIdx, pid));
+        if( pid != rank)
+            outer_row[i] = 1;
     }
-    dg::IHMatrix_t<real_type> local( global.num_rows, comm.buffer_size(), global.values.size());
-    local.row_offsets=global.row_offsets;
-    local.column_indices=buffer_idx;
-    local.values=global.values;
-    dg::MIHMatrix_t<real_type> matrix(   local, comm, dg::row_dist);
-    return matrix;
+
+    // 2nd pass: distribute entries
+    thrust::host_vector<std::array<int,2>> outer_col;
+    cusp::array1d<int, cusp::host_memory> inner_row, inner_col, buffer_row;
+    cusp::array1d<real_type, cusp::host_memory> inner_val, outer_val;
+    int buffer_row_idx = 0;
+    thrust::host_vector<int> row_scatter;
+    for(int i = 0; i < (int)A.num_rows; i++)
+    {
+        for (int jj = A.row_offsets[i]; jj < A.row_offsets[i+1]; jj++)
+        {
+            int lIdx=0, pid = 0;
+            col_policy.global2localIdx( A.column_indices[jj], lIdx, pid);
+            if( outer_row[i] == 1)
+            {
+                buffer_row.push_back( buffer_row_idx);
+                outer_col.push_back( {pid,lIdx});
+                outer_val.push_back( A.values[jj]);
+            }
+            else
+            {
+                inner_row.push_back( i);
+                inner_col.push_back( lIdx);
+                inner_val.push_back( A.values[jj]);
+            }
+        }
+        if( outer_row[i] == 1)
+        {
+            buffer_row_idx++;
+            row_scatter.push_back(i);
+        }
+    }
+    // 3. Now make MPI Gather object
+    thrust::host_vector<int> lColIdx;
+    auto gather_map = dg::gIdx2unique_idx( outer_col, lColIdx);
+    MPIGather<thrust::host_vector> mpi_gather( gather_map,
+            col_policy.communicator());
+    cusp::coo_matrix<int, real_type, cusp::host_memory> inner( A.num_rows,
+            col_policy.local_size(), inner_val.size());
+    inner.row_indices    = inner_row;
+    inner.column_indices = inner_col;
+    inner.values         = inner_val;
+    if( inner.row_indices.size() > 0)
+        inner.sort_by_row_and_column();
+    cusp::coo_matrix<int, real_type, cusp::host_memory> outer( buffer_row_idx,
+            mpi_gather.buffer_size(), outer_val.size());
+    outer.row_indices    = buffer_row;
+    outer.column_indices = lColIdx;
+    outer.values         = outer_val;
+    if( outer.row_indices.size() > 0)
+        outer.sort_by_row_and_column();
+    return { inner, outer, mpi_gather, row_scatter}; // implicitly convert
 }
+
 
 /**
  * @brief Convert a (column-distributed) matrix with global row and column indices to a row distributed matrix
@@ -91,7 +133,7 @@ dg::MIHMatrix_t<real_type> convert( const dg::IHMatrix_t<real_type>& global, con
     // now mat has global rows and global column indices
     auto mat_loc = dg::convertGlobal2LocalRows( mat, g_new);
     // now mat is row distributed with global column indices
-    auto mpi_mat = dg::convert(  mat_loc, g_old);
+    auto mpi_mat = dg::make_mpi_matrix(  mat_loc, g_old);
  * @endcode
  * @tparam ConversionPolicy (can be one of the MPI %grids ) has to have the members:
  *  - <tt> bool global2localIdx(unsigned,unsigned&,unsigned&) const; </tt>
@@ -114,21 +156,28 @@ dg::IHMatrix_t<real_type> convertGlobal2LocalRows( const dg::IHMatrix_t<real_typ
     // 0. Convert to coo matrix
     cusp::coo_matrix<int, real_type, cusp::host_memory> A = global;
     // 1. For all rows determine pid to which it belongs
-    auto gIdx = dg::gIdx2array( A.row_indices, row_policy);
-    // 2. Now scatter those rows to where they belong
-    cusp::array1d<int, cusp::host_memory> row_buffer, col_buffer, val_buffer;
-    dg::mpi_scatter( gIdx, A.row_indices,    row_buffer);
-    dg::mpi_scatter( gIdx, A.column_indices, col_buffer);
-    dg::mpi_scatter( gIdx, A.values,         val_buffer);
-    for(unsigned i=0; i<row_buffer.size(); i++)
-        assert( row_policy.global2localIdx(row_buffer[i], row_buffer[i], int() );
+    auto gIdx = dg::gIdx2gIdx( A.row_indices, row_policy);
+    std::map<int, cusp::array1d<int, cusp::host_memory>> rows, cols;
+    std::map<int, cusp::array1d<real_type, cusp::host_memory>> vals;
+    for( unsigned u=0; u<gIdx.size(); u++)
+    {
+        rows[gIdx[u][0]].push_back( gIdx[u][1]);
+        cols[gIdx[u][0]].push_back( A.column_indices[u]);
+        vals[gIdx[u][0]].push_back( A.values[u]);
+    }
+    // 2. Now send those rows to where they belong
+    auto row_buf = dg::mpi_permute( rows, row_policy.communicator());
+    auto col_buf = dg::mpi_permute( cols, row_policy.communicator());
+    auto val_buf = dg::mpi_permute( vals, row_policy.communicator());
 
     cusp::coo_matrix<int, real_type, cusp::host_memory> B(
-        row_policy.local_size(), global.num_cols, row_buffer.size());
-    B.row_indices    = row_buffer;
-    B.column_indices = col_buffer;
-    B.values         = val_buffer;
-    B.sort_by_row_and_column();
+        row_policy.local_size(), global.num_cols,
+        dg::detail::flatten_map(row_buf).size());
+    B.row_indices    = dg::detail::flatten_map(row_buf);
+    B.column_indices = dg::detail::flatten_map(col_buf);
+    B.values         = dg::detail::flatten_map(val_buf);
+    if( B.row_indices.size() > 0) // BugFix
+        B.sort_by_row_and_column();
     return dg::IHMatrix_t<real_type>(B);
     // 4. Reduce on identical rows/cols
     // .... Maybe later
@@ -148,7 +197,7 @@ dg::IHMatrix_t<real_type> convertGlobal2LocalRows( const dg::IHMatrix_t<real_typ
     // now mat has global rows and global column indices
     auto mat_loc = dg::convertGlobal2LocalRows( mat, g_new);
     // now mat is row distributed with global column indices
-    auto mpi_mat = dg::convert(  mat_loc, g_old);
+    auto mpi_mat = dg::make_mpi_matrix(  mat_loc, g_old);
  * @endcode
  * @tparam ConversionPolicy (can be one of the MPI %grids ) has to have the members:
  *  - <tt> bool local2globalIdx(unsigned,unsigned&,unsigned&) const; </tt>
@@ -187,7 +236,7 @@ dg::MIHMatrix_t<typename MPITopology::value_type> interpolation( const MPITopolo
 {
     dg::IHMatrix_t<typename MPITopology::value_type> mat = dg::create::interpolation(
         g_new.local(), g_old.global(), method);
-    return convert(  mat, g_old);
+    return make_mpi_matrix(  mat, g_old);
 }
 // This is now dg::create::prolongation
 /////@copydoc dg::create::interpolation(const RealGrid1d&,const RealGrid1d&,std::string)
@@ -197,7 +246,7 @@ dg::MIHMatrix_t<typename MPITopology::value_type> interpolation( const MPITopolo
 //{
 //    dg::IHMatrix_t<real_type> mat = dg::create::interpolation(
 //        g_new.local(), g_old.global(), method);
-//    return convert(  mat, g_old);
+//    return make_mpi_matrix(  mat, g_old);
 //}
 
 ///@copydoc dg::create::projection(const RealGrid1d&,const RealGrid1d&,std::string)
@@ -209,7 +258,7 @@ dg::MIHMatrix_t<typename MPITopology::value_type> projection( const MPITopology&
         g_new.global(), g_old.local(), method);
     convertLocal2GlobalCols( mat, g_old);
     auto mat_loc = convertGlobal2LocalRows( mat, g_new);
-    return convert(  mat_loc, g_old);
+    return make_mpi_matrix(  mat_loc, g_old);
 }
 
 /**
@@ -219,7 +268,7 @@ dg::MIHMatrix_t<typename MPITopology::value_type> projection( const MPITopology&
  * with local row and global column indices using the given points and
  * @code
  * auto mat = dg::create::interpolation ( x, g.global(), bcx, method);
- * return dg::convert( mat, g);
+ * return dg::make_mpi_matrix( mat, g);
  * @endcode
  *
  * @copydetails interpolation(const thrust::host_vector<real_type>&,const RealGrid1d<real_type>&,dg::bc,std::string)
@@ -233,7 +282,7 @@ dg::MIHMatrix_t<real_type> interpolation(
 {
     dg::IHMatrix_t<real_type> mat = dg::create::interpolation( x, g.global(),
             bcx, method);
-    return convert(  mat, g);
+    return make_mpi_matrix(  mat, g);
 }
 /**
  * @brief Create an MPI row distributed interpolation 2d matrix
@@ -242,7 +291,7 @@ dg::MIHMatrix_t<real_type> interpolation(
  * with local row and global column indices using the given points and
  * @code
  * auto mat = dg::create::interpolation ( x,y, g.global(), bcx, bcy, method);
- * return dg::convert( mat, g);
+ * return dg::make_mpi_matrix( mat, g);
  * @endcode
  *
  * @copydetails interpolation(const thrust::host_vector<real_type>&,const thrust::host_vector<real_type>&,const aRealTopology2d<real_type>&,dg::bc,dg::bc,std::string)
@@ -257,7 +306,7 @@ dg::MIHMatrix_t<real_type> interpolation(
 {
     dg::IHMatrix_t<real_type> mat = dg::create::interpolation( x,y, g.global(),
             bcx, bcy, method);
-    return convert(  mat, g);
+    return make_mpi_matrix(  mat, g);
 }
 
 /**
@@ -267,7 +316,7 @@ dg::MIHMatrix_t<real_type> interpolation(
  * with local row and global column indices using the given points
  * @code
  * auto mat = dg::create::interpolation ( x,y,z, g.global(), bcx, bcy, bcz, method);
- * return dg::convert( mat, g);
+ * return dg::make_mpi_matrix( mat, g);
  * @endcode
  *
  * @copydetails interpolation(const thrust::host_vector<real_type>&,const thrust::host_vector<real_type>&,const thrust::host_vector<real_type>&,const aRealTopology3d<real_type>&,dg::bc,dg::bc,dg::bc,std::string)
@@ -283,7 +332,7 @@ dg::MIHMatrix_t<real_type> interpolation(
 {
     dg::IHMatrix_t<real_type> mat = dg::create::interpolation( x,y,z,
             g.global(), bcx, bcy, bcz, method);
-    return convert(  mat, g);
+    return make_mpi_matrix(  mat, g);
 }
 
 
--- inc/dg/topology/projection_mpit.cu
+++ inc/dg/topology/projection_mpit.cu
@@ -15,18 +15,55 @@ double sine( double x, double y){return sin(x)*sin(y);}
 double sine( double x, double y, double z){return sin(x)*sin(y);}
 //Actually this file is a test for fast_interpolation
 
+template<class Matrix, class Grid>
+void projection_test(const Matrix& proj, const Matrix& inte,
+            const Grid& g2o, const Grid& g2n ,
+            const dg::MHVec& w2do, const dg::MHVec& w2dn )
+{
+    int rank;
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
+    const dg::MHVec sinO( dg::evaluate( sine, g2o)),
+                    sinN( dg::evaluate( sine, g2n));
+
+    dg::MHVec sinP( sinN);
+    dg::blas2::gemv( proj, sinO, sinP);
+
+    double value0 = sqrt(dg::blas2::dot( sinO, w2do, sinO));
+    if(rank==0)std::cout << "Original vector     "<<value0 << "\n";
+    double value1 = sqrt(dg::blas2::dot( sinP, w2dn, sinP));
+    if(rank==0)std::cout << "Projected vector    "<<value1 << "\n";
+    if(rank==0)std::cout << "Difference in Norms "<<
+                value0 - value1 << std::endl;
+    dg::blas1::axpby( 1., sinN, -1., sinP);
+    double value2 = sqrt( dg::blas2::dot( sinP, w2dn, sinP)/
+                          dg::blas2::dot( sinN, w2dn, sinN));
+    if(rank==0)std::cout << "Difference between projection and evaluation      "
+                         <<value2<<"\n";
+
+    dg::MHVec sinI( sinO);
+    dg::blas2::gemv( inte, sinN, sinI);
+
+    value0 = sqrt(dg::blas2::dot( sinN, w2dn, sinN));
+    if(rank==0)std::cout << "Original vector     "<<value0 << "\n";
+    value1 = sqrt(dg::blas2::dot( sinI, w2do, sinI));
+    if(rank==0)std::cout << "Interpolated vec    "<<value1 << "\n";
+    if(rank==0)std::cout << "Difference in Norms "<<
+                 value0 - value1 << "\n";
+
+    dg::blas1::axpby( 1., sinO, -1., sinI);
+    value2 = sqrt( dg::blas2::dot( sinI, w2do, sinI)/
+                   dg::blas2::dot( sinO, w2do, sinO));
+    if(rank==0)std::cout << "Difference between interpolation and evaluation   "
+                         <<value2<<"\n"<<std::endl;
+}
+
+
 int main(int argc, char* argv[])
 {
     MPI_Init( &argc, &argv);
     int rank, size;
     MPI_Comm_size( MPI_COMM_WORLD, &size);
     MPI_Comm_rank( MPI_COMM_WORLD, &rank);
-    //if(size!=4)
-    //{
-    //    std::cerr << "Please run with 4 processes!\n";
-    //    MPI_Finalize();
-    //    return 0;
-    //}
     int dims[3] = {0,0,0};
     MPI_Dims_create( size, 3, dims);
     std::stringstream ss;
@@ -36,70 +73,68 @@ int main(int argc, char* argv[])
 
     ///%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%//
     if(rank==0)std::cout << "TEST 2D and 3D\n";
-    unsigned n_old = 9, n_new = 3, N_old = 40, N_new = 20;
+    unsigned n_old = 9, n_new = 3, N_old = 20, N_new = 10;
     if(rank==0)std::cout << "Fine   Grid "<< n_old << " x "<<N_old <<"\n";
     if(rank==0)std::cout << "Coarse Grid "<< n_new << " x "<<N_new <<"\n";
     dg::MPIGrid3d g2o (0, M_PI, 0, M_PI, 0,1, n_old, N_old, N_old, 4, comm);
     dg::MPIGrid3d g2n (0, M_PI, 0, M_PI, 0,1, n_new, N_new, N_new, 4, comm);
-    dg::MIHMatrix inte2d = dg::create::interpolation( g2n, g2o);
-    auto proj2d = dg::create::fast_projection( g2o, n_old/n_new, N_old/N_new, N_old/N_new);
-    auto fast_inte2d = dg::create::fast_interpolation( g2n, n_old/n_new, N_old/N_new, N_old/N_new);
-    auto forward = dg::create::fast_transform( dg::DLT<double>::forward( n_old),
-            dg::DLT<double>::forward( n_old), g2o);
-    auto backward = dg::create::fast_transform( dg::DLT<double>::backward( n_old),
-            dg::DLT<double>::backward( n_old), g2o);
-    const dg::MHVec sinO( dg::evaluate( sine, g2o)),
-                                sinN( dg::evaluate( sine, g2n));
     const dg::MHVec w2do = dg::create::weights( g2o);
     const dg::MHVec w2dn = dg::create::weights( g2n);
-    dg::MHVec sinP( sinN), sinI(sinO), sinF(sinO);
-    dg::blas2::gemv( proj2d, sinO, sinP); //FAST PROJECTION
-    double value0 = sqrt(dg::blas2::dot( sinO, w2do, sinO));
-    if(rank==0)std::cout << "Original vector     "<<value0 << "\n";
-    double value1 = sqrt(dg::blas2::dot( sinP, w2dn, sinP));
-    if(rank==0)std::cout << "Projected vector    "<<value1 << "\n";
-    if(rank==0)std::cout << "Difference in Norms "<<value0-value1 << std::endl;
-    dg::blas1::axpby( 1., sinN, -1., sinP);
-    double value2 = sqrt(dg::blas2::dot( sinP, w2dn, sinP)/dg::blas2::dot(sinN, w2dn, sinN));
-    if(rank==0)std::cout << "Difference between projection and evaluation      "<<value2<<"\n";
-    dg::blas2::gemv( inte2d, sinO, sinP);
-    value1 = sqrt(dg::blas2::dot( sinP, w2dn, sinP));
-    if(rank==0)std::cout << "Interpolated vec    "<<value1 << "\n";
-    value0 = sqrt(dg::blas2::dot( sinO, w2do, sinO));
-    if(rank==0)std::cout << "Difference in Norms "<<value0 - value1 << "\n" << std::endl;
-    dg::blas2::gemv( fast_inte2d, sinN, sinI);
-    value2 = sqrt(dg::blas2::dot( sinI, w2do, sinI));
-    if(rank==0)std::cout << "Fast Interpolated vec "<< value2 << "\n";
-    double value3 = sqrt(dg::blas2::dot( sinN, w2dn, sinN));
-    if(rank==0)std::cout << "Difference in Norms   "<<value2 - value3  << "\n" << std::endl;
+    if(rank==0)std::cout << "TEST FAST PROJECT-INTERPOLATE\n";
+    auto fast_proj2d = dg::create::fast_projection( g2o, n_old/n_new, N_old/N_new,
+            N_old/N_new);
+    auto fast_inte2d = dg::create::fast_interpolation( g2n, n_old/n_new,
+            N_old/N_new, N_old/N_new);
+    projection_test( fast_proj2d, fast_inte2d, g2o, g2n, w2do, w2dn);
+    if(rank==0)std::cout << "TEST ORIGINAL PROJECT-INTERPOLATE\n";
+    auto proj2d = dg::create::projection( g2n, g2o);
+    auto inte2d = dg::create::interpolation( g2o, g2n);
+    projection_test( proj2d, inte2d, g2o, g2n, w2do, w2dn);
+
+    if(rank==0)std::cout << "TEST FAST TRANSFORM\n";
+    auto forward = dg::create::fast_transform( dg::DLT<double>::forward(
+                n_old), dg::DLT<double>::forward( n_old), g2o);
+    auto backward = dg::create::fast_transform( dg::DLT<double>::backward(
+                n_old), dg::DLT<double>::backward( n_old), g2o);
+    const dg::MHVec sinO( dg::evaluate( sine, g2o));
+    dg::MHVec sinF(sinO), sinI(sinO);
     dg::blas2::gemv( forward, sinO, sinF);
     dg::blas2::gemv( backward, sinF, sinI);
     dg::blas1::axpby( 1., sinO, -1., sinI);
-    value2 = sqrt(dg::blas2::dot( sinI, w2do, sinI));
-    if(rank==0)std::cout << "Forward-Backward Error   "<<value2 << " (Must be zero)\n" << std::endl;
+    double value = sqrt(dg::blas2::dot( sinI, w2do, sinI));
+    if(rank==0)std::cout << "Forward-Backward Error   "<<value
+                         << " (Must be zero)\n" << std::endl;
     ///%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%//
     if(rank==0)std::cout << "TEST correct mpi matrix conversion \n";
-    inte2d = dg::create::interpolation( g2o, g2n, "linear");
-    dg::IHMatrix inte2dg = dg::create::interpolation( g2o.global(), g2n.global(), "linear");
-    sinP = dg::evaluate( sine, g2n);
-    dg::blas2::gemv( inte2d, sinP, sinI);
-    value0 = sqrt(dg::blas2::dot( sinI, w2do, sinI));
-    dg::HVec sinNg = dg::evaluate( sine, g2n.global()), sinOg(dg::evaluate( sine,g2o.global()));
-    const dg::HVec w2dog = dg::create::weights( g2o.global());
-    dg::blas2::gemv( inte2dg, sinNg, sinOg);
-    value1 = sqrt( dg::blas2::dot( sinOg, w2dog, sinOg));
-    if(rank==0)std::cout << "MPI Interpolation: difference in Norms "<<value0 - value1 << "\n" << std::endl;
+    for( auto method : {"dg", "linear", "cubic"})
+    {
+        if(rank==0) std::cout << method <<"\n";
+        dg::MIHMatrix inte2d = dg::create::interpolation(
+                g2o, g2n, method);
+        dg::IHMatrix inte2dg = dg::create::interpolation(
+                g2o.global(), g2n.global(), method);
+        dg::MHVec sinP = dg::evaluate( sine, g2n);
+        dg::blas2::gemv( inte2d, sinP, sinI);
+        double value0 = sqrt(dg::blas2::dot( sinI, w2do, sinI));
+        dg::HVec sinNg = dg::evaluate( sine, g2n.global()), sinOg(dg::evaluate(
+                    sine, g2o.global()));
+        const dg::HVec w2dog = dg::create::weights( g2o.global());
+        dg::blas2::gemv( inte2dg, sinNg, sinOg);
+        double value1 = sqrt( dg::blas2::dot( sinOg, w2dog, sinOg));
+        if(rank==0)std::cout << "MPI Interpolation: difference in Norms "<<value0 - value1 << "\n" << std::endl;
 
-    dg::MIHMatrix project2d = dg::create::projection( g2n, g2o, "linear");
-    dg::IHMatrix project2dg = dg::create::projection( g2n.global(), g2o.global(), "linear");
-    sinI = dg::evaluate( sine, g2o);
-    dg::blas2::gemv( project2d, sinI, sinP);
-    value0 = sqrt(dg::blas2::dot( sinP, w2dn, sinP));
-    sinOg = dg::evaluate( sine, g2o.global());
-    const dg::HVec w2dng = dg::create::weights( g2n.global());
-    dg::blas2::gemv( project2dg, sinOg, sinNg);
-    value1 = sqrt( dg::blas2::dot( sinNg, w2dng, sinNg));
-    if(rank==0)std::cout << "MPI Projection   : difference in Norms "<<value0 - value1 << "\n" << std::endl;
+        dg::MIHMatrix project2d = dg::create::projection( g2n, g2o, method);
+        dg::IHMatrix project2dg = dg::create::projection( g2n.global(),
+                g2o.global(), method);
+        sinI = dg::evaluate( sine, g2o);
+        dg::blas2::gemv( project2d, sinI, sinP);
+        value0 = sqrt(dg::blas2::dot( sinP, w2dn, sinP));
+        sinOg = dg::evaluate( sine, g2o.global());
+        const dg::HVec w2dng = dg::create::weights( g2n.global());
+        dg::blas2::gemv( project2dg, sinOg, sinNg);
+        value1 = sqrt( dg::blas2::dot( sinNg, w2dng, sinNg));
+        if(rank==0)std::cout << "MPI Projection   : difference in Norms "<<value0 - value1 << "\n" << std::endl;
+    }
 
     MPI_Finalize();
     return 0;
