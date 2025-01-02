090088c77b80380360fb8c77128a8105128be0d1
Matthias <mattwi@fysik.dtu.dk>
2025-01-02 20:01:46 +0100

    Write nc file in terms of easy_atts

--- inc/file/nc_file.h
+++ inc/file/nc_file.h
@@ -1,9 +1,8 @@
 #pragma once
 
-#include <variant>
-#include <netcdf.h>
 #include "nc_error.h"
 #include "easy_dims.h"
+#include "easy_atts.h"
 #include "easy_output.h"
 #include "easy_input.h"
 
@@ -23,17 +22,11 @@ enum NcFileMode
 {
     nc_nowrite,  //!< NC_NOWRITE Open an existing file for read-only access, fail if it does not exist
     nc_write,    //!< NC_WRITE Open an existing file for read and write access, fail if it does not exist
-    nc_clobberr, //!< NC_CLOBBER Create a new file for read and write access, overwrite if file exists
+    nc_clobber, //!< NC_CLOBBER Create a new file for read and write access, overwrite if file exists
     nc_noclobber //!< NC_NOCLOBBER Create new file for read and write access, fail if already exists
-}
+};
 
 
-// Utility type to simplify dealing with heterogeneous types
-// We can write utility to convert json -> WrappedJsonValue -> nc_att_t
-// and back
-using nc_att_t = std::variant<int, unsigned, float, double, bool, std::string,
-      std::vector<int>, std::vector<unsigned>, std::vector<float>,
-      std::vector<double>, std::vector<bool>>;
 
 // This is a singleton that cannot be copied/assigned but only moved/move-assign
 // Only Netcdf-4 files are supported
@@ -79,7 +72,7 @@ struct SerialNcFile
         }
         catch (NC_Error &e)
         {
-            cerr << e.what() << endl;
+            std::cerr << e.what() << std::endl;
         }
     }
     ///////////////////// open/close /////////
@@ -92,11 +85,11 @@ struct SerialNcFile
       @param ncFileFlags File flags from netcdf.h
       @sa nc_open
     */
-    void open(const std::string& path, enum NcFileMode mode = nc_nowrite);
+    void open(const std::string& path, enum NcFileMode mode = nc_nowrite)
     {
         // Like a std::fstream opening fails if file already associated
         if( m_open)
-            throw std::exception( "Close file before opening a new one!");
+            throw std::runtime_error( "Close file before opening a new one!");
 
         NC_Error_Handle err;
         switch (mode)
@@ -152,7 +145,7 @@ struct SerialNcFile
     }
 
     /////////////// Groups /////////////////
-    void def_group( std::string name)
+    void def_grp( std::string name)
     {
         int new_grp_id = 0;
         NC_Error_Handle err;
@@ -162,14 +155,19 @@ struct SerialNcFile
     }
     // using inq_group_parent we can use ".." to go up in the hierarchy
     // All subsequent calls to Atts, Dims and Vars are made to group
-    void set_group( std::string name)
+    // Empty string goes back to root group
+    void set_grp( std::string name = "")
     {
         NC_Error_Handle err;
         if( !m_open)
-            throw std::exception( "Can't set group in a closed file!");
+            throw std::runtime_error( "Can't set group in a closed file!");
         if ( name == ".")
             return;
-        if ( name == "..")
+        if ( name == "")
+        {
+            m_grp = m_ncid;
+        }
+        else if ( name == "..")
         {
             err = nc_inq_grp_parent( m_grp, &m_grp);
         }
@@ -179,32 +177,32 @@ struct SerialNcFile
             err = nc_inq_grp_ncid( m_ncid, name.c_str(), &m_grp);
         }
     }
-    void rename_group( std::string old_name, std::string new_name)
+    void rename_grp( std::string old_name, std::string new_name)
     {
         if( !m_open)
-            throw std::exception( "Can't rename group in a closed file!");
+            throw std::runtime_error( "Can't rename group in a closed file!");
         NC_Error_Handle err;
         int old_grp;
         err = nc_inq_grp_ncid( m_ncid, old_name.c_str(), &old_grp);
         err = nc_rename_grp( old_grp, new_name.c_str());
     }
 
-    std::vector<std::string> get_groups( ) const
+    std::vector<std::string> get_grps( ) const
     {
         if( !m_open)
-            throw std::exception( "Can't get groups in a closed file!");
+            throw std::runtime_error( "Can't get groups in a closed file!");
         NC_Error_Handle err;
         int num_grps;
         err = nc_inq_grps( m_ncid, &num_grps, NULL);
         if( num_grps == 0)
             return {};
         std::vector<int> group_ids( num_grps);
-        err = nc_inq_grps( m_ncid, &num_grps, &num_grps[0]);
+        err = nc_inq_grps( m_ncid, &num_grps, &group_ids[0]);
         std::vector<std::string> groups( num_grps);
 
         for( int i=0; i<num_grps; i++)
         {
-            int len;
+            size_t len;
             err = nc_inq_grpname_len( group_ids[i], &len);
             char name [len];
             err = nc_inq_grpname( m_grp, name);
@@ -217,7 +215,7 @@ struct SerialNcFile
     void def_dim( std::string name, size_t size = NC_UNLIMITED)
     {
         if( !m_open)
-            throw std::exception( "Can't define dimension in a closed file!");
+            throw std::runtime_error( "Can't define dimension in a closed file!");
         NC_Error_Handle err;
         int dim_id;
         err = nc_def_dim( m_grp, name.c_str(), size,&dim_id);
@@ -225,29 +223,29 @@ struct SerialNcFile
     void rename_dim( std::string old_name, std::string new_name)
     {
         if( !m_open)
-            throw std::exception( "Can't renam dimension in a closed file!");
+            throw std::runtime_error( "Can't renam dimension in a closed file!");
         int dimid;
-        NC_Error_handle err;
+        NC_Error_Handle err;
         err = nc_inq_dimid( m_grp, old_name.c_str(), &dimid);
         err = nc_rename_dim( m_grp, dimid, new_name.c_str());
     }
-    size_t get_size( std::string name) const
+    size_t get_dim_size( std::string name) const
     {
         if( !m_open)
-            throw std::exception( "Can't get dimension in a closed file!");
-        NC_Error_handle err;
+            throw std::runtime_error( "Can't get dimension in a closed file!");
+        NC_Error_Handle err;
         int dimid;
         err = nc_inq_dimid( m_grp, name.c_str(), &dimid);
         size_t len;
-        err = nc_inq_dimlen( m_grp, name.c_str(), &len);
+        err = nc_inq_dimlen( m_grp, dimid, &len);
         return len;
 
     }
     std::vector<std::string> get_dims() const
     {
         if( !m_open)
-            throw std::exception( "Can't get dimension in a closed file!");
-        NC_Error_handle err;
+            throw std::runtime_error( "Can't get dimension in a closed file!");
+        NC_Error_Handle err;
         int ndims;
         err = nc_inq_ndims( m_grp, &ndims);
         // Dimension ids are 0, 1, 2, ... in the order in which the dimensions were defined
@@ -256,7 +254,7 @@ struct SerialNcFile
         {
             char dimname [ NC_MAX_NAME+1];
             err = nc_inq_dimname( m_grp, dimid, dimname);
-            dims[i] = dimname;
+            dims[dimid] = dimname;
         }
         return dims;
     }
@@ -269,137 +267,68 @@ struct SerialNcFile
     // Empty var string makes a global (to the group) attribute
     // Overwrites existing!?
     // Strong attribute set
-    template<class T>
-    void set_att( std::string id, std::pair<std::string, T> att)
+    template<class S, class T> // S is string or const char*, T can be nc_att_t
+    void set_att( std::string id, const std::pair<S, T>& att)
     {
-        set_att( id, std::make_tuple( att.first, getNCDataType<T>(),
-                    std::vector<T>(1,att.second)));
+        int varid = name2varid( id, "Can't set attribute in a closed file!");
+        dg::file::set_att( m_grp, varid, att);
     }
 
-    template<class T>
-    void set_att( std::string id, std::pair<std::string, std::vector<T>> att)
+    template<class S, class T> // T cannot be nc_att_t
+    void set_att( std::string id, const std::tuple<S,nc_type, T>& att)
     {
-        set_att( id, std::make_tuple( att.first, getNCDataType<T>(),
-                    att.second));
+        int varid = name2varid( id, "Can't set attribute in a closed file!");
+        dg::file::set_att( m_grp, varid, att);
     }
-    // Amazing
-    void set_att( std::string id, std::pair<std::string, nc_att_t> att)
-    {
-        std::string name = att.first;
-        const nc_att_t v = att.second;
-        std::visit( [this,id,name]( auto&& arg) {
-                this->set_att( id, std::make_pair( name, arg));
-                        }, v);
-    }
-
-    template<class T> // T cannot be nc_att_t
-    void set_att( std::string id, std::tuple<std::string, nc_type, T> att)
-    {
-        set_att( id, std::make_tuple( std::get<0>(att), std::get<1>(att),
-                    std::vector<T>( 1, std::get<2>(att)) );
-    }
-
-    // This function works for compound types
-    template<class T> // T cannot be nc_att_t
-    void set_att( std::string id,
-        std::tuple<std::string, nc_type, std::vector<T>> att);
-
-
     // Iterable can be e.g. std::vector<std::pair...>, std::map , etc.
     template<class Iterable> // *it must be usable in set_att
     void set_atts( std::string id, const Iterable& atts)
     {
-        for( const auto& it : atts)
-            set_att( id, it);
+        int varid = name2varid( id, "Can't set attributes in a closed file!");
+        dg::file::set_atts( m_grp, varid, atts);
     }
 
     /////////////////// Attribute getters
 
-    dg::file::nc_att_t get_att_t( std::string id,
-            std::string att_name) const
+    dg::file::nc_att_t get_att_t( std::string id, std::string att_name) const
     {
         int varid = name2varid( id, "Can't get attribute in a closed file!");
-        auto name = att.c_str();
-        size_t size;
-        err = nc_inq_attlen( m_grp, varid, name, &size);
-        nc_type xtype;
-        err = nc_inq_atttype( m_grp, varid, name, &xtype);
-        if ( xtype == NC_INT and size == 1)
-            return get_att<int>( id, att_name);
-        else if ( xtype == NC_INT and size != 1)
-            return get_att_v<int>( id, att_name);
-        else if ( xtype == NC_UINT and size == 1)
-            return get_att<unsigned>( id, att_name);
-        else if ( xtype == NC_UINT and size != 1)
-            return get_att_v<unsigned>( id, att_name);
-        else if ( xtype == NC_FLOAT and size == 1)
-            return get_att<float>( id, att_name);
-        else if ( xtype == NC_FLOAT and size != 1)
-            return get_att_v<float>( id, att_name);
-        else if ( xtype == NC_DOUBLE and size == 1)
-            return get_att<double>( id, att_name);
-        else if ( xtype == NC_DOUBLE and size != 1)
-            return get_att_v<double>( id, att_name);
-        else if ( xtype == NC_BYTE and size == 1)
-            return get_att<bool>( id, att_name);
-        else if ( xtype == NC_BYTE and size != 1)
-            return get_att_v<bool>( id, att_name);
-        else if ( xtype == NC_STRING )
-            return get_att<std::string>( id, att_name);
-        else
-            throw std::runtime_error( "Cannot convert attribute type to nc_att_t");
+        return dg::file::get_att_t( m_grp, varid, att_name);
     }
 
     template<class T>
     T get_att_i( std::string id, std::string att_name, unsigned idx = 0) const
     {
-        auto vec = get_att_v( id, att_name);
+        auto vec = get_att_v<T>( id, att_name);
         return vec[idx];
     }
 
     // This works for compound types
     template<class T>
-    std::vector<T> get_att_v( std::string id, std::string att_name) const;
+    std::vector<T> get_att_v( std::string id, std::string att_name) const
+    {
+        int varid = name2varid( id, "Can't get attribute in a closed file!");
+        return dg::file::get_att_v<T>( m_grp, varid, att_name);
+    }
+    template<class T>
+    T get_att( std::string id, std::string att_name) const
+    {
+        nc_att_t att = get_att_t( id, att_name);
+        return std::get<T>( att);
+    }
 
-    // utility overloads to be able to implement get_atts
-    template<class T>
-    void get_att( std::string id, std::string att_name, T& att) const
-    {
-        att = get_att_i<T>( id, att_name);
-    }
-    template<class T>
-    void get_att( std::string id, std::string att_name, std::vector<T>& att) const
-    {
-        att = get_att_v<T>( id, att_name);
-    }
-    void get_att( std::string id, std::string att_name, dg::file::nc_att_t& att) const
-    {
-        att = get_att_t( id, att_name);
-    }
 
     // Get all attributes of a given type
     template<class T> // T can be nc_att_t
-    std::map<std::string, T> get_atts( std::string id) const
+    std::map<std::string, T> get_atts( std::string id = ".") const
     {
         int varid = name2varid( id, "Can't get attributes in a closed file!");
-        NC_Error_Handle err;
-        int number;
-        if( varid == NC_GLOBAL)
-            err = nc_inq_natts( m_grp, &number);
-        else
-            err = nc_inq_varnatts( m_grp, varid, &number);
-        std::map < std::string, T> map;
-        for( int i=0; i<number; i++)
-        {
-            char name[NC_MAX_NAME]; // 256
-            err = nc_inq_attname( ncid, varid, i, name);
-            map[name] = T{};
-            get_att( id, att_name, map[name]);
-        }
-        return map;
+        return dg::file::get_atts<T>( m_grp, varid);
     }
+    //std::vector<std::tuple<std::string, nc_type, std::any>> get_atts( std::string id = ".") const;
 
 
+    /// Remove an attribute
     void rm_att( std::string id, std::string att)
     {
         int varid = name2varid( id, "Can't delete attribute in a closed file!");
@@ -407,6 +336,7 @@ struct SerialNcFile
         NC_Error_Handle err;
         err = nc_del_att( m_grp, varid, name);
     }
+    /// Rename an attribute
     void rename_att( std::string id, std::string old_att_name, std::string new_att_name)
     {
         int varid = name2varid( id, "Can't delete attribute in a closed file!");
@@ -416,7 +346,6 @@ struct SerialNcFile
         err = nc_rename_att( m_grp, varid, old_name, new_name);
     }
 
-    std::vector<std::tuple<std::string, nc_type, std::any>> get_atts( std::string id = ".") const;
 
     ////////////// Variables ////////////////////////
     // Overwrite existing?
@@ -433,9 +362,9 @@ struct SerialNcFile
     //        const Attributes& atts);
 
     template<class ContainerType>
-    void put_var( std::string name, const ContainerType& data, unsigned slice=0)
+    void put_var( std::string name, const ContainerType& data, unsigned slice=0);
     template<class ContainerType>
-    void stack_var( std::string name, const ContainerType& data)
+    void stack_var( std::string name, const ContainerType& data);
 
     //template<class Attributes, class ContainerType>
     //void defput_var( std::string name,
@@ -444,7 +373,7 @@ struct SerialNcFile
     //        const ContainerType& data)
 
     template<class ContainerType>
-    void get_var( std::string name, ContainerType& data, unsigned slice=0) const
+    void get_var( std::string name, ContainerType& data, unsigned slice=0) const;
 
     std::vector<std::string> get_vars() const;
     std::vector<unsigned> get_shape( std::string var) const;
@@ -453,7 +382,7 @@ struct SerialNcFile
     int name2varid( std::string id, std::string error_message) const
     {
         if( !m_open)
-            throw std::exception( error_message );
+            throw std::runtime_error( error_message );
         NC_Error_Handle err;
         int varid;
         if ( id == ".")
@@ -479,124 +408,6 @@ struct SerialNcFile
 };
 
 
-template<class T>
-void SerialNcFile::set_att( std::string id,
-    std::tuple<std::string, nc_type, std::vector<T>> att)
-{
-    int varid = name2varid( id, "Can't set attribute in a closed file!");
-    auto name = std::get<0>(att).c_str();
-    nc_type xtype = std::get<1>(att);
-    const std::vector<T>& data = std::get<2>(att);
-    unsigned size = data.size();
-    int ncid = m_grp;
-    NC_Error_Handle err;
-    if ( xtype == NC_INT)
-    {
-        err = nc_put_att_int( ncid, varid, name, size, &data[0]);
-    }
-    else if ( xtype == NC_UINT)
-    {
-        err = nc_put_att_uint( ncid, varid, name, size, &data[0]);
-    }
-    else if ( xtype == NC_FLOAT)
-    {
-        err = nc_put_att_float( ncid, varid, name, size, &data[0]);
-    }
-    else if ( xtype == NC_DOUBLE)
-    {
-        err = nc_put_att_double( ncid, varid, name, size, &data[0]);
-    }
-    else if ( xtype == NC_STRING)
-    {
-        if( size != 1)
-            throw std::runtime_error( "Cannot write a string array attribute to NetCDF");
-        err = nc_put_att_text( ncid, varid, name, data[0].size(), data[0].c_str());
-    }
-    else if( xtype == NC_BYTE) // counts as bool
-    {
-        std::vector<signed char> dataB(size);
-        for( int i=0; i<size; i++)
-            dataB[i] = data[i];
-        err = nc_put_att_schar( ncid, varid, name, NC_BYTE, size, &dataB[0]);
-    }
-    else // default
-    {
-        err = nc_put_att( ncid, varid, name, xtype, size, &data[0]);
-    }
-}
-
-template<class T>
-std::vector<T> SerialNcFile::get_att_v( std::string id, std::string att) const
-{
-    int varid = name2varid( id, "Can't get attribute in a closed file!");
-    auto name = att.c_str();
-    int ncid = m_grp;
-    size_t size;
-    err = nc_inq_attlen( m_grp, varid, name, &size);
-    nc_type xtype;
-    err = nc_inq_atttype( m_grp, varid, name, &xtype);
-    std::vector<T> data(size);
-    NC_Error_Handle err;
-    if ( xtype == NC_STRING or xtype == NC_CHAR)
-    {
-        std::string str( size, 'x');
-        err = nc_get_att_text( ncid, varid, name, &str[0]);
-        if constexpr ( std::is_convertible_v<std::string,T>)
-            data[0] = str;
-        else
-            throw std::runtime_error("Cannot convert NC_STRING to given type");
-    }
-    else if ( xtype == NC_INT)
-    {
-        std::vector<int> tmp( size);
-        err = nc_get_att_int( ncid, varid, name, &tmp[0]);
-        if constexpr ( std::is_convertible_v<int,T>)
-            std::copy( tmp.begin(), tmp.end(), data.begin());
-        else
-            throw std::runtime_error("Cannot convert NC_INT to given type");
-    }
-    else if ( xtype == NC_UINT)
-    {
-        std::vector<unsigned> tmp( size);
-        err = nc_get_att_uint( ncid, varid, name, &tmp[0]);
-        if constexpr ( std::is_convertible_v<unsigned,T>)
-            std::copy( tmp.begin(), tmp.end(), data.begin());
-        else
-            throw std::runtime_error("Cannot convert NC_UINT to given type");
-    }
-    else if ( xtype == NC_FLOAT)
-    {
-        std::vector<float> tmp( size);
-        err = nc_get_att_float( ncid, varid, name, &tmp[0]);
-        if constexpr ( std::is_convertible_v<float,T>)
-            std::copy( tmp.begin(), tmp.end(), data.begin());
-        else
-            throw std::runtime_error("Cannot convert NC_FLOAT to given type");
-    }
-    else if ( xtype == NC_DOUBLE)
-    {
-        std::vector<double> tmp( size);
-        err = nc_get_att_double( ncid, varid, name, &tmp[0]);
-        if constexpr ( std::is_convertible_v<double,T>)
-            std::copy( tmp.begin(), tmp.end(), data.begin());
-        else
-            throw std::runtime_error("Cannot convert NC_DOUBLE to given type");
-    }
-    else if( xtype == NC_BYTE) // counts as bool
-    {
-        std::vector<signed char> tmp(size);
-        err = nc_get_att_schar( ncid, varid, name, NC_BYTE, size, &tmp[0]);
-        if constexpr ( std::is_convertible_v<bool,T>)
-            std::copy( tmp.begin(), tmp.end(), data.begin());
-        else
-            throw std::runtime_error("Cannot convert NC_BYTE to given type");
-    }
-    else // default
-    {
-        err = nc_get_att( ncid, varid, name, &data[0]);
-    }
-    return data;
-}
 
 }// namespace file
 }// namespace dg
---
+++ inc/file/nc_file_t.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+#include "nc_file.h"
+
+int main()
+{
+	dg::file::SerialNcFile file;
+	try {
+		file.open("test.nc", dg::file::nc_clobber);
+	}
+	catch(dg::file::NC_Error & e)	{
+		std::cerr << e.what();
+	}
+	file.def_grp("subgroup");
+	file.set_grp("subgroup");
+	file.set_grp("..");
+	///////////////////////    Groups
+	file.def_grp("test");
+	file.set_grp("test");
+	file.set_grp(".."); // go to parent group
+	file.set_grp("."); // do nothing "." is current group
+	file.set_grp(); // go back to root group
+	std::vector<std::string> grps = file.get_grps();
+	// Traverse all groups
+	// probably recursive
+	///////////////////////   Attributes
+	// set attributes
+	file.set_att(".", std::pair{"title", "Hello"} );
+	file.set_att(".", std::pair{"same", "thing"} );
+	file.set_att(".", std::pair{"title", "Hello world"});
+    file.set_att(".", std::make_pair("ttt", 42) );
+	std::map<std::string, dg::file::nc_att_t> map_atts = {
+		{"anumber", 42},
+        {"axis", "X"},
+		{"real_vector", std::vector{1.0,2.0,3.0}},
+		{"int_vector", std::vector{1,2,3}}
+	};
+	file.set_atts(".", map_atts);
+	file.close();
+    file.open( "test.nc", dg::file::nc_nowrite);
+	// get attributes
+	auto title = file.get_att<std::string>(".", "title"); // get index 0 automatically
+    assert( title == "Hello world");
+	double t = file.get_att_i<double>(".", "ttt"); // get index 1
+    assert( t == 42);
+	auto ts = file.get_att_v<double>(".", "real_vector"); // get all indices as vector
+
+    std::vector<double> result = {1.0,2.0,3.0};
+    assert( ts == result );
+    std::map<std::string, dg::file::nc_att_t> atts =
+        file.get_atts<dg::file::nc_att_t>(); // get the map < string, variant>
+    std::string hello = "Hello world";
+    assert( std::get<std::string>(atts.at("title")) == hello);
+    int fortytwo = 42;
+    assert( std::get<int>(atts.at("ttt")) == fortytwo);
+    std::cout << "PASSED\n";
+
+	//struct NcVariable
+	//{
+	//	std::string name;
+	//	std::map<std::string, dg::file::att_t> atts;
+	//	lambda creator(...);
+	//};
+	//////////////////// Dimensions
+	//file.def_dim("x", 42);
+	//file.defput_dim("x", atts, abscissas); // Create dim and dimension variable
+	//file.def_dim("time");
+
+	//file.dim_size("x");
+	//file.get_size("x");
+	///////////////////// Variables
+
+	//file.get("variable", data);
+	//file.get("name", data2);
+
+	//file.def("name", atts, {"time", "y", "x"});
+	//file.def_and_put("name", { "y" }, atts, data2);
+	//file.put("name", data);
+	//file.put("time", 52, 10);
+	//file.stack("time", 52);
+
+
+	file.close();
+	return 0;
+}
