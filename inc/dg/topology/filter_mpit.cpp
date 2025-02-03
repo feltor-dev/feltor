c86d599f39affdafff06a1218387d5e88d0a0a8a
Matthias <mattwi@fysik.dtu.dk>
2025-02-03 10:33:01 +0100

    Add filter_mpit

--- inc/dg/topology/filter_mpit.cpp
+++ inc/dg/topology/filter_mpit.cpp
@@ -10,92 +10,69 @@
 #include "mpi_weights.h"
 #include "filter.h"
 
-double function( double x, double y){return sin(x)*sin(y);}
-double function( double x, double y, double z){return sin(x)*sin(y)*sin(z);}
+#include "catch2/catch.hpp"
 
-unsigned n = 3, Nx = 8, Ny = 10, Nz = 6;
+static double function( double x, double y){return sin(x)*sin(y);}
+static double function( double x, double y, double z){return sin(x)*sin(y)*sin(z);}
 
-int main(int argc, char* argv[])
+
+TEST_CASE( "mpi filter")
 {
-    MPI_Init( &argc, &argv);
+    const unsigned Nx = 8, Ny = 10, Nz = 6;
     int rank, size;
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
     MPI_Comm_size( MPI_COMM_WORLD, &size);
-    if(size!=4)
+
+    SECTION( "Test 2d exponential filter")
     {
-        std::cerr << "Please run with 4 processes!\n";
-        MPI_Finalize();
-        return 0;
-    }
-    MPI_Comm comm;
-    std::stringstream ss;
-    ss<< "2 2 " << n << " "<<Nx<<" "<<Ny;
-    mpi_init2d( dg::PER, dg::PER, n, Nx, Ny, comm, ss);
-    MPI_Comm_rank( comm, &rank);
+        MPI_Comm comm = dg::mpi_cart_create( MPI_COMM_WORLD, {0,0}, {1,1});
+        dg::MPIGrid2d g3( -M_PI, 0, -5*M_PI, -4*M_PI, 3, Nx, Ny, comm);
+        dg::MPIGrid2d g2( -M_PI, 0, -5*M_PI, -4*M_PI, 2, Nx, Ny, comm);
 
+        const dg::MDVec vec = dg::evaluate( function, g3);
+        const dg::MDVec weights = dg::create::weights( g3);
+        dg::MDVec filtered_vec(vec), projected_vec(dg::evaluate( dg::zero, g2)), inter_vec( vec);
+        auto op = dg::ExponentialFilter( 36, 0.5, 8, g3.nx());
+        dg::MultiMatrix<dg::MDMatrix, dg::MDVec> filter = dg::create::fast_transform(
+           dg::create::modal_filter( op, g3.nx()),
+           dg::create::modal_filter( op, g3.ny()), g3);
+        dg::MIDMatrix project = dg::create::projection( g2,g3);
+        dg::MIDMatrix interpo = dg::create::interpolation( g3,g2);
+
+        dg::blas2::symv( project, vec, projected_vec);
+        dg::blas2::symv( interpo, projected_vec, inter_vec);
+        dg::blas2::symv( filter, vec, filtered_vec);
+        dg::blas1::axpby( 1., filtered_vec, -1., inter_vec);
+        double error = sqrt(dg::blas2::dot( inter_vec, weights, inter_vec)/ dg::blas2::dot( vec, weights, vec));
+        INFO( "Error by filtering: "<<error);
+        CHECK( error < 1e-14);
+    }
+    //MPI_Comm_free(&comm); // TODO Fix Segmentation fault without this line for unknown reasons!?
+    SECTION( "Test 3d exponential filter")
     {
-    if(rank==0)std::cout << "Test 2d exponential filter: \n";
-    dg::MPIGrid2d g3( -M_PI, 0, -5*M_PI, -4*M_PI, 3, Nx, Ny, comm);
-    dg::MPIGrid2d g2( -M_PI, 0, -5*M_PI, -4*M_PI, 2, Nx, Ny, comm);
+        MPI_Comm comm = dg::mpi_cart_create( MPI_COMM_WORLD, {0,0,0}, {1,1,1});
+        dg::MPIGrid3d g3( -M_PI, 0, -5*M_PI, -4*M_PI, 0., 2.*M_PI, 3, Nx, Ny,
+        Nz, comm);
+        dg::MPIGrid3d g2( -M_PI, 0, -5*M_PI, -4*M_PI, 0., 2.*M_PI, 2, Nx, Ny,
+        Nz, comm);
 
-    const dg::MDVec vec = dg::evaluate( function, g3);
-    const dg::MDVec weights = dg::create::weights( g3);
-    dg::MDVec filtered_vec(vec), projected_vec(dg::evaluate( dg::zero, g2)), inter_vec( vec);
-    auto op = dg::ExponentialFilter( 36, 0.5, 8, g3.nx());
-    dg::MultiMatrix<dg::MDMatrix, dg::MDVec> filter = dg::create::fast_transform(
-       dg::create::modal_filter( op, g3.nx()),
-       dg::create::modal_filter( op, g3.ny()), g3);
-    dg::MIDMatrix project = dg::create::projection( g2,g3);
-    dg::MIDMatrix interpo = dg::create::interpolation( g3,g2);
+        const dg::MDVec vec = dg::evaluate( function, g3);
+        const dg::MDVec weights = dg::create::weights( g3);
+        dg::MDVec filtered_vec(vec), projected_vec(dg::evaluate( dg::zero, g2)), inter_vec( vec);
+        auto op = dg::ExponentialFilter( 36, 0.5, 8, g3.nx());
+        dg::MultiMatrix<dg::MDMatrix, dg::MDVec> filter = dg::create::fast_transform(
+           dg::create::modal_filter( op, g3.nx()),
+           dg::create::modal_filter( op, g3.ny()), g3);
+        dg::MIDMatrix project = dg::create::projection( g2,g3);
+        dg::MIDMatrix interpo = dg::create::interpolation( g3,g2);
 
-    dg::blas2::symv( project, vec, projected_vec);
-    dg::blas2::symv( interpo, projected_vec, inter_vec);
-    dg::blas2::symv( filter, vec, filtered_vec);
-    dg::blas1::axpby( 1., filtered_vec, -1., inter_vec);
-    double error = sqrt(dg::blas2::dot( inter_vec, weights, inter_vec)/ dg::blas2::dot( vec, weights, vec));
-    if(rank==0)std::cout << "Error by filtering: "<<error<<std::endl;
-
-    if(rank==0){
-    if( error > 1e-14)
-        std::cout << "2D TEST FAILED!\n";
-    else
-        std::cout << "2D TEST PASSED!\n";
+        dg::blas2::symv( project, vec, projected_vec);
+        dg::blas2::symv( interpo, projected_vec, inter_vec);
+        dg::blas2::symv( filter, vec, filtered_vec);
+        dg::blas1::axpby( 1., filtered_vec, -1., inter_vec);
+        double error = sqrt(dg::blas2::dot( inter_vec, weights, inter_vec)/
+            dg::blas2::dot( vec, weights, vec));
+        INFO( "Error by filtering: "<<error);
+        CHECK( error < 1e-14);
     }
-    }
-    MPI_Comm_free(&comm); // Segmentation fault without this line for unknown reasons!?
-    {
-    MPI_Comm comm;
-    std::stringstream ss;
-    ss<< "2 2 1 " << n << " "<<Nx<<" "<<Ny<<" "<<Nz;
-    mpi_init3d( dg::PER, dg::PER, dg::PER, n, Nx, Ny, Nz, comm, ss);
-    MPI_Comm_rank( comm, &rank);
-    if(rank==0)std::cout << "Test 3d exponential filter: \n";
-    dg::MPIGrid3d g3( -M_PI, 0, -5*M_PI, -4*M_PI, 0., 2.*M_PI, 3, Nx, Ny, Nz, comm);
-    dg::MPIGrid3d g2( -M_PI, 0, -5*M_PI, -4*M_PI, 0., 2.*M_PI, 2, Nx, Ny, Nz, comm);
-
-    const dg::MDVec vec = dg::evaluate( function, g3);
-    const dg::MDVec weights = dg::create::weights( g3);
-    dg::MDVec filtered_vec(vec), projected_vec(dg::evaluate( dg::zero, g2)), inter_vec( vec);
-    auto op = dg::ExponentialFilter( 36, 0.5, 8, g3.nx());
-    dg::MultiMatrix<dg::MDMatrix, dg::MDVec> filter = dg::create::fast_transform(
-       dg::create::modal_filter( op, g3.nx()),
-       dg::create::modal_filter( op, g3.ny()), g3);
-    dg::MIDMatrix project = dg::create::projection( g2,g3);
-    dg::MIDMatrix interpo = dg::create::interpolation( g3,g2);
-
-    dg::blas2::symv( project, vec, projected_vec);
-    dg::blas2::symv( interpo, projected_vec, inter_vec);
-    dg::blas2::symv( filter, vec, filtered_vec);
-    dg::blas1::axpby( 1., filtered_vec, -1., inter_vec);
-    double error = sqrt(dg::blas2::dot( inter_vec, weights, inter_vec)/ dg::blas2::dot( vec, weights, vec));
-    if(rank==0)std::cout << "Error by filtering: "<<error<<std::endl;
-
-    if(rank==0){
-    if( error > 1e-14)
-        std::cout << "3D TEST FAILED!\n";
-    else
-        std::cout << "3D TEST PASSED!\n";
-    }
-    }
-    MPI_Finalize();
-    return 0;
 }
--- inc/dg/topology/mpi_grid_mpit.cpp
+++ inc/dg/topology/mpi_grid_mpit.cpp
@@ -5,108 +5,79 @@
 #include "mpi_grid.h"
 #include "mpi_evaluation.h"
 
-int function( double x, double y)
+#include "catch2/catch.hpp"
+
+static int function( double x, double y)
 {
     return int(10000*x*y);
 }
-int main(int argc, char* argv[])
+TEST_CASE( "MPI Grid")
 {
-    MPI_Init( &argc, &argv);
     int rank, size;
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
     MPI_Comm_size( MPI_COMM_WORLD, &size);
     unsigned nx = 2, ny = 3,  Nx = 8, Ny = 10;
-    int dims[2] = {0,0};
-    MPI_Dims_create( size, 2, dims);
-    MPI_Comm comm;
-    std::stringstream ss;
-    ss<< dims[0]<<" "<<dims[1];
-    dg::mpi_init2d( dg::PER, dg::PER, comm, ss);
-    MPI_Comm_rank( comm, &rank);
+    MPI_Comm comm = dg::mpi_cart_create( MPI_COMM_WORLD, {0,0}, {1,1});
     ///%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%//
-    if(rank==0) std::cout << "Test mpi grid methods!\n";
-    if(rank==0) std::cout << "Test local2globalIdx\n";
-
-    // TODO Check that all processes agree on the index
     // Assume that evaluate works
     dg::MPIGrid2d g2d( {0.,0.}, {1.,1.}, {nx,ny}, {Nx,Ny}, {dg::PER, dg::PER},
             dg::mpi_cart_split_as<2>(comm));
     auto local_vec = dg::evaluate( function, g2d);
     auto global_vec = dg::evaluate( function, g2d.global());
     auto local_vec2 = dg::global2local( global_vec, g2d);
-
-
-    int local_size  = g2d.local_size();
-    bool success = true;
-    for( int i=0; i<local_size; i++)
+    SECTION( "local2globalIdx")
     {
-        if( local_vec.data()[i] != local_vec2.data()[i])
-            success = false;
+        int local_size  = g2d.local_size();
+        for( int i=0; i<local_size; i++)
+            CHECK( local_vec.data()[i] == local_vec2.data()[i]);
     }
-    if( !success)
-        std::cout << "FAILED from rank "<<rank<<"!\n";
-    else
-        std::cout << "SUCCESS from rank "<<rank<<"!\n";
-    if(rank==0)std::cout << std::endl;
-    MPI_Barrier(comm);
 
-    if(rank==0) std::cout << "Test global2localIdx"<<std::endl;
-
-    success = true;
-    int global_size  = g2d.size();
-    for ( int i=0; i<global_size; i++)
+    SECTION("global2localIdx")
     {
-        int lIdx = 0, pid = 0;
-        g2d.global2localIdx( i, lIdx, pid);
-        if( pid == rank)
-            if( global_vec[i] != local_vec.data()[lIdx] or lIdx >= (int)local_vec.data().size())
-                success = false;
-    }
-    if( !success)
-        std::cout << "FAILED from rank "<<rank<<"!\n";
-    else
-        std::cout << "SUCCESS from rank "<<rank<<"!\n";
-    MPI_Barrier(comm);
-
-    if( rank==0) std::cout << "Now test that all processes agree on indices\n";
-    bool success_g2l = true, success_l2g = true;
-    for ( int i=0; i<global_size; i++)
-    {
-        int lIdx = 0, pid = 0;
-        g2d.global2localIdx( i, lIdx, pid);
-        int locals[size], localp[size];
-        // Send everything to root
-        MPI_Gather( &lIdx, 1, MPI_INT, locals, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Gather( &pid, 1, MPI_INT, localp, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        if(rank==0)
+        int global_size = g2d.size();
+        for ( int i=0; i<global_size; i++)
         {
-            for( int k=0; k<size; k++)
-                if( locals[k] != lIdx)
-                    success_g2l = false;
-            for( int k=0; k<size; k++)
-                if( localp[k] != pid)
-                    success_g2l = false;
-        }
-        int gIdx = 0;
-        g2d.local2globalIdx( lIdx, pid, gIdx);
-        if( gIdx != i)
-        {
-            if(rank==0)std::cerr << pid <<" lIdx "<<lIdx<< " gIdx "<<gIdx<<" "<<i<<"\n";
-            success_l2g = false;
+            int lIdx = 0, pid = 0;
+            g2d.global2localIdx( i, lIdx, pid);
+            if( pid == rank)
+            {
+                CHECK( global_vec[i] == local_vec.data()[lIdx]);
+                CHECK( lIdx < (int)local_vec.data().size());
+            }
         }
     }
-    std::cout << (success_g2l ? "SUCCESS" : "FAILED")
-              << " g2l from rank "<<rank<<"!\n";
-    std::cout << (success_l2g ? "SUCCESS" : "FAILED")
-              << " l2g from rank "<<rank<<"!\n";
 
-    // Test start and count
-    int gIdx;
-    g2d.local2globalIdx( 0, rank, gIdx);
-    auto start = g2d.start();
-    int sIdx = start[1]*g2d.shape(0) + start[0];
-    std::cout << (sIdx == gIdx ? "SUCCESS" : "FAILED")
-              << " start from rank "<<rank<<"!\n";
-
-    MPI_Finalize();
-    return 0;
+    SECTION( "Processes agree on indices")
+    {
+        int global_size = g2d.size();
+        for ( int i=0; i<global_size; i++)
+        {
+            int lIdx = 0, pid = 0;
+            g2d.global2localIdx( i, lIdx, pid);
+            int locals[size], localp[size];
+            // Send everything to root
+            MPI_Gather( &lIdx, 1, MPI_INT, locals, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Gather(  &pid, 1, MPI_INT, localp, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            if(rank==0)
+            {
+                for( int k=0; k<size; k++)
+                    CHECK( locals[k] == lIdx);
+                for( int k=0; k<size; k++)
+                    CHECK( localp[k] == pid);
+            }
+            int gIdx = 0;
+            g2d.local2globalIdx( lIdx, pid, gIdx);
+            INFO("Rank "<<rank<<" pid "<<pid <<" lIdx "<<lIdx<< " gIdx "<<gIdx<<" "<<i);
+            CHECK( gIdx == i);
+        }
+    }
+    SECTION( "Start and count")
+    {
+        int gIdx;
+        g2d.local2globalIdx( 0, rank, gIdx);
+        auto start = g2d.start();
+        int sIdx = start[0]*g2d.shape(0) + start[1];
+        INFO( "Rank "<<rank<<" sIdx "<<sIdx<<" gIdx "<<gIdx);
+        CHECK( sIdx == gIdx);
+    }
 }
