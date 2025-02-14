962c030c201429ca217739735662e9343c06b39a
Liam Pattinson <liampattinson@gmail.com>
2025-02-14 11:33:20 +0000

    Improve CMake system for dg library
    - Moved functions to cmake dir
    - Added custom compile flags
    - Simplified header inclusion
    - Benchmarks and tests built by default
    - Added usage guide

---
+++ cmake/FeltorBenchmarks.cmake
@@ -0,0 +1,18 @@
+function(add_dg_benchmark benchmark_path)
+  # Get a unique name for the benchmark
+  cmake_path(GET benchmark_path STEM benchmark_stem)
+  set(benchmark_name "dg_${benchmark_stem}")
+  # Optionally treat .cu files as .cpp files
+  if(NOT FELTOR_USE_GPU)
+      set_source_files_properties(${benchmark_path} PROPERTIES LANGUAGE CXX)
+  endif()
+  # Create the benchmark executable and link dependencies
+  add_executable(${benchmark_name} "${benchmark_path}")
+  target_link_libraries(${benchmark_name} PRIVATE feltor::dg)
+  # Add as dependency to the dg_benchmarks target
+  add_dependencies(dg_benchmarks ${benchmark_name})
+  # Ensure built benchmarks are built in their own directory
+  set_target_properties(${benchmark_name} PROPERTIES 
+      RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/benchmarks"
+  )
+endfunction()
