5b45eee6775c50e016a62acccfc13aaad81eac38
Matthias <mattwi@fysik.dtu.dk>
2025-02-20 14:15:18 +0100

    Merge elliptic_b and elliptic_mpib

--- inc/dg/elliptic_b.cpp
+++ inc/dg/elliptic_b.cpp
@@ -3,7 +3,11 @@
 
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
-#include <cusp/print.h>
+
+#ifdef WITH_MPI
+#include <mpi.h>
+#include "backend/mpi_init.h"
+#endif
 
 #include "backend/timer.h"
 #include "topology/evaluation.h"
@@ -34,102 +38,135 @@ double variation3d( double x, double y, double z) {
 }
 
 
-int main()
+int main( int argc, char* argv[])
 {
-    dg::Timer t;
     unsigned n, Nx, Ny, Nz;
+    double eps=1e-6;
+#ifdef WITH_MPI
+    MPI_Init( &argc, &argv);
+    int rank;
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
+
+    MPI_Comm comm;
+    dg::mpi_init3d( bcx, bcy, bcz, n, Nx, Ny, Nz, comm);
+    if(rank==0)std::cout << "Type epsilon! \n";
+    if(rank==0)std::cin >> eps;
+    MPI_Bcast(  &eps,1 , MPI_DOUBLE, 0, comm);
+#else
     std::cout << "Type n, Nx, Ny and Nz\n";
     std::cin >> n >> Nx >> Ny >> Nz;
-    double eps;
     std::cout << "Type epsilon! \n";
     std::cin >> eps;
-    bool jump_weight;
-    std::cout << "Jump weighting on or off? Type 1 for true or 0 for false (default): \n";
-    std::cin >> jump_weight;
-    std::cout << "TEST CYLINDRICAL LAPLACIAN\n";
+    //bool jump_weight;
+    //std::cout << "Jump weighting on or off? Type 1 for true or 0 for false (default): \n";
+    //std::cin >> jump_weight;
+#endif
+
+    dg::Timer t;
+    {
+    DG_RANK0 std::cout << "TEST CYLINDRICAL LAPLACIAN\n";
     //std::cout << "Create Laplacian\n";
     //! [invert]
-    dg::CylindricalGrid3d grid( R_0, R_0+lx, 0, ly, 0,lz, n, Nx, Ny,Nz, bcx, bcy, bcz);
-    dg::DVec w3d = dg::create::volume( grid);
-    dg::DVec x = dg::evaluate( initial, grid);
+    dg::x::CylindricalGrid3d grid( R_0, R_0+lx, 0, ly, 0,lz, n, Nx, Ny,Nz, bcx, bcy, bcz
+#ifdef WITH_MPI
+    , comm
+#endif
+    );
+    const dg::x::DVec w3d = dg::create::volume( grid);
+    dg::x::DVec x = dg::evaluate( initial, grid);
 
-    dg::Elliptic3d<dg::aGeometry3d, dg::DMatrix, dg::DVec> laplace(grid, dg::centered);
+    dg::Elliptic3d<dg::x::aGeometry3d, dg::x::DMatrix, dg::x::DVec>
+        laplace( grid, dg::centered);
 
-    laplace.set_jump_weighting(jump_weight);
+    dg::PCG pcg( x, n*n*Nx*Ny*Nz);
 
-    dg::PCG< dg::DVec > pcg( x, n*n*Nx*Ny*Nz);
+    const dg::x::DVec solution = dg::evaluate ( fct, grid);
+    const dg::x::DVec b = dg::evaluate ( laplace3d_fct, grid);
 
-    const dg::DVec solution = dg::evaluate ( fct, grid);
-    dg::DVec b = dg::evaluate ( laplace3d_fct, grid);
-
-    std::cout << "For a precision of "<< eps<<" ..."<<std::endl;
-    unsigned num;
+    DG_RANK0 std::cout << "For a precision of "<< eps<<" ..."<<std::endl;
     t.tic();
-    num = pcg.solve( laplace, x, b, 1., w3d, eps);
+    unsigned num = pcg.solve( laplace, x, b, 1., w3d, eps);
     t.toc();
-    std::cout << "Number of pcg iterations "<<num<<std::endl;
-    std::cout << "... on the device took   "<< t.diff()<<"s\n";
+    DG_RANK0 std::cout << "Number of pcg iterations "<<num<<std::endl;
+    DG_RANK0 std::cout << "... on the device took   "<< t.diff()<<"s\n";
     //! [invert]
-    dg::DVec  error(  solution);
+    dg::x::DVec  error(  solution);
     dg::blas1::axpby( 1., x,-1., error);
 
     double normerr = dg::blas2::dot( w3d, error);
     double norm = dg::blas2::dot( w3d, solution);
     dg::exblas::udouble res;
     norm = sqrt(normerr/norm); res.d = norm;
-    std::cout << "L2 Norm of relative error is:               " <<res.d<<"\t"<<res.i<<std::endl;
-    const dg::DVec deriv = dg::evaluate( fctX, grid);
-    dg::DMatrix DX = dg::create::dx( grid);
+    DG_RANK0 std::cout << "L2 Norm of relative error is:               " <<res.d<<"\t"<<res.i<<std::endl;
+    const dg::x::DVec deriv = dg::evaluate( fctX, grid);
+    dg::x::DMatrix DX = dg::create::dx( grid);
     dg::blas2::gemv( DX, x, error);
     dg::blas1::axpby( 1., deriv, -1., error);
     normerr = dg::blas2::dot( w3d, error);
     norm = dg::blas2::dot( w3d, deriv);
-    std::cout << "L2 Norm of relative error in derivative is: " <<sqrt( normerr/norm)<<std::endl;
-    std::cout << "Compute variation in Elliptic               ";
-    const dg::DVec variatio = dg::evaluate ( variation3d, grid);
+    DG_RANK0 std::cout << "L2 Norm of relative error in derivative is: " <<sqrt( normerr/norm)<<std::endl;
+    DG_RANK0 std::cout << "Compute variation in Elliptic               ";
+    const dg::x::DVec variatio = dg::evaluate ( variation3d, grid);
     laplace.variation( solution, x, error);
     dg::blas1::axpby( 1., variatio, -1., error);
     norm = dg::blas2::dot( w3d, variatio);
     normerr = dg::blas2::dot( w3d, error);
-    std::cout <<sqrt( normerr/norm) << "\n";
+    DG_RANK0 std::cout <<sqrt( normerr/norm) << "\n";
+    }
 
 
-    std::cout << "TEST SPLIT SOLUTION\n";
-    x = dg::evaluate( initial, grid);
-    b = dg::evaluate ( laplace2d_fct, grid);
+    { // Split solution
+    DG_RANK0 std::cout << "TEST SPLIT SOLUTION\n";
+    dg::x::CylindricalGrid3d grid( R_0, R_0+lx, 0, ly, 0,lz, n, Nx, Ny,Nz, bcx, bcy, bcz
+#ifdef WITH_MPI
+    , comm
+#endif
+    );
+    const dg::x::DVec w3d = dg::create::volume( grid);
+    dg::x::DVec x = dg::evaluate( initial, grid);
+    dg::x::DVec b = dg::evaluate ( laplace2d_fct, grid);
     //create grid and perp and parallel volume
-    dg::ClonePtr<dg::aGeometry2d> grid_perp = grid.perp_grid();
-    dg::DVec w2d = dg::create::volume( *grid_perp);
-    dg::DVec g_parallel = grid.metric().value(2,2);
+    dg::ClonePtr<dg::x::aGeometry2d> grid_perp = grid.perp_grid();
+    const dg::x::DVec w2d = dg::create::volume( *grid_perp);
+    dg::x::DVec g_parallel = grid.metric().value(2,2);
     dg::blas1::transform( g_parallel, g_parallel, dg::SQRT<>());
-    dg::DVec chi = dg::evaluate( dg::one, grid);
+    dg::x::DVec chi = dg::evaluate( dg::one, grid);
     dg::blas1::pointwiseDivide( chi, g_parallel, chi);
     //create split Laplacian
-    std::vector< dg::Elliptic<dg::aGeometry2d, dg::DMatrix, dg::DVec> > laplace_split(
-            grid.Nz(), {*grid_perp, dg::centered});
+    std::vector< dg::Elliptic<dg::x::aGeometry2d, dg::x::DMatrix, dg::x::DVec> > laplace_split(
+#ifdef WITH_MPI
+            grid.local().Nz(),
+#else
+            grid.Nz(),
+#endif
+            {*grid_perp, dg::centered});
     // create split  vectors and solve
-    std::vector<dg::View<dg::DVec>> b_split, x_split, chi_split;
-    pcg.construct( w2d, w2d.size());
+    dg::PCG pcg( w2d, grid_perp->size());
     std::vector<unsigned>  number(grid.Nz());
     t.tic();
     dg::blas1::pointwiseDivide( b, g_parallel, b);
-    b_split = dg::split( b, grid);
-    chi_split = dg::split( chi, grid);
-    x_split = dg::split( x, grid);
-    for( unsigned i=0; i<grid.Nz(); i++)
+    auto b_split   = dg::split( b, grid);
+    auto chi_split = dg::split( chi, grid);
+    auto x_split   = dg::split( x, grid);
+    for( unsigned i=0; i<laplace_split.size(); i++)
     {
         laplace_split[i].set_chi( chi_split[i]);
         number[i] = pcg.solve( laplace_split[i], x_split[i], b_split[i], 1., w2d, eps);
     }
     t.toc();
-    std::cout << "Number of iterations in split     "<< number[0]<<"\n";
-    std::cout << "Split solution on the device took "<< t.diff()<<"s\n";
-    dg::blas1::axpby( 1., x,-1., solution, error);
-    normerr = dg::blas2::dot( w3d, error);
-    norm = dg::blas2::dot( w3d, solution);
-    std::cout << "L2 Norm of relative error is:     " <<sqrt( normerr/norm)<<std::endl;
+    DG_RANK0 std::cout << "Number of iterations in split     "<< number[0]<<"\n";
+    DG_RANK0 std::cout << "Split solution on the device took "<< t.diff()<<"s\n";
+    const dg::x::DVec solution = dg::evaluate ( fct, grid);
+    dg::blas1::axpby( 1., x,-1., solution, x);
+    double normerr = dg::blas2::dot( w3d, x);
+    double norm = dg::blas2::dot( w3d, solution);
+    DG_RANK0 std::cout << "L2 Norm of relative error is:     " <<sqrt( normerr/norm)<<std::endl;
+    }
 
     //both function and derivative converge with order P
+#ifdef WITH_MPI
+    MPI_Finalize();
+#endif
 
     return 0;
 }
