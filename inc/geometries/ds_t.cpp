1e9fe35ffaf4e6c40bf82e2bb2ed729a24664eea
Matthias <mattwi@fysik.dtu.dk>
2025-03-03 10:34:26 +0100

    Merge ds_t and ds_mpit

--- inc/geometries/ds_t.cpp
+++ inc/geometries/ds_t.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <iomanip>
 
+#ifdef WITH_MPI
+#include <mpi.h>
+#endif
+
 #include "dg/algorithm.h"
 #include "magnetic_field.h"
 #include "testfunctors.h"
@@ -13,10 +17,42 @@ const double a  = 1; //small radius
 
 int main(int argc, char * argv[])
 {
-    std::cout << "# Test the parallel derivative DS in cylindrical coordinates for circular flux surfaces with DIR and NEU boundary conditions.\n";
-    std::cout << "# Type n (3), Nx(20), Ny(20), Nz(20)\n";
+#ifdef WITH_MPI
+    MPI_Init( &argc, &argv);
+    int rank;
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
+#endif
+    DG_RANK0 std::cout << "# Test the parallel derivative DS in cylindrical coordinates for circular flux surfaces with DIR and NEU boundary conditions.\n";
     unsigned n, Nx, Ny, Nz, mx[2], max_iter = 1e4;
     std::string method = "cubic";
+
+#ifdef WITH_MPI
+    MPI_Comm comm;
+    dg::mpi_init3d( dg::NEU, dg::NEU, dg::PER, n, Nx, Ny, Nz, comm);
+    unsigned letters = 0;
+    if( rank == 0)
+    {
+        std::cout <<"# You typed\n"
+                  <<"n:  "<<n<<"\n"
+                  <<"Nx: "<<Nx<<"\n"
+                  <<"Ny: "<<Ny<<"\n"
+                  <<"Nz: "<<Nz<<std::endl;
+        std::cout <<"# Type mx (10) and my (10)\n";
+        std::cin >> mx[0] >> mx[1];
+        std::cout << "# You typed\n"
+                  <<"mx: "<<mx[0]<<"\n"
+                  <<"my: "<<mx[1]<<std::endl;
+        std::cout << "# Type method (dg, nearest, linear, cubic) \n";
+        std::cin >> method;
+        method.erase( std::remove( method.begin(), method.end(), '"'), method.end());
+        letters = method.size();
+    }
+    MPI_Bcast( mx, 2, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast( &letters, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    method.resize( letters);
+    MPI_Bcast( &method[0], letters, MPI_CHAR, 0, MPI_COMM_WORLD);
+#else
+    std::cout << "# Type n (3), Nx(20), Ny(20), Nz(20)\n";
     std::cin >> n>> Nx>>Ny>>Nz;
     std::cout <<"# You typed\n"
               <<"n:  "<<n<<"\n"
@@ -31,33 +67,38 @@ int main(int argc, char * argv[])
     std::cout << "# Type method (dg, nearest, linear, cubic) \n";
     std::cin >> method;
     method.erase( std::remove( method.begin(), method.end(), '"'), method.end());
+#endif
     std::cout << "# You typed\n"
               <<"method: "<< method<<std::endl;
-    std::cout << "# Create parallel Derivative!\n";
+    DG_RANK0 std::cout << "# Create parallel Derivative!\n";
 
     //![doxygen]
-    const dg::CylindricalGrid3d g3d( R_0-a, R_0+a, -a, a, 0, 2.*M_PI, n, Nx, Ny, Nz, dg::NEU, dg::NEU, dg::PER);
+    const dg::x::CylindricalGrid3d g3d( R_0-a, R_0+a, -a, a, 0, 2.*M_PI, n, Nx, Ny, Nz, dg::NEU, dg::NEU, dg::PER
+#ifdef WITH_MPI
+    , comm
+#endif
+    );
     //create magnetic field
     const dg::geo::TokamakMagneticField mag = dg::geo::createCircularField( R_0, I_0);
     auto bhat = dg::geo::createBHat(mag);
     //create Fieldaligned object and construct DS from it
-    dg::geo::Fieldaligned<dg::aProductGeometry3d,dg::IDMatrix,dg::DVec>  dsFA(
+    dg::geo::Fieldaligned<dg::x::aProductGeometry3d,dg::x::IDMatrix,dg::x::DVec>  dsFA(
             bhat, g3d, dg::NEU, dg::NEU, dg::geo::NoLimiter(), 1e-8, mx[0], mx[1],
             -1, method);
-    dg::geo::DS<dg::aProductGeometry3d, dg::IDMatrix, dg::DVec>
+    dg::geo::DS<dg::x::aProductGeometry3d, dg::x::IDMatrix, dg::x::DVec>
         ds( dsFA );
     //![doxygen]
     ///##########################################################///
     auto ff = dg::geo::TestFunctionDirNeu(mag);
-    const dg::DVec fun = dg::pullback( ff, g3d);
-    dg::DVec derivative(fun);
-    const dg::DVec divb = dg::pullback( dg::geo::Divb(mag), g3d);
-    const dg::DVec sol0 = dg::pullback( dg::geo::DsFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
-    const dg::DVec sol1 = dg::pullback( dg::geo::DssFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
-    const dg::DVec sol2 = dg::pullback( dg::geo::DsDivFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
-    const dg::DVec sol3 = dg::pullback( dg::geo::DsDivDsFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
-    const dg::DVec sol4 = dg::pullback( dg::geo::OMDsDivDsFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
-    std::vector<std::pair<std::string, std::array<const dg::DVec*,2>>> names{
+    const dg::x::DVec fun = dg::pullback( ff, g3d);
+    dg::x::DVec derivative(fun);
+    const dg::x::DVec divb = dg::pullback( dg::geo::Divb(mag), g3d);
+    const dg::x::DVec sol0 = dg::pullback( dg::geo::DsFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
+    const dg::x::DVec sol1 = dg::pullback( dg::geo::DssFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
+    const dg::x::DVec sol2 = dg::pullback( dg::geo::DsDivFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
+    const dg::x::DVec sol3 = dg::pullback( dg::geo::DsDivDsFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
+    const dg::x::DVec sol4 = dg::pullback( dg::geo::OMDsDivDsFunction<dg::geo::TestFunctionDirNeu>(mag,ff), g3d);
+    std::vector<std::pair<std::string, std::array<const dg::x::DVec*,2>>> names{
          {"forward",{&fun,&sol0}},          {"backward",{&fun,&sol0}},
          {"forward2",{&fun,&sol0}},         {"backward2",{&fun,&sol0}},
          {"centered",{&fun,&sol0}},         {"centered_bc_along",{&fun,&sol0}},
@@ -66,16 +107,16 @@ int main(int argc, char * argv[])
          {"divCentered",{&fun,&sol2}},      {"directLap",{&fun,&sol3}},
          {"directLap_bc_along",{&fun,&sol3}}, {"invCenteredLap",{&sol4,&fun}}
     };
-    std::cout << "# TEST NEU Boundary conditions!\n";
-    std::cout << "# TEST ADJOINT derivatives do unfortunately not fulfill Neumann BC!\n";
+    DG_RANK0 std::cout << "# TEST NEU Boundary conditions!\n";
+    DG_RANK0 std::cout << "# TEST ADJOINT derivatives do unfortunately not fulfill Neumann BC!\n";
     ///##########################################################///
-    std::cout <<"Neumann:\n";
-    dg::DVec vol3d = dg::create::volume( g3d);
+    DG_RANK0 std::cout <<"Neumann:\n";
+    dg::x::DVec vol3d = dg::create::volume( g3d);
     for( const auto& tuple :  names)
     {
         std::string name = std::get<0>(tuple);
-        const dg::DVec& function = *std::get<1>(tuple)[0];
-        const dg::DVec& solution = *std::get<1>(tuple)[1];
+        const dg::x::DVec& function = *std::get<1>(tuple)[0];
+        const dg::x::DVec& solution = *std::get<1>(tuple)[1];
         if( name.find("inv") != std::string::npos ||
                 name.find( "div") != std::string::npos)
             callDS( ds, name, function, derivative, max_iter,1e-8);
@@ -88,21 +129,21 @@ int main(int argc, char * argv[])
         double sol = dg::blas2::dot( vol3d, solution);
         dg::blas1::axpby( 1., solution, -1., derivative);
         double norm = dg::blas2::dot( derivative, vol3d, derivative);
-        std::cout <<"    "<<name<<":" <<std::setw(18-name.size())
+        DG_RANK0 std::cout <<"    "<<name<<":" <<std::setw(18-name.size())
                   <<" "<<sqrt(norm/sol)<<"\n";
     }
     ///##########################################################///
-    std::cout << "# Reconstruct parallel derivative!\n";
+    DG_RANK0 std::cout << "# Reconstruct parallel derivative!\n";
     dsFA.construct( bhat, g3d, dg::DIR, dg::DIR, dg::geo::NoLimiter(), 1e-8, mx[0], mx[1], -1, method);
     ds.construct( dsFA);
-    std::cout << "# TEST DIR Boundary conditions!\n";
+    DG_RANK0 std::cout << "# TEST DIR Boundary conditions!\n";
     ///##########################################################///
-    std::cout << "Dirichlet: \n";
+    DG_RANK0 std::cout << "Dirichlet: \n";
     for( const auto& tuple :  names)
     {
         std::string name = std::get<0>(tuple);
-        const dg::DVec& function = *std::get<1>(tuple)[0];
-        const dg::DVec& solution = *std::get<1>(tuple)[1];
+        const dg::x::DVec& function = *std::get<1>(tuple)[0];
+        const dg::x::DVec& solution = *std::get<1>(tuple)[1];
         if( name.find("inv") != std::string::npos ||
                 name.find( "div") != std::string::npos)
             callDS( ds, name, function, derivative, max_iter,1e-8);
@@ -116,50 +157,50 @@ int main(int argc, char * argv[])
         double sol = dg::blas2::dot( vol3d, solution);
         dg::blas1::axpby( 1., solution, -1., derivative);
         double norm = dg::blas2::dot( derivative, vol3d, derivative);
-        std::cout <<"    "<<name<<":" <<std::setw(18-name.size())
+        DG_RANK0 std::cout <<"    "<<name<<":" <<std::setw(18-name.size())
                   <<" "<<sqrt(norm/sol)<<"\n";
     }
 
     ///##########################################################///
-    std::cout << "# TEST FIELDALIGNED EVALUATION of a Gaussian\n";
+    DG_RANK0 std::cout << "# TEST FIELDALIGNED EVALUATION of a Gaussian\n";
     dg::Gaussian init0(R_0+0.5, 0, 0.2, 0.2, 1);
     dg::GaussianZ modulate(0., M_PI/3., 1);
     dg::Timer t;
     t.tic();
-    dg::DVec aligned = dg::geo::fieldaligned_evaluate( g3d, bhat, init0, modulate, Nz/2, 2);
+    dg::x::DVec aligned = dg::geo::fieldaligned_evaluate( g3d, bhat, init0, modulate, Nz/2, 2);
     t.toc();
-    std::cout << "# took "<<t.diff()<<"s\n";
+    DG_RANK0 std::cout << "# took "<<t.diff()<<"s\n";
     ds.ds( dg::centered, aligned, derivative);
     double norm = dg::blas2::dot(vol3d, derivative);
-    std::cout << "# Norm Centered Derivative "<<sqrt( norm)<<" (compare with that of ds_mpit)\n";
+    DG_RANK0 std::cout << "# Norm Centered Derivative "<<sqrt( norm)<<" (compare with that of ds_mpit)\n";
     t.tic();
     aligned = dsFA.evaluate( init0, modulate, Nz/2, 2);
     t.toc();
-    std::cout << "# took "<<t.diff()<<"s\n";
+    DG_RANK0 std::cout << "# took "<<t.diff()<<"s\n";
     ds.ds( dg::centered, aligned, derivative);
     norm = dg::blas2::dot(vol3d, derivative);
-    std::cout << "# Norm Centered Derivative "<<sqrt( norm)<<" (compare with that of ds_mpit)\n";
+    DG_RANK0 std::cout << "# Norm Centered Derivative "<<sqrt( norm)<<" (compare with that of ds_mpit)\n";
     ///##########################################################///
-    std::cout << "# TEST STAGGERED GRID DERIVATIVE\n";
-    dg::DVec zMinus(fun), eMinus(fun), zPlus(fun), ePlus(fun);
-    dg::DVec funST(fun);
-    dg::geo::Fieldaligned<dg::aProductGeometry3d,dg::IDMatrix,dg::DVec>  dsFAST(
+    DG_RANK0 std::cout << "# TEST STAGGERED GRID DERIVATIVE\n";
+    dg::x::DVec zMinus(fun), eMinus(fun), zPlus(fun), ePlus(fun);
+    dg::x::DVec funST(fun);
+    dg::geo::Fieldaligned<dg::x::aProductGeometry3d,dg::x::IDMatrix,dg::x::DVec>  dsFAST(
             bhat, g3d, dg::NEU, dg::NEU, dg::geo::NoLimiter(), 1e-8, mx[0], mx[1],
             g3d.hz()/2., method);
-    std::cout <<"Time: ";
+    DG_RANK0 std::cout <<"Time: ";
     t.tic();
     for( unsigned i=0; i<10; i++)
         ds.centered( fun, derivative);
     t.toc();
     double gbytes=fun.size()*sizeof(double)/1e9;
-    std::cout << t.diff()/10 << " #\t "<<gbytes*83*10/t.diff()<<"GB/s\n" ;
+    DG_RANK0 std::cout << t.diff()/10 << " #\t "<<gbytes*83*10/t.diff()<<"GB/s\n" ;
 
     for( auto bc : {dg::NEU, dg::DIR})
     {
         if( bc == dg::DIR)
-            std::cout << "DirichletST:\n";
+            DG_RANK0 std::cout << "DirichletST:\n";
         if( bc == dg::NEU)
-            std::cout << "NeumannST:\n";
+            DG_RANK0 std::cout << "NeumannST:\n";
         dsFAST( dg::geo::zeroMinus, fun, zMinus);
         dsFAST( dg::geo::einsPlus,  fun, ePlus);
         dg::geo::assign_bc_along_field_1st( dsFAST, zMinus, ePlus, zMinus, ePlus,
@@ -174,7 +215,7 @@ int main(int argc, char * argv[])
         dg::blas1::axpby( 1., sol0, -1., derivative);
         double norm = dg::blas2::dot( derivative, vol3d, derivative);
         std::string name = "forward";
-        std::cout <<"    "<<name<<":" <<std::setw(18-name.size())
+        DG_RANK0 std::cout <<"    "<<name<<":" <<std::setw(18-name.size())
                   <<" "<<sqrt(norm/sol)<<"\n";
 
         // now try the adjoint direction (should be exactly the same result)
@@ -191,8 +232,11 @@ int main(int argc, char * argv[])
         dg::blas1::axpby( 1., sol0, -1., derivative);
         norm = dg::blas2::dot( derivative, vol3d, derivative);
         name = "backward";
-        std::cout <<"    "<<name<<":" <<std::setw(18-name.size())
+        DG_RANK0 std::cout <<"    "<<name<<":" <<std::setw(18-name.size())
                   <<" "<<sqrt(norm/sol)<<"\n";
     }
+#ifdef WITH_MPI
+    MPI_Finalize();
+#endif
     return 0;
 }
