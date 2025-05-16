c9dc07804ec4743972ea6b727cb952adbe4a2dc6
Liam Pattinson <liampattinson@gmail.com>
2025-05-16 17:49:38 +0100

    Add geometries library target

--- cmake/FeltorBenchmarks.cmake
+++ cmake/FeltorBenchmarks.cmake
@@ -12,7 +12,27 @@ function(add_dg_benchmark benchmark_path)
   # Add as dependency to the dg_benchmarks target
   add_dependencies(dg_benchmarks ${benchmark_name})
   # Ensure benchmarks are built in their own directory
-  set_target_properties(${benchmark_name} PROPERTIES 
+  set_target_properties(${benchmark_name} PROPERTIES
       RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/benchmarks"
+  )
+endfunction()
+
+function(add_dg_geometries_benchmark benchmark_path)
+  if(CCCL_THRUST_DEVICE_SYSTEM STREQUAL "CUDA" OR CCCL_THRUST_DEVICE_SYSTEM STREQUAL "")
+    set_source_files_properties("${benchmark_path}" PROPERTIES LANGUAGE CUDA)
+  endif()
+  # Get a unique name for the benchmark
+  cmake_path(GET benchmark_path STEM benchmark_stem)
+  set(benchmark_name "dg_geometries_${benchmark_stem}")
+  # Create the benchmark executable and link dependencies
+  add_executable(${benchmark_name} "${benchmark_path}")
+  target_link_libraries(${benchmark_name} PRIVATE dg::dg)
+  target_link_libraries(${benchmark_name} PRIVATE dg::file)
+  target_link_libraries(${benchmark_name} PRIVATE dg::geometries)
+  # Add as dependency to the dg_benchmarks target
+  add_dependencies(dg_benchmarks ${benchmark_name})
+  # Ensure benchmarks are built in their own directory
+  set_target_properties(${benchmark_name} PROPERTIES
+      RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/geometries_benchmarks"
   )
 endfunction()
--- cmake/FeltorTests.cmake
+++ cmake/FeltorTests.cmake
@@ -55,7 +55,7 @@ function(add_file_test test_path)
   endif()
   # Get a unique name for the test
   cmake_path(GET test_path STEM test_stem)
-  set(test_name "dg_${test_stem}")
+  set(test_name "dg_file_${test_stem}")
   # Get custom main function
   set(test_main "${CMAKE_SOURCE_DIR}/tests/catch-tests.cpp")
   # Create the test executable and link dependencies
@@ -68,6 +68,30 @@ function(add_file_test test_path)
   # Build tests in ./build/file_tests
   set_target_properties(${test_name} PROPERTIES
     RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/file_tests"
+  )
+  # Register the tests with CTest
+  catch_discover_tests(${test_name})
+endfunction()
+
+function(add_geometries_test test_path)
+  if(CCCL_THRUST_DEVICE_SYSTEM STREQUAL "CUDA" OR CCCL_THRUST_DEVICE_SYSTEM STREQUAL "")
+    set_source_files_properties("${test_path}" PROPERTIES LANGUAGE CUDA)
+  endif()
+  # Get a unique name for the test
+  cmake_path(GET test_path STEM test_stem)
+  set(test_name "dg_geometries_${test_stem}")
+  # Get custom main function
+  set(test_main "${CMAKE_SOURCE_DIR}/tests/catch-tests.cpp")
+  # Create the test executable and link dependencies
+  add_executable(${test_name} "${test_main}" "${test_path}")
+  target_link_libraries(${test_name} PRIVATE dg::dg)
+  target_link_libraries(${test_name} PRIVATE dg::geometries)
+  target_link_libraries(${test_name} PRIVATE Catch2::Catch2)
+  # Add as dependency to the dg_tests target
+  add_dependencies(dg_tests ${test_name})
+  # Build tests in ./build/geometries_tests
+  set_target_properties(${test_name} PROPERTIES
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/geometries_tests"
   )
   # Register the tests with CTest
   catch_discover_tests(${test_name})
