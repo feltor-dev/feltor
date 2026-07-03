69013dbda0d53bcd63a95404c4daf1773a23379d
Matthias <mattwi@fysik.dtu.dk>
2026-07-03 15:38:54 +0200

    Remove fourier functor in this release
    If anything, only used in the stellarator branch

--- inc/dg/functors.h
+++ inc/dg/functors.h
@@ -1918,23 +1918,8 @@ inline double horner( const double * c, unsigned M, double x)
         b = c[M-2-i] + b*x;
     return b;
 }
-inline void fourier_modes( double * modes, unsigned K, unsigned S, double period, double x)
-{
-    // size of modes is K+S+1
-    double xbar = 2.*M_PI / period * x;
-    if( K  > 0)
-        modes[0] = 1.; // The first mode is always 1
-    for( unsigned k=1; k<K; k++)
-    {
-        modes[k] = cos( k * xbar);
-    }
-    for( unsigned s=1; s<S+1; s++)
-    {
-        modes[K+s-1] = sin( s * xbar);
-    }
 
-}
-}
+} // namespace detail
 ///@endcond
 
 /**
@@ -2023,77 +2008,6 @@ struct Horner2d
     unsigned m_M, m_N;
     mutable std::array<double,3> m_prev;
 };
-
-/**
- * @brief \f$ f(x) = a_0 + \sum_{k=1}^{K-1} a_k \cos\left( 2\pi \frac{k}{P} x\right) + \sum_{s=1}^S b_s \sin\left( 2\pi \frac{s}{P} x\right)
- *
- * where \f$ P\f$ is the periodicity \f$ K\f$ is the number of cosine modes (we count the constant as a cosine mode) and \f$ S\f$ is the number sine modes
- */
-struct RealFourier1d
-{
-    ///Initialize 1 coefficient to 1
-    RealFourier1d(): m_ab( 1, 1), m_K(1), m_S(0), m_prev( {0,1}){}
-
-    /// K + S == ab.size()
-    RealFourier1d( const std::vector<double>& ab, unsigned K, unsigned S, double period) : m_ab(ab), m_K(K), m_S(S), m_period( period ), m_modes( ab), m_prev( {1e300, 1e300}){
-        assert( ab.size() >= K+S);
-        }
-
-    double operator()( double x) const
-    {
-        if( m_prev[0] == x)
-            return m_prev[1];
-        detail::fourier_modes( &m_modes[0], m_K, m_S, m_period, x);
-        //m_prev[1] = dg::blas1::vdot( dg::Product(), m_modes, m_ab);
-        //m_prev[1] = dg::blas1::dot( m_modes, m_ab);
-        m_prev[1] = 0;
-        for( unsigned u=0; u<m_modes.size(); u++)
-            m_prev[1] += m_modes[u]*m_ab[u];
-        return m_prev[1];
-    }
-    private:
-    std::vector<double> m_ab;
-    unsigned m_K, m_S;
-    double m_period;
-    mutable std::vector<double> m_modes;
-    mutable std::array<double,2> m_prev;
-};
-
-//struct Horner2dRealFourier1d
-//{
-//    // z direction is contiguous in memory, then y, then x
-//    Horner2dRealFourier1d( const std::vector<double>& c, unsigned M, unsigned
-//        N, unsigned K, unsigned S, double period)
-//    double operator()( double x, double y, double z)
-//    {
-//        // Optimisation rationale: Evaluate z direction first as this is the
-//        // slowest varying in dg::evaluate
-//        if( m_prev[2] == z && m_prev[1] == y && m_prev[0] == x)
-//            return m_prev[3];
-//        if( m_prev[2] != z )
-//        {
-//            detail::fourier_modes( modes, m_K, m_S, period, z);
-//            for( unsigned i=0; i<m_M*m_N; i++)
-//            {
-//                // We could here also use symv here like in dg::Average
-//                auto view = create_view( &m_c[i*(m_K+m_S)], m_K);
-//                m_cxy[i] = dg::blas1::vdot( dg::Product(), view, m_modesz);
-//        }
-//
-//        if( m_prev[1] != y || m_prev[2] != z) // only when both y and z stayed the same can this be skipped
-//            for( unsigned i=0; i<m_M; i++)
-//                m_cx[i] = detail::horner( &m_cxy[i*m_N], m_N, y);
-//        // At this point we know for sure that one of x, y, or z changed
-//        m_prev[3] = detail::horner( &m_cx[0], m_M, x);
-//        m_prev[0] = x, m_prev[1] = y, m_prev[2] = z;
-//        return m_prev[3];
-//
-//    }
-//    private:
-//    mutable std::array<double,4> m_prev;
-//
-//};
-
 
 /**
  * @brief Compute a histogram on a 1D grid
--- inc/dg/functors_t.cpp
+++ inc/dg/functors_t.cpp
@@ -5,6 +5,8 @@
 
 #include "catch2/catch_all.hpp"
 
+//TODO Add tests for other functors
+
 TEST_CASE("Basic Functors")
 {
     std::vector<double> xs( { 1.,2.,3.,4.});
@@ -27,50 +29,6 @@ TEST_CASE("Basic Functors")
             CHECK( 0<=xd[u]);
             CHECK( xd[u] <1);
         }
-    }
-
-}
-
-TEST_CASE("Fourier Series")
-{
-    // Taken from the axis of a W7-X vmec file
-
-    const unsigned nfp = 5;
-    const double phi = 0.5;
-
-    SECTION("COSINE")
-    {
-        const std::vector<double> raxis_cc = {5.64539273e+00,  3.51552974e-01,
-        1.22083861e-02,  6.20226702e-04, -3.23533404e-04, -1.51678453e-05,
-        -8.81100973e-05,  6.59075599e-05, 2.08857013e-05, -6.94325109e-06,
-        -1.48425226e-06,  1.17023571e-06, 8.29344548e-07};
-        const double raxis = 5.367776503087852;
-        dg::RealFourier1d raxis_f( raxis_cc, raxis_cc.size(), 0, 2.*M_PI/(double)nfp);
-        double raxis_num = raxis_f( phi);
-        INFO( "raxis num " << raxis_num);
-        CHECK( fabs( raxis - raxis_num) < 1e-8);
-    }
-    SECTION("SINE")
-    {
-        std::vector<double> zaxis_cs = {-2.95355961e-01, -1.34627261e-02,
-        -1.21378487e-04, 5.46036476e-05, 5.34576546e-05,  3.07960230e-05,
-        7.57340608e-05, 6.57699520e-06, 5.29530210e-06,  1.46030535e-06,
-        2.22207868e-07, 7.06320086e-08};
-        dg::blas1::scal( zaxis_cs, -1);
-        const double zaxis = 0.16405022896331034;
-        dg::RealFourier1d zaxis_f( zaxis_cs, 0, zaxis_cs.size(), 2.*M_PI/(double)nfp);
-        double zaxis_num = zaxis_f( phi);
-        INFO( "zaxis num " << zaxis_num);
-        CHECK( fabs( zaxis - zaxis_num) < 1e-8);
-    }
-    SECTION("const")
-    {
-        std::vector<double> ones( 1 + 12 + 12, 1);
-        dg::RealFourier1d delta_f( ones, 13, 12, 2.*M_PI/(double)nfp);
-        const double delta = 0.059471471292880385;
-        double delta_num = delta_f(phi);
-        INFO( "delta num "<<delta_num);
-        CHECK( fabs( delta - delta_num ) < 1e-8);
     }
 }
 
